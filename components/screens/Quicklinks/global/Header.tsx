81278579aab552ef5c326132a1a02ddf5b4b38cd
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-03-18 19:56:07 +0530

    add: sidebar and header

---
+++ components/screens/Quicklinks/global/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="text-3xl pb-2">Kshitij&apos;s Dashboard</h1>
+      <span className="text-sm text-neutral-400">
+        Quicklinks is the fastest way to work collaboratively
+      </span>
+    </header>
+  );
+}
