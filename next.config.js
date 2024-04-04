2a11d9dc40369f7ecfc66d70b6d569495eb0849c
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-04-04 15:03:34 +0530

    update: next config

--- next.config.js
+++ next.config.js
@@ -1,37 +1,48 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-    //dest: "public",
-    //reactStrictMode: true,
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-    // cacheOnFrontEndNav: true,
-    // aggressiveFrontEndNavCaching: true,
-    // reloadOnOnline: true,
-    // swcMinify: true,
-    // workboxOptions: {
-    //     disableDevLogs: true,
-    // },
-    // ... other options you like
+  //dest: "public",
+  //reactStrictMode: true,
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+
+  // cacheOnFrontEndNav: true,
+  // aggressiveFrontEndNavCaching: true,
+  // reloadOnOnline: true,
+  // swcMinify: true,
+  // workboxOptions: {
+  //     disableDevLogs: true,
+  // },
+  // ... other options you like
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        //looseMode: true,
-        esmExternals: "loose", // <-- add this
-        serverComponentsExternalPackages: ["mongoose"] // <-- and this
-    },
-    webpack: (config) => {
-        config.experiments = {
-            topLevelAwait: true,
-            layers: true,
-        };
-        return config;
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  experimental: {
+    //looseMode: true,
+    esmExternals: "loose", // <-- add this
+    serverComponentsExternalPackages: ["mongoose"], // <-- and this
+  },
+  webpack: (config, { isServer }) => {
+    // Experiments configuration
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true,
+    };
 
-module.exports = withPWA(nextConfig)
+    return config;
+  },
+};
+
+module.exports = withPWA(nextConfig);
