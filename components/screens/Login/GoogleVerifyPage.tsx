517c737ba94ffe551ceb8a8da129181126353845
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-04-10 22:16:00 +0530

    modified: store user google email in localstorage instead of boolean value for verification

--- components/screens/Login/GoogleVerifyPage.tsx
+++ components/screens/Login/GoogleVerifyPage.tsx
@@ -1,69 +1,75 @@
-'use client';
-import Google from '../../../public/icons/google.svg';
-import { useAuthSession } from '@/utils/hooks/useAuthSession';
-import { useUser } from '@/utils/hooks/useUser';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import { setGoogleVerificationStatus } from '../../../utils/redux/auth/auth.slice';
-import { useAppDispatch } from '../../../utils/redux/store';
-import { APP_ROUTES } from '@/utils/constants/appInfo';
-import { useEffect, useState } from 'react';
+"use client";
+import { useAuthSession } from "@/utils/hooks/useAuthSession";
+import { useUser } from "@/utils/hooks/useUser";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { setGoogleVerificationEmail } from "../../../utils/redux/auth/auth.slice";
+import { useAppDispatch } from "../../../utils/redux/store";
+import { APP_ROUTES } from "@/utils/constants/appInfo";
+import { useState } from "react";
 
 const GoogleVerifyPage = ({ signOutUser, setError, passcode }: any) => {
-  const { user, isUserVerified } = useUser();
+  const { user } = useUser();
   const router = useRouter();
   const fetchedUserEmail = user?.email;
   const dispatch = useAppDispatch();
   const { signInWithSocial } = useAuthSession();
   const [verificationFailed, setVerificationFailed] = useState<boolean>(false);
-
-  const handleGoogleLogin = async (e: MouseEvent<HTMLButtonElement>) => {
+  const handleGoogleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    const storedPasscode = localStorage.getItem('passcode');
-    const isVerifiedOnDevice = localStorage.getItem('isGoogleVerified');
-    if (isVerifiedOnDevice === 'true' && storedPasscode === passcode) {
-      router.push(APP_ROUTES.home);
-      return;
-    }
+    // const storedPasscode = localStorage.getItem("passcode");
+    // const isVerifiedOnDevice = localStorage.getItem("isGoogleVerified");
+    // if (isVerifiedOnDevice === "true" && storedPasscode === passcode) {
+    //   router.push(APP_ROUTES.home);
+    //   return;
+    // }
 
     try {
       const user = await signInWithSocial();
-      if (!user || !user.email) throw new Error('User email is empty');
+      if (!user || !user.email) throw new Error("User email is empty");
       const email = user.email;
 
       if (email === fetchedUserEmail) {
-        dispatch(setGoogleVerificationStatus(true));
-        localStorage.setItem('isGoogleVerified', 'true');
-        localStorage.setItem('passcode', passcode);
-        router.push(APP_ROUTES.home);
+        handleSuccessfulVerification(email);
       } else {
-        dispatch(setGoogleVerificationStatus(false));
-        setVerificationFailed(true);
-        let counter = 3;
-        const intervalId = setInterval(() => {
-          setError(`Email not verified!, Signing out in ${counter} seconds!`);
-          counter--;
-          if (counter < 0) {
-            clearInterval(intervalId);
-            signOutUser();
-          }
-        }, 1000);
+        handleFailedVerification();
       }
     } catch (error) {
       console.log(error);
     }
   };
 
+  const handleFailedVerification = () => {
+    // dispatch(setGoogleVerificationEmail(false));
+    setVerificationFailed(true);
+    let counter = 3;
+    const intervalId = setInterval(() => {
+      setError(`Email not verified!, Signing out in ${counter} seconds!`);
+      counter--;
+      if (counter < 0) {
+        clearInterval(intervalId);
+        signOutUser();
+      }
+    }, 1000);
+  };
+
+  const handleSuccessfulVerification = (verifiedEmail: string) => {
+    dispatch(setGoogleVerificationEmail(verifiedEmail));
+    // localStorage.setItem("isGoogleVerified", "true");
+    // localStorage.setItem("passcode", passcode);
+    router.push(APP_ROUTES.home);
+  };
+
   return (
     <div>
       <button
-        className='w-full flex flex-row justify-center items gap-[0.5rem] px-[1rem] py-[0.5rem] border-[#959595] rounded-[0.5rem] bg-[#fff] color-[#000] capitalize'
+        className="w-full flex flex-row justify-center items-center gap-[0.5rem] px-[1rem] py-[0.5rem] border-[#959595] rounded-[0.5rem] bg-[#fff] color-[#000] capitalize"
         onClick={handleGoogleLogin}
         disabled={verificationFailed}
       >
-        <Image src={Google} alt='icon' />
-        Verify with Google
+        <Image src="/icons/google.svg" alt="icon" width={20} height={20} />
+        <span>Verify with Google</span>
       </button>
     </div>
   );
