f53cc070a4d74720017869ae8b373e37130bde1e
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-04-12 21:57:10 +0530

    fix: bottom bar logout icon issue

--- components/global/Bottombar.tsx
+++ components/global/Bottombar.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { APP_ROUTES, AppRoutesHelper } from '@/utils/constants/appInfo';
-import { useUser } from '@/utils/hooks/useUser';
-import { USERTYPE } from '@prisma/client';
-import { usePathname, useRouter } from 'next/navigation';
-import Logo from '../../public/logo/logo_white.png';
-import Image from 'next/image';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import Link from 'next/link';
+import { APP_ROUTES, AppRoutesHelper } from "@/utils/constants/appInfo";
+import { useUser } from "@/utils/hooks/useUser";
+import { USERTYPE } from "@prisma/client";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 const NAVIGATION_OPTIONS = [
   {
@@ -79,13 +77,15 @@ export const Bottombar = ({ visible = true }: { visible?: boolean }) => {
 
   return (
     <div
-      className={`flex flex-row fixed bottom-0 left-0 right-0 py-1 px-1 max-md:mx-1 max-md:my-1 gap-6 z-10 bg-neutral-900 max-md:rounded-[1.15em] md:bottom-auto md:left-0 md:top-0 md:flex-col md:w-20 md:h-full md:fixed-none`}
+      className={`flex flex-row fixed bottom-0 left-0 right-0 py-1 px-1 max-md:mx-1 max-md:my-1 gap-6 z-10 bg-neutral-900 max-md:rounded-2xl md:bottom-auto md:left-0 md:top-0 md:flex-col md:w-20 md:h-full md:fixed-none`}
     >
       <Link href={APP_ROUTES.home}>
         <Image
-          src={Logo}
-          alt='Moon Portal Logo'
-          className='w-[3rem] h-[3rem] mx-auto my-4 rounded max-md:hidden '
+          src="/logo/logo_white.png"
+          alt="Moon Portal Logo"
+          width={100}
+          height={100}
+          className="w-12 h-12 mx-auto my-4 rounded max-md:hidden "
         />
       </Link>
       {options.map((option) => (
@@ -95,27 +95,25 @@ export const Bottombar = ({ visible = true }: { visible?: boolean }) => {
           }}
           key={option.path}
           className={` ${
-            option.path === path ? 'bg-white text-black' : 'bg-black text-white'
-          } flex flex-col items-center justify-center py-1 w-1/3 cursor-pointer rounded-[1em] md:w-full`}
+            option.path === path ? "bg-white text-black" : "bg-black text-white"
+          } flex flex-col items-center justify-center py-1 w-1/3 cursor-pointer rounded-2xl md:w-full`}
         >
           <span
             className={` ${
-              option.path === path ? 'text-black' : 'text-white'
+              option.path === path ? "text-black" : "text-white"
             } font-thin material-icons-outlined text-md `}
           >
             {option.icon}
           </span>
-          <p className='text-[0.5em] opacity-[0.75]'>{option.name}</p>
+          <p className="text-[0.5em] opacity-75">{option.name}</p>
         </div>
-      ))}{' '}
+      ))}{" "}
       <Link href={APP_ROUTES.logout}>
         <button
-          className={`flex flex-col text-md items-center justify-center py-1 cursor-pointer rounded-[1em] w-[90%] max-md:hidden absolute bottom-5 bg-black hover:bg-neutral-700 `}
+          className={`flex flex-col text-md items-center justify-center py-1 cursor-pointer rounded-2xl w-[90%] max-md:hidden absolute bottom-5 bg-black hover:bg-neutral-700 `}
         >
-          <LogoutRoundedIcon
-            sx={{ color: 'white', height: '1.5rem', width: '1.5rem' }}
-          />
-          <span className='text-[0.5em] opacity-[0.75] text-white max-md:hidden'>
+          <span className="material-symbols-outlined text-white">logout</span>
+          <span className="text-[0.5em] opacity-75 text-white max-md:hidden">
             Logout
           </span>
         </button>
