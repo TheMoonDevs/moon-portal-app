48c47cd64961840a81ca4897135490a32f4a3dee
shreyas0924 <shreyasp0924@gmail.com>
2024-05-07 11:46:21 +0530

    file-uploads page structure changes

--- app/file-uploads/page.tsx
+++ app/file-uploads/page.tsx
@@ -1,12 +1,19 @@
 import { Bottombar } from "@/components/global/Bottombar";
 import { PageAccess } from "@/components/global/PageAccess";
-import UploadFileComponent from "@/components/screens/UploadFile/UploadFileComponent";
+import { DropzoneButton } from "@/components/screens/UploadFile/DropzoneButton";
+import FilesTable from "@/components/screens/UploadFile/FilesTable";
+import Searchbar from "@/components/screens/UploadFile/Searchbar";
 
 export default function Home() {
   return (
     <PageAccess isAuthRequired={true}>
-      <UploadFileComponent />
-      <Bottombar />
+      <div className="md:mx-4 md:mt-6 flex flex-col gap-4">
+        <h1 className="lg:text-2xl md:text-xl md:mx-3 font-semibold">Upload Files</h1>
+        <Searchbar />
+        <DropzoneButton />
+        <FilesTable />
+        <Bottombar />
+      </div>
     </PageAccess>
   );
 }
