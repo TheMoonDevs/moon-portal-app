4a65ca614b16c4c5ff53ab39867b1377e5245998
shreyas0924 <shreyasp0924@gmail.com>
2024-05-07 11:44:12 +0530

    searchbar ui

--- components/screens/UploadFile/Searchbar.tsx
+++ components/screens/UploadFile/Searchbar.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const Searchbar = () => {
   return (
-    <div className=''>
-     Searchbar 
-    </div>
-  )
-}
+    <form className="flex max-w-xl mx-3">
+      <label htmlFor="simple-search" className="sr-only">
+        Search
+      </label>
+      <div className="relative w-full">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="simple-search"
+          className="bg-gray-50 border border-gray-400  text-sm rounded-lg  w-full ps-10 p-2.5    "
+          placeholder="Search branch name..."
+          required
+        />
+      </div>
+      {/* <button
+        type="submit"
+        className="p-2.5 ms-2 text-sm font-medium text-white rounded-lg border border-gray-400"
+      >
+        <svg
+          className="w-4 h-4 text-gray-900"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+        <span className="sr-only">Search</span>
+      </button> */}
+    </form>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
