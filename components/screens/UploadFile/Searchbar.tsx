825dcccf8a3fa18349fa88a4d93e84c394013106
shreyas0924 <shreyasp0924@gmail.com>
2024-05-09 14:57:28 +0530

    search bar with ui and functionality

--- components/screens/UploadFile/Searchbar.tsx
+++ components/screens/UploadFile/Searchbar.tsx
@@ -1,6 +1,15 @@
+"use client";
+import { setSearchTerm } from "@/utils/redux/searchTerm/search.slice";
+import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
 import React from "react";
 
 const Searchbar = () => {
+  const searchTerm = useAppSelector((state) => state.searchTerm.term);
+  const dispatch = useAppDispatch();
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchTerm(e.target.value));
+  };
   return (
     <form className="flex max-w-xl mx-3">
       <label htmlFor="simple-search" className="sr-only">
@@ -28,31 +37,12 @@ const Searchbar = () => {
           type="text"
           id="simple-search"
           className="bg-gray-50 border border-gray-400  text-sm rounded-lg  w-full ps-10 p-2.5    "
-          placeholder="Search branch name..."
+          placeholder="Search files..."
+          value={searchTerm}
           required
+          onChange={handleSearchInputChange}
         />
       </div>
-      {/* <button
-        type="submit"
-        className="p-2.5 ms-2 text-sm font-medium text-white rounded-lg border border-gray-400"
-      >
-        <svg
-          className="w-4 h-4 text-gray-900"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-          />
-        </svg>
-        <span className="sr-only">Search</span>
-      </button> */}
     </form>
   );
 };
