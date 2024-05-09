2f0942ef2f203a3f4a81d3f1a2d1e3b95e4796b1
shreyas0924 <shreyasp0924@gmail.com>
2024-05-09 16:55:27 +0530

    preview in dropzone

--- components/screens/UploadFile/DropzoneButton.tsx
+++ components/screens/UploadFile/DropzoneButton.tsx
@@ -4,10 +4,13 @@ import { Button } from "@/components/elements/Button";
 import { RootState, useAppDispatch } from "@/utils/redux/store";
 import { useSelector } from "react-redux";
 import {
+  FileWithPath,
   addFile,
   removeFile,
 } from "@/utils/redux/filesUpload/filesUpload.slice";
-import { Dropzone, FileWithPath, MIME_TYPES } from "@mantine/dropzone";
+import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
+import { IconCloudUpload } from "@tabler/icons-react";
+import { Group, rem } from "@mantine/core";
 import { useUser } from "@/utils/hooks/useUser";
 
 export function DropzoneButton() {
@@ -15,24 +18,18 @@ export function DropzoneButton() {
   const { user } = useUser();
   const files = useSelector((state: RootState) => state.filesUpload.files);
   const openRef = useRef<() => void>(null);
-  const handleDrop = (droppedFiles: any[]) => {
-    // Remove previously uploaded file
-    if (files.length > 0) {
-      dispatch(removeFile(0));
-    }
+  const handleDrop = (droppedFiles: FileWithPath[]) => {
+    console.log(droppedFiles);
     // Add the new file
-    dispatch(addFile(droppedFiles[0]));
+    dispatch(addFile(droppedFiles));
   };
-
-  // const handleRemove = (index: number) => {
-  //   dispatch(removeFile(index));
-  // };
 
   const handleUpload = async () => {
     if (files.length > 0) {
-      const file = files[0] as FileWithPath;
       const formData = new FormData();
-      formData.append("file", file);
+      files.forEach((file) => {
+        formData.append("file", file, file.path);
+      });
       if (user) {
         const userId = user.id;
         formData.append("userId", userId);
@@ -57,54 +54,80 @@ export function DropzoneButton() {
     }
   };
 
+  const previews = files.map((file: FileWithPath, index) => {
+    const fileUrl = URL.createObjectURL(file);
+    return (
+      <div className="relative group" key={index}>
+        <span
+          className="material-symbols-outlined absolute -top-2 -right-2 cursor-pointer text-white bg-[rgba(0,0,0,0.5)] rounded-full p-1"
+          style={{ fontSize: "1rem" }}
+          onClick={() =>
+            dispatch(
+              removeFile({
+                path: file.path,
+                lastModified: file.lastModified,
+              })
+            )
+          }
+        >
+          close
+        </span>
+        {file.type === "image/png" || file.type === "image/jpeg" ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            style={{ height: "100px", aspectRatio: "1/1" }}
+            alt={file.path}
+            key={index}
+            src={fileUrl}
+            onLoad={() => URL.revokeObjectURL(fileUrl)}
+          />
+        ) : (
+          <a
+            href={fileUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-500 hover:underline"
+          >
+            {file.path}
+          </a>
+        )}
+      </div>
+    );
+  });
+
   return (
-    <div className=" rounded-xl md:my-4 md:mx-3 p-2 cursor-pointer ">
+    <div className="border-2 border-dashed border-gray-400 rounded-xl md:my-4 md:mx-3 p-2 ">
       <Dropzone
         onDrop={handleDrop}
         radius="md"
         accept={[
           MIME_TYPES.pdf,
+          MIME_TYPES.doc,
+          MIME_TYPES.docx,
           MIME_TYPES.jpeg,
           MIME_TYPES.png,
+          MIME_TYPES.xls,
+          MIME_TYPES.xlsx,
           MIME_TYPES.svg,
-          MIME_TYPES.doc,
+          MIME_TYPES.webp
         ]}
         maxSize={30 * 1024 ** 2}
-        className="group relative flex h-48 cursor-pointer items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 transition-colors hover:border-gray-400"
+        className="py-8 cursor-pointer"
+        multiple
       >
-        {files.length > 0 ? (
+        <div style={{ pointerEvents: "none" }}>
+          <Group justify="center">
+            <Dropzone.Idle>
+              <IconCloudUpload
+                style={{ width: rem(20), height: rem(40) }}
+                stroke={1.5}
+              />
+            </Dropzone.Idle>
+          </Group>
           <div className="flex justify-center items-center font-semibold text-gray-500">
-            {files.map((file, index) => (
-              <div key={index}>{file.path}</div>
-            ))}
+            <Dropzone.Idle>Drop your documents here, or</Dropzone.Idle>
           </div>
-        ) : (
-          <>
-            <div className="pointer-events-none space-y-1 text-center">
-              <svg
-                className="mx-auto h-8 w-8 text-gray-400 group-hover:text-gray-500 "
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <Dropzone.Idle>
-                Drop your documents here, or click to upload
-              </Dropzone.Idle>
-            </div>
-            <input
-              className="absolute inset-0 h-full w-full cursor-pointer opacity-0"
-              type="file"
-            />
-          </>
-        )}
+        </div>
         <p
           className="cursor-pointer text-blue-500 underline underline-offset-2 flex justify-center items-center my-2"
           onClick={() => openRef.current?.()}
@@ -112,6 +135,7 @@ export function DropzoneButton() {
           click to browse
         </p>
       </Dropzone>
+      <div className="flex gap-4 flex-wrap">{previews}</div>
       {files.length > 0 && (
         <div className="flex justify-center items-center">
           <Button onClick={handleUpload}>Upload File</Button>
