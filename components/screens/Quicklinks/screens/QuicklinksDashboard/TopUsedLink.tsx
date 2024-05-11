20cc36635a26d5192416d92856f08666ecb84bdc
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2024-05-11 19:59:13 +0530

    View - name change & linkActions as popover

--- components/screens/Quicklinks/screens/QuicklinksDashboard/TopUsedLink.tsx
+++ components/screens/Quicklinks/screens/QuicklinksDashboard/TopUsedLink.tsx
@@ -5,7 +5,7 @@ export default function TopUsedLink({
   children: React.ReactNode;
 }) {
   return (
-    <div className=" bg-gray-100 p-8 rounded-sm my-4">
+    <div className=" p-8 rounded-sm my-4">
       <h2 className="uppercase tracking-[0.5rem] text-base font-normal text-gray-500">
         Top Used
       </h2>
