658abc81eb9d8592dde5efa13121174c62fbac2b
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2024-06-02 02:21:01 +0530

    metadata additions

--- portal/app/user/worklogs/page.tsx
+++ portal/app/user/worklogs/page.tsx
@@ -1,6 +1,14 @@
 import { Bottombar } from "@/components/global/Bottombar";
 import { PageAccess } from "@/components/global/PageAccess";
 import { WorklogsPage } from "@/components/screens/Worklogs/WorklogsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  manifest: "/manifest.json", // we are accessing our manifest file here
+  title: "My Worklogs - TheMoonDevs",
+  // TODO - change favicon to check icon on green circle
+  // icon: "/favicon.ico",
+};
 
 export default function Worklogs() {
   return (
