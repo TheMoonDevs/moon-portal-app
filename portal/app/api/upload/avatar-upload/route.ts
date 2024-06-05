d86ba6ac6d6c4f025a6fc1f38d1738f3a41507af
shreyas0924 <shreyasp0924@gmail.com>
2024-06-05 19:52:36 +0530

    feat: api for uploading avatar image

---
+++ portal/app/api/upload/avatar-upload/route.ts
@@ -0,0 +1,51 @@
+import { prisma } from "@/prisma/prisma";
+import { fileUploadSdk } from "@/utils/services/fileUploadSdk";
+import { File } from "buffer";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const files = formData.getAll("file") as unknown as File[];
+
+    if (!files || files.length === 0) {
+      return NextResponse.json("File not found", { status: 404 });
+    }
+
+    const filePromises = files.map(async (file) => {
+      const s3Response = await fileUploadSdk.uploadFile({
+        file,
+      });
+
+      if (!s3Response || s3Response.$metadata.httpStatusCode !== 200) {
+        throw new Error("Failed to upload file");
+      }
+
+      const fileInfo = {
+        fileUrl: fileUploadSdk.getPublicFileUrl({
+          file,
+        }),
+        fileName: file.name,
+        mimeType: file.type,
+        fileSize: file.size,
+      };
+
+      return {
+        ...fileInfo,
+      };
+    });
+
+    const fileInfo = await Promise.all(filePromises);
+
+    // Save to db
+
+    const DBresponse = await prisma.fileUpload.createMany({
+      data: fileInfo,
+    });
+    // console.log(DBresponse);
+    return NextResponse.json({ DBresponse, fileInfo });
+  } catch (reason) {
+    console.log(reason);
+    return NextResponse.json({ message: "failure" });
+  }
+}
