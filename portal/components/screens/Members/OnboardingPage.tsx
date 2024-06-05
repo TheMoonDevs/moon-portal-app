23c842a61047cbdbaf386723d6833e009b04a77b
shreyas0924 <shreyasp0924@gmail.com>
2024-06-05 19:53:58 +0530

    feat: onboarding page component with all the fields and database logic

---
+++ portal/components/screens/Members/OnboardingPage.tsx
@@ -0,0 +1,433 @@
+"use client";
+
+import { Textarea } from "@/components/elements/Textarea";
+import { Button } from "@/components/elements/Button";
+import { Input } from "@/components/elements/Input";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/elements/Select";
+
+import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
+import {
+  resetForm,
+  updateForm,
+} from "@/utils/redux/onboarding/onboarding.slice";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/elements/InputOtp";
+import { UploadAvatar } from "./UploadAvatar";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import React, { useEffect, useState } from "react";
+import { USERVERTICAL, USERROLE } from "@prisma/client";
+import { useCountryInfo } from "@/utils/hooks/useCountryInfo";
+import Image from "next/image";
+import { CircleCheck, CircleX, Info } from "lucide-react";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/elements/Tooltip";
+import DatePicker from "./DatePicker";
+
+interface OnboardingPageProps {
+  onClose?: () => void;
+}
+
+export default function OnboardingPage({ onClose }: OnboardingPageProps) {
+  const dispatch = useAppDispatch();
+  const formData = useAppSelector((state) => state.onboardingForm);
+  const [message, setMessage] = useState("");
+  const [messageColor, setMessageColor] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | any>
+  ) => {
+    const { name, value } = e.target;
+    dispatch(updateForm({ name: name as keyof typeof formData, value }));
+  };
+
+  const handleSelectChange = (value: string, name: keyof typeof formData) => {
+    dispatch(updateForm({ name, value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // Concatenate first name and last name
+    const fullName = `${formData.firstName} ${formData.lastName}`;
+
+    const updatedFormData = {
+      ...formData,
+      name: fullName,
+      username: username,
+      passcode: password,
+      dateOfBirth: selectedDate.split("T")[0],
+    };
+    try {
+      const response = await fetch("/api/onboarding", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedFormData),
+      });
+
+      if (response.ok) {
+        dispatch(resetForm());
+        setUsername("");
+        setPassword("");
+        if (onClose) onClose();
+      } else {
+        console.error("Failed to submit form");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  const handleUsernameInput = async (username: any) => {
+    console.log("handleUsernameInput called with username:", username);
+    try {
+      setMessageColor("");
+
+      const response = await fetch(`/api/onboarding?username=${username}`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("API response data:", data);
+
+      if (data.message === "Username is already taken") {
+        setMessage("Username is taken");
+        setMessageColor("red");
+      } else {
+        setMessage("Good to go");
+        setMessageColor("green");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setMessage("An error occurred");
+      setMessageColor("red");
+    }
+  };
+
+  const handleComplete = (value: any) => {
+    if (value.length === 3) {
+      handleUsernameInput(value);
+    }
+  };
+  const showMessage = (newMessage: string, newMessageColor: string) => {
+    if (newMessage !== "" && newMessageColor !== "") {
+      setMessage(newMessage);
+      setMessageColor(newMessageColor);
+    }
+  };
+
+  const handleUsernameChange = (newValue: any) => {
+    const upperCaseValue = newValue.toUpperCase();
+    setUsername(upperCaseValue);
+    if (upperCaseValue.length === 3) {
+      handleComplete(upperCaseValue);
+    } else {
+      setMessage("");
+      setMessageColor("");
+    }
+  };
+
+  const handlePasswordChange = (newValue: any) => {
+    setPassword(newValue);
+  };
+
+  const handleDateChange = (selectedDate: string) => {
+    if (selectedDate) {
+      console.log("Selected date:", selectedDate);
+      setSelectedDate(selectedDate);
+    } else {
+      // Clear the selected date
+      setSelectedDate("");
+    }
+  };
+
+  useEffect(() => {
+    setMessage("");
+    setMessageColor("");
+  }, [username]);
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-[60rem] mx-auto p-6 my-4 border border-gray-400 rounded-lg shadow-xl"
+    >
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="First name"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+
+        <Input
+          type="text"
+          placeholder="Last name"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+      </div>
+
+      <Input
+        type="email"
+        placeholder="Email address"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        className="mb-4"
+      />
+      <div className="">
+        {formData.avatar !== "" ? (
+          <div className="flex gap-4 my-3 w-full items-center">
+            <label className="w-1/4">Profile Picture URL :</label>
+            <a href={formData.avatar} target="_blank">
+              {formData.avatar}
+            </a>
+          </div>
+        ) : (
+          <UploadAvatar />
+        )}
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="UPI ID"
+          name="upiId"
+          value={formData.upiId}
+          onChange={handleChange}
+        />
+
+        {/* Date of Birth */}
+        <DatePicker onDateChange={handleDateChange} />
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="City"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          placeholder="Position"
+          name="position"
+          value={formData.position}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex justify-between items-center gap-1">
+          <Input
+            type="text"
+            placeholder="Work Hour Overlap"
+            name="workHourOverlap"
+            value={formData.workHourOverlap}
+            onChange={handleChange}
+            className=""
+          />
+
+          <TooltipProvider>
+            <Tooltip delayDuration={100}>
+              <TooltipTrigger>
+                {" "}
+                <Info className="opacity-60 h-5 w-5" />
+              </TooltipTrigger>
+              <TooltipContent className="bg-white">
+                <p>Example: 2pm - 5pm</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+        <Input
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+      </div>
+      <Textarea
+        placeholder="Address"
+        name="address"
+        value={formData.address}
+        onChange={handleChange}
+        className="mb-4"
+      />
+
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex gap-4 items-center">
+          <label htmlFor="username" className="block mb-2">
+            Username:
+          </label>
+          <InputOTP
+            id="username"
+            maxLength={3}
+            pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+            alt="Username input"
+            value={username}
+            onFocus={() => showMessage("", " ")}
+            onChange={handleUsernameChange}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+          </InputOTP>
+          {message && (
+            <div className="ml-4">
+              {messageColor === "red" ? (
+                <TooltipProvider>
+                  <Tooltip delayDuration={100}>
+                    <TooltipTrigger>
+                      {" "}
+                      <CircleX color="red" />
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-white">
+                      <p>Username Already Taken</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              ) : (
+                <TooltipProvider>
+                  <Tooltip delayDuration={100}>
+                    <TooltipTrigger>
+                      {" "}
+                      <CircleCheck color="green" />
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-white">
+                      <p>Username Available</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              )}
+            </div>
+          )}
+        </div>
+        <div className="flex gap-4 items-center">
+          <label htmlFor="password" className="block mb-2">
+            Password:
+          </label>
+          <InputOTP
+            id="password"
+            maxLength={3}
+            alt="Password input"
+            value={password}
+            onChange={handlePasswordChange}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+          </InputOTP>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <Select>
+          <SelectTrigger id="user-vertical">
+            <SelectValue placeholder="User Vertical" />
+          </SelectTrigger>
+          <SelectContent className="bg-white">
+            {Object.values(USERVERTICAL).map((vertical) => (
+              <SelectItem
+                key={vertical}
+                value={vertical}
+                onClick={() => handleSelectChange(vertical, "userVertical")}
+                className="hover:bg-gray-200 rounded-lg cursor-pointer"
+              >
+                {vertical}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex justify-between items-center gap-1">
+          <Input
+            type="text"
+            placeholder="Timezone"
+            name="timezone"
+            value={
+              // countryInfo
+              //   ? (dispatch(
+              //       updateForm({ name: "timezone", value: countryInfo.timezone })
+              //     ),
+              //     countryInfo.timezone)
+              //   :
+              formData.timezone
+            }
+            onChange={handleChange}
+          />
+          <TooltipProvider>
+            <Tooltip delayDuration={100}>
+              <TooltipTrigger>
+                {" "}
+                <Info className="opacity-60 h-5 w-5" />
+              </TooltipTrigger>
+              <TooltipContent className="bg-white">
+                <p>Example: IST</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+        <div className="flex justify-between items-center gap-1">
+          <Input
+            type="text"
+            placeholder="Country Code"
+            name="countryCode"
+            value={
+              // countryInfo
+              //   ? (dispatch(
+              //       updateForm({
+              //         name: "countryCode",
+              //         value: countryInfo.countryCode,
+              //       })
+              //     ),
+              //     countryInfo.countryCode)
+              //   :
+              formData.countryCode
+            }
+            onChange={handleChange}
+          />
+          <TooltipProvider>
+            <Tooltip delayDuration={100}>
+              <TooltipTrigger>
+                {" "}
+                <Info className="opacity-60 h-5 w-5" />
+              </TooltipTrigger>
+              <TooltipContent className="bg-white">
+                <p>Example: +91</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+      <div className="flex gap-4 items-center mt-6">
+        <div className="ml-auto">
+          <Button type="submit">Save changes</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
