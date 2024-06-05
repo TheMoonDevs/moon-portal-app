5a7cc4638abd2a8c0c794e72a56cef18d108c25d
shreyas0924 <shreyasp0924@gmail.com>
2024-06-05 19:48:22 +0530

    feat: added new user role and wrote types for personalData from user collection

---
+++ portal/prisma/UserScehmaInterfaces.ts
@@ -0,0 +1,11 @@
+export interface personalData {
+  phone: number;
+  upiId: string;
+  dateOfBirth: string;
+  city: string;
+  position: string;
+  workHourOverlap: string;
+  address: string;
+  avatar: string;
+  banner?: string;
+}
