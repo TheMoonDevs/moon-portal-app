07d209b587b1c549f73dde7c7ecf3a7454680398
shreyas0924 <shreyasp0924@gmail.com>
2024-06-10 18:48:13 +0530

    fix: removed extra loading spinner component

--- portal/components/elements/Loaders.tsx
+++ portal/components/elements/Loaders.tsx
@@ -1,9 +1,9 @@
 export const Spinner = ({ className }: { className?: string }) => {
   return (
-    <div role="status">
+    <div>
       <svg
         aria-hidden="true"
-        className={`w-8 h-8 animate-spin ${className} `}
+        className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
