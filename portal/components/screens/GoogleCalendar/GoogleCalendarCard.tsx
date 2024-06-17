9c5cbe5a2b7ee3de0ec762cc0deed2335ff5f707
ChandraShekhar <csyadav0513@gmail.com>
2024-06-17 19:27:11 +0530

    Fix UI Bug

--- portal/components/screens/GoogleCalendar/GoogleCalendarCard.tsx
+++ portal/components/screens/GoogleCalendar/GoogleCalendarCard.tsx
@@ -299,31 +299,32 @@ const GoogleCalendaCard: React.FC = () => {
       <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-3 h-[1px] w-full" />
 
       <form className="my-3" onSubmit={handleSubmit}>
-        <div className="flex justify-end items-center">
-          <Tooltip title={"Set Task All Day"} arrow>
-            <Checkbox
-              checked={formData.allDay}
-              onChange={toggleAllDay}
-              sx={{
-                color: "#fff",
-                "&.Mui-checked": {
-                  color: "#0096FF",
-                },
-                "& .MuiSvgIcon-root": {
-                  width: "1.2em",
-                  height: "1.2em",
-                },
-              }}
-            />
-          </Tooltip>
-          <label
-            className="text-sm font-medium text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            htmlFor="allDay"
-          >
-            All Day
-          </label>
-        </div>
-
+        <LabelInputContainer>
+          <div className="flex justify-end items-center">
+            <Tooltip title={"Set Task All Day"} arrow>
+              <Checkbox
+                checked={formData.allDay}
+                onChange={toggleAllDay}
+                sx={{
+                  color: "#fff",
+                  "&.Mui-checked": {
+                    color: "#0096FF",
+                  },
+                  "& .MuiSvgIcon-root": {
+                    width: "1.2em",
+                    height: "1.2em",
+                  },
+                }}
+              />
+            </Tooltip>
+            <label
+              className="text-sm font-medium text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="allDay"
+            >
+              All Day
+            </label>
+          </div>
+        </LabelInputContainer>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <label htmlFor="title">
@@ -347,25 +348,26 @@ const GoogleCalendaCard: React.FC = () => {
               onChange={handleInputChange}
             />
           </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <label
-              htmlFor="details"
-              className="text-sm  font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Details
-            </label>
-            <textarea
-              id="details"
-              className="flex min-h-[60px] w-full rounded-md border border-input bg-gray-100 text-gray-700 font-bold px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-              name="details"
-              placeholder="Add Details"
-              value={formData.details}
-              onChange={handleInputChange}
-            />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <label
-              className="
+        </div>
+        <LabelInputContainer className="mb-4">
+          <label
+            htmlFor="details"
+            className="text-sm  font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Details
+          </label>
+          <textarea
+            id="details"
+            className="flex min-h-[60px] w-full rounded-md border border-input bg-gray-100 text-gray-700 font-bold px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+            name="details"
+            placeholder="Add Details"
+            value={formData.details}
+            onChange={handleInputChange}
+          />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <label
+            className="
                 text-sm
                 font-medium
                 mt-1
@@ -373,57 +375,83 @@ const GoogleCalendaCard: React.FC = () => {
                 leading-none
                 peer-disabled:cursor-not-allowed
                 peer-disabled:opacity-70"
-              htmlFor="location"
-            >
-              Location
-            </label>
-            <input
-              id="location"
-              name="location"
-              placeholder="Add Location"
-              type="text"
-              className="flex h-10 w-full bg-gray-100 font-bold text-gray-700 shadow-input rounded-md px-3 py-2 text-sm placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition duration-400"
-              value={formData.location}
-              onChange={handleInputChange}
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <label htmlFor="startDate">
-              {formValidations.startDate ? (
-                <span className="text-red-500 font-bold ">
-                  *Start Date is Required
-                </span>
-              ) : (
-                <span className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                  {" "}
-                  Start Date
-                </span>
-              )}
-            </label>
-            <TextField
-              id="mui-date-picker"
-              type="date"
-              value={
-                formData.startDate
-                  ? format(formData.startDate, "yyyy-MM-dd")
-                  : ""
+            htmlFor="location"
+          >
+            Location
+          </label>
+          <input
+            id="location"
+            name="location"
+            placeholder="Add Location"
+            type="text"
+            className="flex h-10 w-full bg-gray-100 font-bold text-gray-700 shadow-input rounded-md px-3 py-2 text-sm placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition duration-400"
+            value={formData.location}
+            onChange={handleInputChange}
+          />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <label htmlFor="startDate">
+            {formValidations.startDate ? (
+              <span className="text-red-500 font-bold ">
+                *Start Date is Required
+              </span>
+            ) : (
+              <span className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                {" "}
+                Start Date
+              </span>
+            )}
+          </label>
+          <TextField
+            id="mui-date-picker"
+            type="date"
+            value={
+              formData.startDate ? format(formData.startDate, "yyyy-MM-dd") : ""
+            }
+            onChange={(e) =>
+              handleDateChange("startDate")(
+                e.target.value ? new Date(e.target.value) : null
+              )
+            }
+            InputLabelProps={{
+              shrink: true,
+            }}
+            placeholder="Select date"
+            onFocus={(e) => {
+              if (!formData.startDate) {
+                e.target.value = getTodayDateString();
               }
-              onChange={(e) =>
-                handleDateChange("startDate")(
-                  e.target.value ? new Date(e.target.value) : null
-                )
-              }
-              InputLabelProps={{
-                shrink: true,
-              }}
-              placeholder="Select date"
-              onFocus={(e) => {
-                if (!formData.startDate) {
-                  e.target.value = getTodayDateString();
-                }
-              }}
-              InputProps={{
-                style: {
+            }}
+            InputProps={{
+              style: {
+                backgroundColor: "white",
+                color: "#4A5568",
+                fontWeight: "bold",
+                width: "100%",
+                height: "40px",
+                borderRadius: "4px",
+                fontSize: "14px",
+              },
+            }}
+            variant="outlined"
+          />
+        </LabelInputContainer>
+
+        <LabelInputContainer className="mb-4">
+          <div className="flex justify-between  w-full items-center gap-3">
+            <div className={`w-full`}>
+              <label
+                className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                htmlFor="repeat"
+              >
+                Repeat
+              </label>
+              <Select
+                value={formData.repeat}
+                onChange={handleSelectChange}
+                displayEmpty
+                inputProps={{ "aria-label": "Without label" }}
+                style={{
                   backgroundColor: "white",
                   color: "#4A5568",
                   fontWeight: "bold",
@@ -431,128 +459,124 @@ const GoogleCalendaCard: React.FC = () => {
                   height: "40px",
                   borderRadius: "4px",
                   fontSize: "14px",
-                },
-              }}
-              variant="outlined"
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <div className="flex justify-between  w-full items-center gap-3">
-              <div className={`w-full`}>
+                  paddingRight: "30px", // Adjust padding for dropdown arrow
+                }}
+                MenuProps={{
+                  PaperProps: {
+                    style: {
+                      backgroundColor: "white",
+                      color: "#4A5568",
+                    },
+                  },
+                }}
+                renderValue={(selected) => (
+                  <span style={{ color: selected ? "#4A5568" : "#BCCCDC" }}>
+                    {selected || "Select"}
+                  </span>
+                )}
+              >
+                <MenuItem value="no-repeat">Doesn&apos;t repeat</MenuItem>
+                <MenuItem value="daily">Daily</MenuItem>
+                <MenuItem value="weekly">
+                  Weekly on{" "}
+                  {getWeekdayFromDate(new Date(formData.startDate || today))}
+                </MenuItem>
+                <MenuItem value="monthly">
+                  Monthly on the third{" "}
+                  {getWeekdayFromDate(new Date(formData.startDate || today))}
+                </MenuItem>
+                <MenuItem value="annually">
+                  Annually on{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    month: "long",
+                    day: "numeric",
+                  }).format(new Date(formData.startDate || today))}
+                </MenuItem>
+                <MenuItem value="every-weekday">Weekly on Weekdays</MenuItem>
+              </Select>
+            </div>
+
+            {formData.repeat !== "no-repeat" && (
+              <div className="w-1/2">
                 <label
                   className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  htmlFor="repeat"
+                  htmlFor="endRepeat"
                 >
-                  Repeat
+                  End Repeat
                 </label>
-                <Select
-                  value={formData.repeat}
-                  onChange={handleSelectChange}
-                  displayEmpty
-                  inputProps={{ "aria-label": "Without label" }}
-                  style={{
-                    backgroundColor: "white",
-                    color: "#4A5568",
-                    fontWeight: "bold",
-                    width: "100%",
-                    height: "40px",
-                    borderRadius: "4px",
-                    fontSize: "14px",
-                    paddingRight: "30px", // Adjust padding for dropdown arrow
+                <TextField
+                  id="end-date-picker"
+                  type="date"
+                  value={
+                    formData.endDate
+                      ? format(formData.endDate, "yyyy-MM-dd")
+                      : ""
+                  }
+                  onChange={(e) =>
+                    handleDateChange("endDate")(
+                      e.target.value ? new Date(e.target.value) : null
+                    )
+                  }
+                  InputLabelProps={{
+                    shrink: true,
                   }}
-                  MenuProps={{
-                    PaperProps: {
-                      style: {
-                        backgroundColor: "white",
-                        color: "#4A5568",
-                      },
+                  placeholder="Select date"
+                  onFocus={(e) => {
+                    if (!formData.endDate) {
+                      e.target.value = getTodayDateString();
+                    }
+                  }}
+                  InputProps={{
+                    style: {
+                      backgroundColor: "white",
+                      color: "#4A5568",
+                      fontWeight: "bold",
+                      width: "100%",
+                      height: "40px",
+                      borderRadius: "4px",
+                      fontSize: "14px",
                     },
                   }}
-                  renderValue={(selected) => (
-                    <span style={{ color: selected ? "#4A5568" : "#BCCCDC" }}>
-                      {selected || "Select"}
-                    </span>
-                  )}
-                >
-                  <MenuItem value="no-repeat">Doesn&apos;t repeat</MenuItem>
-                  <MenuItem value="daily">Daily</MenuItem>
-                  <MenuItem value="weekly">
-                    Weekly on{" "}
-                    {getWeekdayFromDate(new Date(formData.startDate || today))}
-                  </MenuItem>
-                  <MenuItem value="monthly">
-                    Monthly on the third{" "}
-                    {getWeekdayFromDate(new Date(formData.startDate || today))}
-                  </MenuItem>
-                  <MenuItem value="annually">
-                    Annually on{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      month: "long",
-                      day: "numeric",
-                    }).format(new Date(formData.startDate || today))}
-                  </MenuItem>
-                  <MenuItem value="every-weekday">Weekly on Weekdays</MenuItem>
-                </Select>
+                  variant="outlined"
+                />
               </div>
+            )}
+          </div>
+        </LabelInputContainer>
 
-              {formData.repeat !== "no-repeat" && (
-                <div className="w-1/2">
-                  <label
-                    className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    htmlFor="endRepeat"
-                  >
-                    End Repeat
-                  </label>
-                  <TextField
-                    id="end-date-picker"
-                    type="date"
-                    value={
-                      formData.endDate
-                        ? format(formData.endDate, "yyyy-MM-dd")
-                        : ""
-                    }
-                    onChange={(e) =>
-                      handleDateChange("endDate")(
-                        e.target.value ? new Date(e.target.value) : null
-                      )
-                    }
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                    placeholder="Select date"
-                    onFocus={(e) => {
-                      if (!formData.endDate) {
-                        e.target.value = getTodayDateString();
-                      }
-                    }}
-                    InputProps={{
-                      style: {
-                        backgroundColor: "white",
-                        color: "#4A5568",
-                        fontWeight: "bold",
-                        width: "100%",
-                        height: "40px",
-                        borderRadius: "4px",
-                        fontSize: "14px",
-                      },
-                    }}
-                    variant="outlined"
-                  />
-                </div>
-              )}
-            </div>
-          </LabelInputContainer>
-
-          <Tooltip title={"Generate Google Calendar link"} arrow>
-            <button
-              className="cool-button bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 mt-3 relative block w-full text-white rounded-md h-12 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-600 hover:to-blue-800"
-              type="submit"
-            >
-              Generate Link &rarr;
-            </button>
-          </Tooltip>
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent h-[1px] w-full" />
-        </div>
+        {/* {!formData.allDay && (
+              <div className="flex justify-between item-center gap-3">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <div className="w-[50%]">
+                    <label className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Start Time
+                    </label>
+                    <TimePicker
+                      value={formData.startTime as Date | null}
+                      onChange={handleTimeChange("startTime")}
+                    />
+                  </div>
+                  <div className="w-[50%]">
+                    <label className="text-sm font-medium mt-1 text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      End Time
+                    </label>
+                    <TimePicker
+                      value={formData.endTime as Date | null}
+                      onChange={handleTimeChange("endTime")}
+                    />
+                  </div>
+                </LocalizationProvider>
+              </div>
+            )} */}
+        <Tooltip title={"Generate Google Calendar link"} arrow>
+          <button
+            className="cool-button bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 mt-3 relative block w-full text-white rounded-md h-12 font-semibold shadow-lg transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-600 hover:to-blue-800"
+            type="submit"
+          >
+            Generate Link &rarr;
+          </button>
+        </Tooltip>
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent h-[1px] w-full" />
       </form>
     </div>
   );
