2dfc87a91271025d00c83d5b179e9522cc9d6363
ChandraShekhar <csyadav0513@gmail.com>
2024-06-17 22:00:39 +0530

    Refactor handleSubmit

--- portal/components/screens/GoogleCalendar/GoogleCalendarCard.tsx
+++ portal/components/screens/GoogleCalendar/GoogleCalendarCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "@/app/lib/utils";
+
 import Image from "next/image";
 
 import { useEffect, useState } from "react";
@@ -8,8 +8,16 @@ import { format } from "date-fns";
 import Tooltip from "@mui/material/Tooltip";
 
 import { MenuItem, TextField, Select, Checkbox } from "@mui/material";
+import LabelInputContainer from "./LabelInputContainer";
+import {
+  validateForm,
+  buildGoogleCalendarURL,
+  formatDates,
+  generateRecurrenceRule,
+  FormDataType,
+} from "./GoogleCalendarUtils";
 
-interface FormData {
+export interface FormData {
   title: string;
   details: string;
   location: string;
@@ -25,7 +33,7 @@ interface FormData {
 const today = new Date();
 
 const GoogleCalendaCard: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
+  const [formData, setFormData] = useState<FormDataType>({
     title: "",
     details: "",
     location: "",
@@ -39,8 +47,8 @@ const GoogleCalendaCard: React.FC = () => {
   });
 
   const [formValidations, setFormValidations] = useState({
-    title: undefined as boolean | undefined,
-    startDate: undefined as boolean | undefined,
+    title: false,
+    startDate: false,
   });
   console.log("formValidations" + formValidations);
 
@@ -64,154 +72,33 @@ const GoogleCalendaCard: React.FC = () => {
     });
   }, [formData.title, formData.startDate]);
 
+  //! 📝 Handle form submission
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // Check if either title or startDate is empty
-    if (!formData.title || !formData.startDate) {
-      setFormValidations({
-        title: !formData.title,
-        startDate: !formData.startDate,
-      });
+    // Validate the form data 📝
+    const validations = validateForm(formData);
+    if (validations.title || validations.startDate) {
+      setFormValidations(validations);
       return;
     }
 
-    let formattedStartDate: string, formattedEndDate: string;
+    // Format the start and end dates 📅
+    const { startDate, endDate } = formatDates(formData);
 
-    if (formData.allDay) {
-      const startDate = new Date(formData.startDate);
-      startDate.setDate(startDate.getDate() + 1); // Add one day to correct the date issue
-      const startDateStr = startDate
-        .toISOString()
-        .split("T")[0]
-        .replace(/-/g, "");
-      formattedStartDate = startDateStr;
-      formattedEndDate = startDateStr; // For all-day events, start and end date are the same
-      console.log("All Day Event: ", formattedStartDate);
-    } else {
-      const startDate = new Date(formData.startDate);
-      startDate.setDate(startDate.getDate());
-      if (formData.startTime) {
-        if (typeof formData.startTime === "string") {
-          const [hours, minutes] = formData.startTime.split(":").map(Number);
-          startDate.setHours(hours, minutes);
-        } else if (formData.startTime instanceof Date) {
-          startDate.setHours(
-            formData.startTime.getHours(),
-            formData.startTime.getMinutes()
-          );
-        }
-      }
+    // Generate the recurrence rule 🔄
+    const recurrence = generateRecurrenceRule(formData);
 
-      formattedStartDate =
-        startDate.toISOString().replace(/[-:.]/g, "").split(".")[0] + "Z"; // Remove milliseconds and add Z for UTC
+    // Build the Google Calendar URL 🔗
+    const googleCalendarLink = buildGoogleCalendarURL(
+      formData,
+      startDate,
+      endDate,
+      recurrence
+    );
 
-      // Calculate the end date based on the end date and time if provided, otherwise default to 2 hours from the start time
-      const endDate = formData.endDate
-        ? new Date(formData.endDate)
-        : new Date(startDate);
-      if (formData.endDate) {
-        endDate.setDate(endDate.getDate());
-      }
-      if (formData.endTime) {
-        if (typeof formData.endTime === "string") {
-          const [hours, minutes] = formData.endTime.split(":").map(Number);
-          endDate.setHours(hours, minutes);
-        } else if (formData.endTime instanceof Date) {
-          endDate.setHours(
-            formData.endTime.getHours(),
-            formData.endTime.getMinutes()
-          );
-        }
-      } else {
-        // Default duration is 2 hours if end time is not provided
-        endDate.setTime(startDate.getTime() + 2 * 60 * 60 * 1000); // Add 2 hours
-      }
-
-      formattedEndDate =
-        endDate.toISOString().replace(/[-:.]/g, "").split(".")[0] + "Z"; // Remove milliseconds and add Z for UTC
-
-      console.log("Timed Event: ", formattedStartDate);
-    }
-
-    console.log("Start Date: ", formData.startDate);
-    console.log("Formatted Start Date: ", formattedStartDate);
-    console.log("Formatted End Date: ", formattedEndDate);
-
-    let recurrence = "";
-    if (formData.repeat !== "no-repeat") {
-      let freq = "";
-      let until = "";
-      if (formData.endRepeat) {
-        const endRepeat = new Date(formData.endRepeat);
-        endRepeat.setHours(23, 59, 59); // Set endRepeat to the end of the day
-
-        // Adjust the end repeat date to account for timezone differences
-        const endOffset = endRepeat.getTimezoneOffset();
-        endRepeat.setMinutes(endRepeat.getMinutes() - endOffset);
-
-        until = `;UNTIL=${endRepeat
-          .toISOString()
-          .replace(/[-:]/g, "")
-          .slice(0, -5)}Z`;
-      }
-
-      switch (formData.repeat) {
-        case "daily":
-          freq = "DAILY";
-          break;
-        case "weekly":
-          freq = "WEEKLY";
-          recurrence = `RRULE:FREQ=${freq};BYDAY=MO${until}`;
-          break;
-        case "monthly":
-          freq = "MONTHLY";
-          recurrence = `RRULE:FREQ=${freq};BYDAY=+3MO${until}`;
-          break;
-        case "annually":
-          freq = "YEARLY";
-          const startDate = formData.startDate
-            ? new Date(formData.startDate)
-            : new Date();
-          recurrence = `RRULE:FREQ=${freq};BYMONTH=${
-            startDate.getMonth() + 1
-          };BYMONTHDAY=${startDate.getDate()}${until}`;
-          break;
-        case "every-weekday":
-          freq = "WEEKLY";
-          recurrence = `RRULE:FREQ=${freq};BYDAY=MO,TU,WE,TH,FR${until}`;
-          break;
-        default:
-          freq = formData.repeat.toUpperCase();
-      }
-
-      if (!recurrence) {
-        recurrence = `RRULE:FREQ=${freq}${until}`;
-      }
-    }
-
-    // Build the Google Calendar URL with only non-empty fields 🔗
-    let googleCalendarLink = `https://calendar.google.com/calendar/u/0/r/eventedit?text=${encodeURIComponent(
-      formData.title
-    )}&dates=${formattedStartDate}/${formattedEndDate}`;
-
-    if (formData.details) {
-      googleCalendarLink += `&details=${encodeURIComponent(formData.details)}`;
-    }
-    if (formData.location) {
-      googleCalendarLink += `&location=${encodeURIComponent(
-        formData.location
-      )}`;
-    }
-    if (recurrence) {
-      googleCalendarLink += `&recur=${encodeURIComponent(recurrence)}`;
-    }
-
-    setFormValidations({
-      title: false,
-      startDate: false,
-    });
-
+    // Reset the form after submission 🔄
+    setFormValidations({ title: false, startDate: false });
     setFormData({
       title: "",
       details: "",
@@ -225,6 +112,7 @@ const GoogleCalendaCard: React.FC = () => {
       endDate: null,
     });
 
+    // Open the Google Calendar event in a new tab 🌐
     window.open(googleCalendarLink, "_blank");
   };
 
@@ -382,7 +270,7 @@ const GoogleCalendaCard: React.FC = () => {
             placeholder="Add Location"
             type="text"
             className="flex h-10 w-full bg-gray-100 font-bold text-gray-700 shadow-input rounded-md px-3 py-2 text-sm placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 transition duration-400"
-            value={formData.location}
+            value={formData.location ?? ""}
             onChange={handleInputChange}
           />
         </LabelInputContainer>
@@ -571,7 +459,7 @@ const GoogleCalendaCard: React.FC = () => {
                 id="end-time"
                 name="end-time"
                 type="time"
-                value={formData.endTime || null}
+                value={formData.endTime ?? ""}
                 onChange={(e) => handleTimeChange("endTime")(e.target.value)}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
@@ -593,17 +481,3 @@ const GoogleCalendaCard: React.FC = () => {
   );
 };
 export default GoogleCalendaCard;
-
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2  w-full", className)}>
-      {children}
-    </div>
-  );
-};
---
+++ portal/components/screens/GoogleCalendar/LabelInputContainer.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/app/lib/utils";
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+
+export default LabelInputContainer;
