41b3c7a6d9ecc3fd21151e61e83d88eb826f649e
shreyas0924 <shreyasp0924@gmail.com>
2024-06-18 15:48:22 +0530

    fix: updated metrics card Ui

--- portal/components/screens/Worklogs/WorklogBreakdown/MetricCard.tsx
+++ portal/components/screens/Worklogs/WorklogBreakdown/MetricCard.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Card, CardContent, Typography } from "@mui/material";
 
 interface MetricCardProps {
   title: string;
   content: React.ReactNode;
+  logo: React.ReactNode;
 }
 
-const MetricCard = ({ title, content }: MetricCardProps) => (
-  <Card className=" flex flex-col justify-between w-[45%] p-2 shadow-md hover:shadow-lg cursor-pointer rounded-xl border border-neutral-200">
-    <CardContent>
-      <h1 className="font-semibold mb-3">{title}</h1>
-      {content}
-    </CardContent>
-  </Card>
+const MetricCard = ({ title, content, logo }: MetricCardProps) => (
+  <div className="flex flex-col justify-between w-[45%] p-4 shadow-md hover:shadow-lg cursor-pointer rounded-xl border border-neutral-400  ">
+    <div>
+      <div className="flex justify-between">
+        <h1 className="font-semibold mb-3 text-xl">{title}</h1>
+        {logo}
+      </div>
+      <div className="text-base">{content}</div>
+    </div>
+  </div>
 );
 
 export default MetricCard;
