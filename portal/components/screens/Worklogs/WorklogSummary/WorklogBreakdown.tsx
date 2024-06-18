38c6f73c8a8562accb3fa8f66c6c209a039bde63
shreyas0924 <shreyasp0924@gmail.com>
2024-06-18 15:48:41 +0530

    fix: added bar chart for weekday review

--- portal/components/screens/Worklogs/WorklogSummary/WorklogBreakdown.tsx
+++ portal/components/screens/Worklogs/WorklogSummary/WorklogBreakdown.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { WorkLogs } from "@prisma/client";
-import { PieChart } from "@mui/x-charts/PieChart";
+import { PieChart, BarChart } from "@mui/x-charts";
 import { Stack, Typography } from "@mui/material";
 import { format, parseISO } from "date-fns";
 import { calculateMetrics } from "../WorklogBreakdown/BreakdownMetrics";
 import MetricCard from "../WorklogBreakdown/MetricCard";
+import {
+  AudioLines,
+  CircleCheckBig,
+  ListTodo,
+  SquareGanttChart,
+  CalendarCheck,
+  CircleArrowUp,
+} from "lucide-react";
 
 interface WorklogBreakdownProps {
   worklogSummary: WorkLogs[];
@@ -15,14 +23,26 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
 }) => {
   const metrics = calculateMetrics(worklogSummary);
 
+  const weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const completedTasksData = weekdays.map(
+    (day) => metrics.completedTasksByWeekday[day] || 0
+  );
+
   return (
     <main className="flex flex-col justify-center gap-5 m-6">
       <h1 className="font-semibold text-xl font-sans justify-start">
         Worklog Breakdown
       </h1>
-      <Stack spacing={4} alignItems="center">
+      <Stack spacing={2} alignItems="center">
         <PieChart
-          colors={["blue", "green"]}
+          colors={["blue", "#22c55e", "red", "orange", "purple"]}
           series={[
             {
               arcLabel: (item) => `${item.label} (${item.value})`,
@@ -30,7 +50,7 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
               data: [
                 {
                   label: "In Progress",
-                  value: metrics.totalTasks - metrics.completedTasks,
+                  value: metrics.inProgressTasks,
                   id: 0,
                 },
                 {
@@ -38,16 +58,51 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                   value: metrics.completedTasks,
                   id: 1,
                 },
+                {
+                  label: "Failed",
+                  value: metrics.failedTasks,
+                  id: 2,
+                },
+                {
+                  label: "Blocked",
+                  value: metrics.blockedTasks,
+                  id: 3,
+                },
+                {
+                  label: "Scheduled",
+                  value: metrics.scheduledTasks,
+                  id: 4,
+                },
               ],
             },
           ]}
           width={450}
           height={300}
         />
-
         {/* Display total tasks */}
         <Typography variant="body1">
           <strong>Total Tasks:</strong> {metrics.totalTasks}
+        </Typography>
+        <BarChart
+          xAxis={[
+            {
+              scaleType: "band",
+              data: weekdays,
+              label: "Days", // Adding title for x-axis
+            },
+          ]}
+          yAxis={[
+            {
+              label: "Tasks", // Adding title for y-axis
+            },
+          ]}
+          series={[{ data: completedTasksData, color: "#374151" }]}
+          width={550}
+          height={300}
+        />
+
+        <Typography variant="body1" className="pb-5">
+          <strong>Tasks by Weekday</strong>
         </Typography>
 
         {/* Additional metrics */}
@@ -59,6 +114,7 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                 Updated Days: {metrics.updateMetrics.updatedDays}
               </Typography>
             }
+            logo={<AudioLines color="#31c449" size={20} />}
           />
           <MetricCard
             title="Task Completion Rate"
@@ -67,6 +123,7 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                 Completion Rate: {metrics.taskCompletionRate.toFixed(2)}%
               </Typography>
             }
+            logo={<CircleCheckBig color="#31c449" size={20} />}
           />
           <MetricCard
             title="Average Tasks Per Day"
@@ -75,6 +132,7 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                 Avg. Tasks/Day: {metrics.averageTasksPerDay.toFixed(2)}
               </Typography>
             }
+            logo={<ListTodo color="#31c449" size={20} />}
           />
           <MetricCard
             title="Longest Productive Streak"
@@ -83,6 +141,7 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                 Longest Streak: {metrics.longestProductiveStreak} days
               </Typography>
             }
+            logo={<SquareGanttChart color="#31c449" size={20} />}
           />
           <MetricCard
             title="Top Productive Days"
@@ -92,6 +151,16 @@ const WorklogBreakdown: React.FC<WorklogBreakdownProps> = ({
                 {day.completedTasks} tasks completed
               </Typography>
             ))}
+            logo={<CalendarCheck color="#31c449" size={20} />}
+          />
+          <MetricCard
+            title="High Priority Tasks"
+            content={
+              <Typography variant="body1">
+                High Priority Tasks: {metrics.highPriorityTasks}
+              </Typography>
+            }
+            logo={<CircleArrowUp color="#31c449" size={20} />}
           />
         </div>
       </Stack>
