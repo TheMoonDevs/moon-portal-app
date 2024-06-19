2f0ea0264a6f664989c04cff81fab2160a0af25e
Manish Guhe <manishguhe301@gmail.com>
2024-06-19 21:23:03 +0530

    - removed currencymodal component - added popover to select currency - replaced currencymodal with popover

---
+++ payzone/components/global/CurrencySelectPopover.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { List, ListItem, ListItemText, Popover } from '@mui/material';
+import { useSyncBalances } from '@/utils/hooks/useSyncBalances';
+
+interface CustomPopoverProps {
+  popoverProps: {
+    id: string | undefined;
+    open: boolean;
+    anchorEl: HTMLButtonElement | null;
+    onClose: () => void;
+  };
+  handleCurrencySelect: (currency: string, value: number) => void;
+}
+
+const CurrencySelectPopover: React.FC<CustomPopoverProps> = ({
+  popoverProps,
+  handleCurrencySelect,
+}) => {
+  const { id, open, anchorEl, onClose } = popoverProps;
+  const { exchange } = useSyncBalances();
+  return (
+    <Popover
+      id={id}
+      open={open}
+      anchorEl={anchorEl}
+      onClose={onClose}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+    >
+      <List
+        sx={{
+          width: '100%',
+          maxWidth: 360,
+          bgcolor: 'background.paper',
+          maxHeight: '200px',
+          overflow: 'auto',
+        }}
+      >
+        {exchange &&
+          Object.entries(exchange.exchangeCurrency).map(([currency, value]) => (
+            <ListItem
+              key={currency}
+              disablePadding
+              onClick={() => handleCurrencySelect(currency, value as number)}
+              sx={{ padding: '0.5rem', cursor: 'pointer' }}
+            >
+              <ListItemText
+                primary={`1 TMD === ${(
+                  (Number(value) *
+                    (exchange?.exchangeData?.creditsRateINR ?? 0)) /
+                  exchange.exchangeCurrency.INR
+                ).toFixed(2)} ${currency}`}
+              />
+            </ListItem>
+          ))}
+      </List>
+    </Popover>
+  );
+};
+
+export default CurrencySelectPopover;
