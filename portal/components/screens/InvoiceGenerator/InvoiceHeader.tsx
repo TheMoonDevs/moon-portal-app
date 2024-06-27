3417edc7dca6ff7c8c0e7b48ca0f53d8028d800e
ChandraShekhar <csyadav0513@gmail.com>
2024-06-27 13:40:28 +0530

    Improve InvoiceHeader

--- portal/components/screens/InvoiceGenerator/InvoiceHeader.tsx
+++ portal/components/screens/InvoiceGenerator/InvoiceHeader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const InvoiceHeader = () => {
   return (
     <header>
-      <div className="fixed left-0 right-0 top-0 z-10 bg-white flex flex-row gap-3 py-2 px-3 items-center justify-between border-b border-neutral-300 shadow-md md:pl-[6rem] h-14">
+      <div className="fixed left-0 right-0 top-0 z-10 bg-white flex flex-row gap-3 py-2 px-3 items-center justify-between border-b border-neutral-300 shadow-md md:pl-[1rem] h-14">
         <div className="flex items-center justify-between w-full sm:w-auto">
           <Link href={APP_ROUTES.home} className="flex items-center">
             <Image
@@ -15,7 +15,7 @@ const InvoiceHeader = () => {
               alt="logo"
               className="w-8 aspect-square mr-2"
             />
-            <h1 className="hidden sm:block md:text-lg text-sm whitespace-nowrap cursor-pointer font-extrabold pr-3 mr-3 hover:text-gray-800 transition-colors duration-300">
+            <h1 className="md:text-lg text-sm whitespace-nowrap cursor-pointer font-extrabold pr-3 mr-3 hover:text-gray-800 transition-colors duration-300">
               The Moon Devs
             </h1>
           </Link>
