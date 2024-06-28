c3824c8315dc26ec2ad15d97ca02d35db5d75bff
ChandraShekhar <csyadav0513@gmail.com>
2024-06-28 15:41:40 +0530

    Refactor page layout and components

--- portal/app/admin/invoice-generator/page.tsx
+++ portal/app/admin/invoice-generator/page.tsx
@@ -1,17 +1,11 @@
 import { PageAccess } from "@/components/global/PageAccess";
-import InvoiceHeader from "@/components/screens/InvoiceGenerator/InvoiceHeader";
 import InvoicePage from "@/components/screens/InvoiceGenerator/InvoicePage";
 
 const InvoiceGenerator = () => {
   return (
     <PageAccess isAuthRequired={true} isAdminRequired={false}>
       <div className="h-screen w-full overflow-hidden">
-        <header className="fixed top-0 left-0 w-full z-10">
-          <InvoiceHeader />
-        </header>
-        <main className="flex flex-col md:flex-row h-full mt-14 overflow-hidden">
-          <InvoicePage />
-        </main>
+        <InvoicePage />
       </div>
     </PageAccess>
   );
