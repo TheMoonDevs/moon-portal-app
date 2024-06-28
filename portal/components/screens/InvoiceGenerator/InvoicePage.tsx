226e208a7cdb1109e8cc63eda34fd62608ac4c0f
ChandraShekhar <csyadav0513@gmail.com>
2024-06-28 12:25:59 +0530

    Make InvoicePage Responsive

--- portal/components/screens/InvoiceGenerator/InvoicePage.tsx
+++ portal/components/screens/InvoiceGenerator/InvoicePage.tsx
@@ -1,16 +1,41 @@
+"use client";
+import { useState } from "react";
 import Invoice from "./Invoice";
-import InvoiceModel from "./InvoiceModel";
+import { Button } from "@mui/material";
+
+import InvoiceModal from "./InvoiceModel";
+import { Edit, X } from "lucide-react";
 
 const InvoicePage = () => {
+  const [showInput, setShowInput] = useState(false);
+
+  const toggleInput = () => {
+    setShowInput(!showInput);
+  };
+
   return (
-    <>
-      <section className="w-full md:w-1/2 p-4 border overflow-y-auto  mb-14 flex-1">
+    <div className="flex flex-col md:flex-row w-full  h-full ">
+      <div className="block md:hidden ">{showInput && <InvoiceModal />}</div>
+      <section className="w-full md:w-1/2 p-4">
+        <div className="hidden md:block overflow-y-auto mb-10     rounded-lg">
+          <InvoiceModal />
+        </div>
+      </section>
+      <section className="w-full md:w-1/2 p-4 border-dashed border mb-14  overflow-y-auto  shadow-lg rounded-lg">
         <Invoice />
       </section>
-      <section className="w-full md:w-1/2 p-4 flex-1 overflow-y-auto">
-        <InvoiceModel />
-      </section>
-    </>
+      <div className="block md:hidden fixed top-2 right-3 z-10">
+        <div
+          className={
+            "p-1 rounded-full cursor-pointer transition-all duration-300 ease-in-out  "
+          }
+          onClick={toggleInput}
+        >
+          {showInput ? <X color="red" /> : <Edit color="gray" />}
+        </div>
+      </div>
+    </div>
   );
 };
+
 export default InvoicePage;
