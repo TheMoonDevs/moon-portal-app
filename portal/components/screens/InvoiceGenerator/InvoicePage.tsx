d564a86faefdceb03121df9c28a076f17e6df1aa
ChandraShekhar <csyadav0513@gmail.com>
2024-06-28 15:44:41 +0530

    Improve UI and add pdfTargetRef

--- portal/components/screens/InvoiceGenerator/InvoicePage.tsx
+++ portal/components/screens/InvoiceGenerator/InvoicePage.tsx
@@ -1,40 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Invoice from "./Invoice";
-import { Button } from "@mui/material";
-
 import InvoiceModal from "./InvoiceModel";
-import { Edit, X } from "lucide-react";
+import InvoiceHeader from "./InvoiceHeader";
 
 const InvoicePage = () => {
   const [showInput, setShowInput] = useState(false);
+  const pdfTargetRef = useRef<HTMLElement | any>(null);
 
   const toggleInput = () => {
     setShowInput(!showInput);
   };
 
   return (
-    <div className="flex flex-col md:flex-row w-full  h-full ">
-      <div className="block md:hidden ">{showInput && <InvoiceModal />}</div>
-      <section className="w-full md:w-1/2 p-4">
-        <div className="hidden md:block overflow-y-auto mb-10     rounded-lg">
-          <InvoiceModal />
+    <>
+      <header className="w-full">
+        <InvoiceHeader
+          pdfTargetRef={pdfTargetRef}
+          toggleInput={toggleInput}
+          showInput={showInput}
+        />
+      </header>
+      <main className="flex flex-col md:flex-row h-full overflow-hidden">
+        <div className="flex flex-col md:flex-row w-full h-full">
+          <div className="block md:hidden">{showInput && <InvoiceModal />}</div>
+          <section className="w-full md:w-1/2 p-4">
+            <div className="hidden md:block overflow-y-auto mb-10 rounded-lg">
+              <InvoiceModal />
+            </div>
+          </section>
+          <section className="w-full md:w-1/2 p-4 border-dashed border mb-14 overflow-y-auto shadow-lg rounded-lg">
+            <Invoice pdfTargetRef={pdfTargetRef} />
+          </section>
         </div>
-      </section>
-      <section className="w-full md:w-1/2 p-4 border-dashed border mb-14  overflow-y-auto  shadow-lg rounded-lg">
-        <Invoice />
-      </section>
-      <div className="block md:hidden fixed top-2 right-3 z-10">
-        <div
-          className={
-            "p-1 rounded-full cursor-pointer transition-all duration-300 ease-in-out  "
-          }
-          onClick={toggleInput}
-        >
-          {showInput ? <X color="red" /> : <Edit color="gray" />}
-        </div>
-      </div>
-    </div>
+      </main>
+    </>
   );
 };
 
