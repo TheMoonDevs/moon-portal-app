cc428f74eb5b021501a4196608cbe450572683b5
ChandraShekhar <csyadav0513@gmail.com>
2024-06-28 15:44:15 +0530

    Improve UI and Add pdfTargetRef

--- portal/components/screens/InvoiceGenerator/Invoice.tsx
+++ portal/components/screens/InvoiceGenerator/Invoice.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
-const Invoice = () => {
+interface InvoiceProps {
+  pdfTargetRef: React.MutableRefObject<HTMLElement | any>;
+}
+
+const Invoice: React.FC<InvoiceProps> = ({ pdfTargetRef }) => {
   return (
-    <section className="md:h-screen">
-      <div className="p-8 bg-white shadow-md">
-        {/* Header Section */}
+    <section>
+      <div className="p-8 bg-white shadow-md" ref={pdfTargetRef}>
         <div className="flex justify-between items-center mb-8">
           <div className="flex items-center">
             <Image
@@ -15,12 +18,11 @@ const Invoice = () => {
               className="aspect-square mr-2"
             />
           </div>
-          <h1 className="text-4xl  md:text-5xl font-normal font-serif">
+          <h1 className="text-4xl md:text-5xl font-normal font-serif">
             INVOICE
           </h1>
         </div>
 
-        {/* Billed To Section */}
         <div className="flex justify-between mb-8">
           <div>
             <h2 className="text-sm font-bold">BILLED TO:</h2>
@@ -36,7 +38,6 @@ const Invoice = () => {
           </div>
         </div>
 
-        {/* Invoice Table */}
         <div className="overflow-x-auto">
           <table className="w-full mb-8">
             <thead>
@@ -72,7 +73,7 @@ const Invoice = () => {
                   colSpan={3}
                   className="p-2 font-bold text-xl md:text-2xl text-right relative"
                 >
-                  <div className="absolute top-0 right-0 w-20 sm:w-24 md:w-32  border-t-2 border-gray-700"></div>
+                  <div className="absolute top-0 right-0 w-20 sm:w-24 md:w-32 border-t-2 border-gray-700"></div>
                   Total
                 </td>
                 <td className="p-2 font-bold text-xl md:text-2xl relative">
@@ -84,13 +85,11 @@ const Invoice = () => {
           </table>
         </div>
 
-        {/* Payment Due Date */}
         <p className="text-sm mb-8">
           Please finish the payment by the due date: 31st March 2024
         </p>
-        <p className="text-3xl  mb-8">Thank you!</p>
+        <p className="text-3xl mb-8">Thank you!</p>
 
-        {/* Payment Information */}
         <div className="flex flex-col sm:flex-row">
           <div className="md:w-1/2 md:pr-8">
             <h2 className="text-sm font-bold mb-2">PAYMENT INFORMATION</h2>
@@ -107,7 +106,7 @@ const Invoice = () => {
               IFSC - UBIN0836531
             </p>
           </div>
-          <div className="md:w-1/2 mt-8 md:mt-0 sm:text-right flex justify-end  flex-col">
+          <div className="md:w-1/2 mt-8 md:mt-0 w-full sm:text-right flex justify-end  flex-col">
             <p className="text-xl font-normal sm:text-[22px] md:text-[25px] font-serif">
               Subhakar Tikkireddy
             </p>
