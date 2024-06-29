e1c1aad907714ad4bf695387f72931c483550d68
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-06-29 10:34:02 +0530

    updating spreadsheet data from admin changes

--- portal/app/api/user/route.ts
+++ portal/app/api/user/route.ts
@@ -1,8 +1,16 @@
 import { prisma } from "@/prisma/prisma";
+import GoogleSheetsAPI from "@/utils/services/googleSheetSdk";
 import { SlackBotSdk } from "@/utils/services/slackBotSdk";
 import { USERROLE, USERSTATUS, USERTYPE } from "@prisma/client";
 import { JsonObject } from "@prisma/client/runtime/library";
 import { NextResponse, NextRequest } from "next/server";
+
+const sheetConfig = {
+  clientEmail: process.env.GIAM_CLIENT_EMAIL || "",
+  privateKey: process.env.GIAM_PRIVATE_KEY || "",
+};
+
+const sheetSDK = new GoogleSheetsAPI(sheetConfig);
 
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.searchParams.get("id") as string;
@@ -32,23 +40,26 @@ export async function GET(request: NextRequest) {
             !(userData.thirdPartyData as JsonObject).slackData
           ) {
             const slackUser = allSlackUsers.find(
-              (slackUser:any) => slackUser?.profile?.email === userData?.email
+              (slackUser: any) => slackUser?.profile?.email === userData?.email
             );
             console.log(slackUser?.profile?.email, userData?.email);
-    
+
             if (slackUser) {
               const newThirdPartyData = {
-                ...userData.thirdPartyData as JsonObject,
+                ...(userData.thirdPartyData as JsonObject),
                 slackData: slackUser,
               };
-    
+
               return prisma.user.update({
                 where: { id: userData.id },
-                data: { thirdPartyData: newThirdPartyData, slackId: slackUser.id},
+                data: {
+                  thirdPartyData: newThirdPartyData,
+                  slackId: slackUser.id,
+                },
               });
             }
           }
-        } catch (error:any) {
+        } catch (error: any) {
           console.error(`Error updating user ${userData.id}: ${error.message}`);
         }
       })
@@ -80,13 +91,43 @@ export async function GET(request: NextRequest) {
 export async function POST(request: Request) {
   try {
     const { id, ...rest } = await request.json();
-
     const user = await prisma.user.create({
       data: {
         ...rest,
       },
     });
 
+    const currentDate = new Date()
+      .toLocaleDateString("en-GB")
+      .split("/")
+      .reverse()
+      .join("-");
+
+    const sheetData = [
+      `${rest.username}${rest.password}`,
+      rest.name,
+      rest.email,
+      rest.personalData.phone,
+      rest.payData.upiId,
+      "=(YEAR(NOW())-YEAR(G14))",
+      rest.personalData.dateOfBirth,
+      rest.vertical,
+      rest.personalData.city,
+      rest.workData.positionInternal,
+      rest.personalData.workHourOverlap,
+      rest.workData.workHours,
+      currentDate,
+      "---", //office email
+      rest.personalData.address,
+    ];
+
+    await sheetSDK.appendSheetData({
+      spreadsheetId: "1w2kCO6IIYHi7YJBqfeg9ytmiIDRGhOgGzUPb_0u-UZ0",
+      targetId: "0",
+      values: [sheetData],
+      range: "A:A",
+      majorDimension: "ROWS",
+    });
     let json_response = {
       status: "success",
       data: {
@@ -114,6 +155,64 @@ export async function PUT(request: Request) {
       create: { ...rest },
       update: { ...rest },
     });
+
+    const currentDate = new Date()
+      .toLocaleDateString("en-GB")
+      .split("/")
+      .reverse()
+      .join("-");
+
+    const sheetData = [
+      `${rest.username}${rest.password}`,
+      rest.name,
+      rest.email,
+      rest.personalData.phone,
+      rest.payData.upiId,
+      "=(YEAR(NOW())-YEAR(G14))",
+      rest.personalData.dateOfBirth,
+      rest.vertical,
+      rest.personalData.city,
+      rest.workData.positionInternal,
+      rest.personalData.workHourOverlap,
+      rest.workData.workHours,
+      currentDate,
+      "---", //office email
+      rest.personalData.address,
+    ];
+
+    const sheet = await sheetSDK.getSheetData({
+      spreadsheetId: "1w2kCO6IIYHi7YJBqfeg9ytmiIDRGhOgGzUPb_0u-UZ0",
+      targetId: "0",
+      range: "A:Z",
+      majorDimension: "ROWS",
+    });
+
+    let isSheetDataUpdated = false;
+    await Promise.all(
+      sheet.values.map(async (row: any, index: number) => {
+        console.log(row[0]);
+        if (row[0] === `${rest.username}${rest.password}`) {
+          await sheetSDK.updateSheetData({
+            spreadsheetId: "1w2kCO6IIYHi7YJBqfeg9ytmiIDRGhOgGzUPb_0u-UZ0",
+            targetId: "0",
+            values: [sheetData],
+            range: `A${index + 1}`,
+            majorDimension: "ROWS",
+          });
+          isSheetDataUpdated = true;
+        }
+      })
+    );
+
+    if (!isSheetDataUpdated) {
+      await sheetSDK.appendSheetData({
+        spreadsheetId: "1w2kCO6IIYHi7YJBqfeg9ytmiIDRGhOgGzUPb_0u-UZ0",
+        targetId: "0",
+        values: [sheetData],
+        range: "A:A",
+        majorDimension: "ROWS",
+      });
+    }
 
     let json_response = {
       status: "success",
