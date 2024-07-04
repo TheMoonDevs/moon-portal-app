5abc830ca9027a9f32932b17edc0ec298f827571
Manish Guhe <manishguhe301@gmail.com>
2024-07-04 15:45:16 +0530

    - added ui for send notifications - added api route for creating notification

--- portal/components/screens/Admin/AdminPage.tsx
+++ portal/components/screens/Admin/AdminPage.tsx
@@ -1,10 +1,12 @@
 import { MobileBox } from "../Login/Login";
 import { AdminUsers } from "./AdminUsers";
+import SendNotifications from "./SendNotifications";
 
 export const AdminPage = () => {
   return (
-    <div className="flex flex-row flex-wrap gap-4 items-center justify-center  bg-neutral-700 md:bg-neutral-900 h-screen overflow-y-hidden">
+    <div className="flex flex-row flex-wrap gap-4 items-center justify-center  bg-neutral-700 md:bg-neutral-900 h-screen overflow-y-hidden max-sm:flex-col max-sm:h-full max-sm:overflow-y-auto">
       <AdminUsers />
+      <SendNotifications/>
     </div>
   );
 };
