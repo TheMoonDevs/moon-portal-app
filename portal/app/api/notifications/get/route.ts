51e3769c22db40c96965a947090e121c7bda09fb
Manish Guhe <manishguhe301@gmail.com>
2024-07-04 18:09:35 +0530

    - added route for getting notifications - installed swr package

---
+++ portal/app/api/notifications/get/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/prisma/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const userId = request.nextUrl.searchParams.get("userId") as string;
+
+  try {
+    const notifications = await prisma.notification.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    const json_response = {
+      status: "success",
+      data: {
+        notifications,
+      },
+    };
+
+    return NextResponse.json(json_response);
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
+    return new NextResponse(JSON.stringify(error), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
