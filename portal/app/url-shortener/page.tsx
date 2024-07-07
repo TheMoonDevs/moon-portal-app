c5214c4d4fd48d99532d8bf27bb2542f46d0b24b
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-07-07 20:19:38 +0530

    fix: table width issues

--- portal/app/url-shortener/page.tsx
+++ portal/app/url-shortener/page.tsx
@@ -12,13 +12,16 @@ export default function URLShortnerPage() {
     <PageAccess isAuthRequired={true}>
       <Bottombar visible={true} />
       <div className=" h overflow-hidden ">
-        <div className="cursor-pointer rounded-lg pl-4 p-2 pt-3 flex items-center gap-2 text-neutral-900   ">
-          <h1 className=" tracking-[0.01em] text-3xl font-bold">
-            URL Shortener
+        <div className="rounded-lg pt-6 pl-6 flex items-center gap-2 text-neutral-900   ">
+          <h1 className=" tracking-[0.01em] text-3xl font-bold flex gap-4 items-center">
+            <span className="material-icons-outlined bg-neutral-100 p-2 rounded-full text-blue-500 -rotate-45 border border-neutral-200">
+              link
+            </span>{" "}
+            <span>URL Shortener</span>
           </h1>
         </div>
         <div className="flex items-start justify-center gap-6 pt-16">
-          <div className="flex flex-col gap-8 items-center justify-center w-full max-sm:flex-col-reverse max-sm:w-full">
+          <div className="flex flex-col gap-8 items-center justify-center w-full max-sm:w-full">
             <ShortUrlCard />
             <ShortUrlList />
           </div>
