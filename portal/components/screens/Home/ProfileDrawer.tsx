22bf1a19824a6f2ac71633860931901712962a07
Manish Guhe <manishguhe301@gmail.com>
2024-07-08 12:51:22 +0530

    minor changes

--- portal/components/screens/Home/ProfileDrawer.tsx
+++ portal/components/screens/Home/ProfileDrawer.tsx
@@ -2,7 +2,7 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Drawer, Box, Typography } from "@mui/material";
+import { Drawer, Box } from "@mui/material";
 import { closeSlideIn } from "@/utils/redux/userProfileDrawer/userProfileDrawer.slice";
 import { RootState } from "@/utils/redux/store";
 import { useUser } from "@/utils/hooks/useUser";
@@ -19,7 +19,6 @@ import { WorklogSummaryView } from "../Worklogs/WorklogSummary/WorklogSummaryVie
 import { PortalSdk } from "@/utils/services/PortalSdk";
 import useAsyncState from "@/utils/hooks/useAsyncState";
 import { LoadingSkeleton } from "@/components/elements/LoadingSkeleton";
-import Image from "next/image";
 import { APP_ROUTES } from "@/utils/constants/appInfo";
 
 interface LoggedInUser {
@@ -44,8 +43,7 @@ export const UserProfileDrawer: React.FC = () => {
     (state: RootState) => state.userProfileDrawer.selectedUser
   );
   const loggedinUser = useUser() as LoggedInUser;
-  const payData = JSON.stringify(loggedinUser.user.payData) as PayData;
-
+  const payData = loggedinUser?.user?.payData as PayData;
   const [worklogSummary, setWorklogSummary] = useState<WorkLogs[]>([]);
   const { loading, setLoading } = useAsyncState();
 
@@ -61,75 +59,33 @@ export const UserProfileDrawer: React.FC = () => {
         null
       );
       setWorklogSummary(response.data.workLogs);
-      setLoading(false);
     } catch (error) {
       console.error(error);
+    } finally {
       setLoading(false);
     }
   }, [setLoading, selectedUser?.id]);
 
   useEffect(() => {
-    fetchWorklogData();
-  }, [fetchWorklogData]);
+    if (isOpen && selectedUser) {
+      fetchWorklogData();
+    }
+  }, [fetchWorklogData, isOpen, selectedUser]);
 
   if (!selectedUser) return null;
 
-  // to translate the vertical of the user
-  function translateUserVertical(vertical: string): string {
-    const verticalMap: { [key in string]: string } = {
+  const translateUserVertical = (vertical: string): string => {
+    const verticalMap: { [key: string]: string } = {
       DEV: "Developer",
       DESIGN: "Designer",
       MARKETING: "Marketing",
       COMMUNITY: "Community Manager",
       FINANCE: "Finance Specialist",
-      LEGAL: "Finance Specialist",
+      LEGAL: "Legal Specialist",
       HR: "Human Resources",
       OPERATIONS: "Operations",
     };
     return verticalMap[vertical] || "Unknown Vertical";
-  }
-
-  // for slack redirect to conversation
-  const openSlackConversation = () => {
-    const slackUrl = `https://slack.com/app_redirect?channel=${selectedUser.slackId}`;
-    window.open(slackUrl, "_blank");
-  };
-
-  // for uploading the avatar
-  const UploadAvatar = async (file: FileWithPath) => {
-    const formData = new FormData();
-
-    formData.append("file", file, file.path);
-    if (loggedinUser) {
-      const userId = loggedinUser.user.id;
-      formData.append("userId", userId);
-    }
-    formData.append("folderName", "userAvatars");
-    try {
-      const response = await fetch("/api/upload/file-upload", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        console.log("File uploaded successfully!");
-        console.log("Uploaded file info:", data.fileInfo);
-
-        // Update the avatar URL with the file's URL
-        const avatarUrl = data.fileUrl;
-        console.log(avatarUrl);
-        dispatch(updateAvatarUrl(avatarUrl));
-        setUploadedFiles([data.fileInfo]);
-        dispatch(resetPreview());
-      } else {
-        console.error("Failed to upload file:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Error uploading file:", error);
-    } finally {
-    }
   };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -141,7 +97,6 @@ export const UserProfileDrawer: React.FC = () => {
     }
   };
 
-  // to trucenate the wallet address of the user
   const truncateAddress = (
     address: string | undefined,
     visibleChars: number = 4
@@ -151,6 +106,32 @@ export const UserProfileDrawer: React.FC = () => {
     return `${address.slice(0, visibleChars)}...${address.slice(
       -visibleChars
     )}`;
+  };
+
+  const UploadAvatar = async (file: FileWithPath) => {
+    const formData = new FormData();
+    formData.append("file", file, file.path);
+    if (loggedinUser) {
+      formData.append("userId", loggedinUser.user.id);
+    }
+    formData.append("folderName", "userAvatars");
+    try {
+      const response = await fetch("/api/upload/file-upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(updateAvatarUrl(data.fileUrl));
+        setUploadedFiles([data.fileInfo]);
+        dispatch(resetPreview());
+      } else {
+        console.error("Failed to upload file:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
 
   return (
@@ -164,36 +145,34 @@ export const UserProfileDrawer: React.FC = () => {
         }}
         role="presentation"
       >
-        <div className="h-[100px]  relative">
+        <div className="h-[120px] relative">
           <img
             src={selectedUser?.banner || "/images/gradientBanner.jpg"}
-            className="abolsute w-full h-full object-cover"
+            className="absolute w-full h-full object-cover"
             alt="Profile Banner"
           />
-          {loggedinUser.user.id === selectedUser.id && (
-            <Image
-              src="/icons/photoCamera.svg"
-              width={25}
-              height={25}
-              alt="Camera Icon"
-              className="absolute top-2 right-2 bg-white rounded-full p-[2px]"
-            />
+          {loggedinUser.user.id === selectedUser?.id && (
+            <span
+              className="material-symbols-outlined absolute top-2 right-2 bg-white rounded-full p-[6px] cursor-pointer"
+              style={{ fontSize: "16px" }}
+            >
+              add_a_photo 
+            </span>
           )}
-          <div className="rounded-full absolute -bottom-[3.25rem] left-5 border-4 w-24 h-24 border-white ">
+          <div className="rounded-full absolute -bottom-[3.25rem] left-5 border-4 w-24 h-24 border-white">
             <img
-              src={selectedUser.avatar || "/icons/placeholderAvatar.svg"}
-              alt={selectedUser.name || ""}
-              className="  object-center rounded-full w-full h-full bg-white"
+              src={selectedUser?.avatar || "/icons/placeholderAvatar.svg"}
+              alt={selectedUser?.name || ""}
+              className="object-center rounded-full w-full h-full bg-white"
             />
-            {loggedinUser.user.id === selectedUser.id && (
-              <label className="absolute top-2 -right-2 bg-white rounded-full p-[2px]">
-                <Image
-                  src="/icons/photoCamera.svg"
-                  width={25}
-                  height={25}
-                  alt="Camera Icon"
-                  className=" bg-white rounded-full p-[2px]"
-                />
+            {loggedinUser.user.id === selectedUser?.id && (
+              <label className="absolute top-2 -right-2 bg-white rounded-full p-[2px] flex items-center justify-center cursor-pointer">
+                <span
+                  className="material-symbols-outlined bg-white rounded-full p-[4px]"
+                  style={{ fontSize: "16px" }}
+                >
+                  add_a_photo
+                </span>
                 <input
                   type="file"
                   accept="image/jpeg,image/png"
@@ -204,131 +183,153 @@ export const UserProfileDrawer: React.FC = () => {
             )}
           </div>
         </div>
-        <div className="flex flex-col px-5  pb-2">
+        <div className="flex flex-col px-5 pb-2">
           <div className="flex pt-16 justify-between items-center">
-            <h3 className="font-bold text-2xl">{selectedUser.name}</h3>
-            <Typography variant="body2">{selectedUser.timezone}</Typography>
-            
+            <h3 className="font-bold text-2xl">{selectedUser?.name}</h3>
+            <p className="text-sm text-gray-500 font-semibold capitalize">
+              {selectedUser?.timezone}
+            </p>
           </div>
-          <p className="text-sm pt-1 pb-2">
-            @{selectedUser.username + selectedUser.password}
+          <p className="text-sm py-1">
+            @{selectedUser?.username + selectedUser?.password}{" "}
           </p>
-          <div className="flex gap-3 text-black font-bold text-xs py-1">
-            <div className="rounded-lg border-2 border-gray-300  px-1 py-1 ">
-              {translateUserVertical(selectedUser.vertical || "")}
+          <div className="flex gap-3 text-black font-bold text-xs py-2">
+            <div className="rounded-md border-2 border-gray-300 px-2 py-1 capitalize">
+              {translateUserVertical(selectedUser?.vertical || "")}
             </div>
-            <div className="rounded-lg border-2 border-gray-300 px-1 py-1 ">
-              {selectedUser.userType}
+            <div className="rounded-md border-2 border-gray-300 px-2 py-1 capitalize">
+              {selectedUser?.userType?.toLowerCase()}
             </div>
-            <div className="rounded-lg border-2 border-gray-300 px-1 py-1">
-              {selectedUser.role}
-            </div>{" "}
+            <div className="rounded-md border-2 border-gray-300 px-2 py-1 capitalize">
+              {selectedUser?.role?.toLowerCase()}
+            </div>
           </div>
           <div className="flex gap-4 py-2">
-            <button
-              onClick={openSlackConversation}
-              className="bg-black text-white px-2 py-2 rounded-xl flex gap-2 items-center"
+            <Link
+              href={`https://slack.com/app_redirect?channel=${selectedUser?.slackId}`}
+              target="_blank"
+              className="bg-black text-white px-3 py-2 rounded-lg text-sm flex gap-2 items-center hover:bg-gray-800 transition duration-300"
             >
-              <Image src="/icons/message.svg" width={20} height={20} alt="Slack" />
+              <span
+                className="material-symbols-outlined"
+                style={{ fontSize: "20px" }}
+              >
+                chat
+              </span>
               Message
-            </button>
-            {loggedinUser.user.id === selectedUser.id && (
-              <button className=" px-2 py-2  text-black border border-gray-300 rounded-xl flex gap-2">
-                <Image src={"/icons/edit.svg"} width={20} height={20} alt="Edit"/>
+            </Link>
+            {loggedinUser.user.id === selectedUser?.id && (
+              <button className="text-black border px-3 py-2 rounded-lg border-gray-300 flex items-center gap-2 hover:bg-gray-200 transition duration-300">
+                <span
+                  className="material-symbols-outlined"
+                  style={{ fontSize: "20px" }}
+                >
+                  edit_square
+                </span>
                 Edit Profile
               </button>
             )}
           </div>
-          <p className="text-sm py-2">
-            {selectedUser.description ||
+          <p className="text-sm py-4 ">
+            {selectedUser?.description ||
               "This is the Seleted user's description or bio that will be displayed here and every user will have a unique bio or description that will show off their personality and their work."}
           </p>
           {!loading ? (
             <div className="flex gap-1 flex-col">
-             <h6 className="font-bold">Worklogs</h6>
+              <h6 className="font-bold pb-2">Worklogs</h6>
               <div className="border-2 border-gray-300 rounded-xl h-[310px] relative p-3 overflow-y-hidden">
-                <WorklogSummaryView
-                  worklogSummary={worklogSummary.slice(0, 5)}
-                />
+                <div className="bottom-8 px-2 h-full overflow-hidden">
+                  <WorklogSummaryView
+                    worklogSummary={worklogSummary.slice(0, 5)}
+                  />
+                </div>
                 <Link
-                  href={`${APP_ROUTES.userWorklogSummary}/${selectedUser.id}`}
-                  className="absolute bottom-2 right-2 bg-white rounded-xl border-gray-300 border-2 p-1 text-sm"
+                  href={`${APP_ROUTES.userWorklogSummary}/${selectedUser?.id}`}
+                  className="absolute bottom-2 right-2 bg-white rounded-md border-gray-300 border-2 py-1 px-2 text-sm hover:bg-gray-200 transition duration-300"
                 >
                   View Full Summary
                 </Link>
               </div>
             </div>
           ) : (
-            <LoadingSkeleton />
+            <LoadingSkeleton /> 
           )}
-
-          <div className="pt-3">
-          <h6 className="font-bold">Engagements</h6>
+          <div className="py-4">
+            <h6 className="font-bold pb-2">Engagements</h6>
             <ul className="flex flex-col gap-3 pt-2">
-              <li className="flex items-center gap-3">
-                <div className="w-12 h-12 border-gray-300 border-2 rounded-xl flex font-bold items-center justify-center">
+              <li className="flex items-center gap-3 text-sm">
+                <div className="w-10 h-10 border-gray-300 border-2 rounded-xl flex font-bold items-center justify-center">
                   1
                 </div>
                 This is the First Engagement
               </li>
-              <li className="flex items-center gap-3">
-                <div className="w-12 h-12 border-gray-300 border-2 rounded-xl flex items-center justify-center font-bold">
+              <li className="flex items-center gap-3 text-sm">
+                <div className="w-10 h-10 border-gray-300 border-2 rounded-xl flex items-center justify-center font-bold">
                   2
                 </div>
                 This is the Second Engagement
               </li>
-              <li className="flex items-center gap-3">
-                <div className="w-12 h-12 border-gray-300 border-2 rounded-xl flex items-center justify-center font-bold">
+              <li className="flex items-center gap-3 text-sm">
+                <div className="w-10 h-10 border-gray-300 border-2 rounded-xl flex items-center justify-center font-bold">
                   3
                 </div>
                 This is the Third Engagement
               </li>
             </ul>
           </div>
-          <div className="pt-3">
-          <h6 className="font-bold">Missions/Task</h6>
+          <div className="pb-4">
+            <h6 className="font-bold pb-2">Missions/Task</h6>
             <ul className="flex flex-col gap-1 p-3 border-2 mt-1 border-gray-300 rounded-xl list-none">
-              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500">
+              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500 text-sm">
                 Task 1 or mission 1
               </li>
-              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500">
+              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500 text-sm">
                 Task 2 or mission 2
               </li>
-              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500">
+              <li className="flex items-center pl-5 relative before:content-['•'] before:absolute before:left-0 before:text-gray-500 text-sm">
                 Task 3 or mission 3
               </li>
             </ul>
           </div>
-          {loggedinUser.user.id === selectedUser.id && (
-            <div className="flex flex-col gap-1 pt-3">
-              <h6 className="font-bold">Payment Details</h6>
-              <div className="flex flex-col gap-2 border-2 border-gray-300 rounded-xl p-3">
-                <Typography
-                  variant="body2"
-                  sx={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  UPI ID: {payData.upiId || "No UPI Available"}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  Wallet Address:{" "}
-                  {truncateAddress(payData?.walletAddress) || "Not Available"}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  Pay Out: {payData.stipendAmount || "Not Available"}{" "}
-                  {payData.stipendCurrency || ""}
-                </Typography>
+          {loggedinUser.user.id === selectedUser?.id && (
+            <div className="flex flex-col gap-1 pb-4">
+              <h6 className="font-bold pb-2">Payment Details</h6>
+              <div className="flex flex-col gap-3 border-2 border-gray-300 rounded-xl p-4 shadow-lg bg-white">
+                <div className="flex items-center gap-2">
+                  <span className="material-symbols-outlined text-gray-600">
+                    account_balance_wallet
+                  </span>
+                  <p className="text-sm font-bold text-gray-700">
+                    UPI ID:{" "}
+                    <span className="text-gray-900">
+                      {payData.upiId || "No UPI Available"}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="material-symbols-outlined text-gray-600">
+                    account_balance
+                  </span>
+                  <p className="text-sm font-bold text-gray-700">
+                    Wallet Address:{" "}
+                    <span className="text-gray-900">
+                      {truncateAddress(payData?.walletAddress) ||
+                        "Not Available"}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="material-symbols-outlined text-gray-600">
+                    attach_money
+                  </span>
+                  <p className="text-sm font-bold text-gray-700">
+                    Pay Out:{" "}
+                    <span className="text-gray-900">
+                      {payData.stipendAmount || "Not Available"}{" "}
+                      {payData.stipendCurrency || ""}
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
           )}
