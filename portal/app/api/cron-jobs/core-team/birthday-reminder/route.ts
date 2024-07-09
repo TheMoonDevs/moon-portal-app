5c73105a1b8a44cb4cd3ae33a4d908ca1ad277a5
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-07-09 17:21:17 +0530

    fixed some bugs in birthday reminder

--- portal/app/api/cron-jobs/core-team/birthday-reminder/route.ts
+++ portal/app/api/cron-jobs/core-team/birthday-reminder/route.ts
@@ -1,71 +1,85 @@
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
 import { prisma } from "@/prisma/prisma";
-import { SlackBotSdk } from "@/utils/services/slackBotSdk";
+import { SlackBotSdk, SlackChannels } from "@/utils/services/slackBotSdk";
 import { JsonObject } from "@prisma/client/runtime/library";
 import { NextResponse, NextRequest } from "next/server";
 
-function extractMonthDay(dateStr: string): string {
+const slackBot = new SlackBotSdk();
+
+const extractMonthDay = (dateStr: string): string => {
   const date = new Date(dateStr);
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
   const day = ("0" + date.getDate()).slice(-2);
   return `${month}-${day}`;
-}
+};
+
+const currentDateStr = extractMonthDay(new Date().toISOString());
+const dateInFourDaysStr = extractMonthDay(
+  new Date(new Date().setDate(new Date().getDate() + 4)).toISOString()
+);
 
 export async function GET(request: NextRequest) {
   try {
     const users = await prisma.user.findMany({
       where: {
+        userType: "MEMBER",
         role: "CORETEAM",
         status: "ACTIVE",
       },
     });
-    
-    const today = new Date();
-    const fourDaysLater = new Date();
-    fourDaysLater.setDate(today.getDate() + 4);
-
-    const todayStr = extractMonthDay(today.toISOString());
-    const fourDaysLaterStr = extractMonthDay(fourDaysLater.toISOString());
-
-    const slackBot = new SlackBotSdk();
 
     for (const user of users) {
-      const { personalData, email } = user;
-      // const dateOfBirth = (personalData as JsonObject)?.dateOfBirth;
-      // console.log(dateOfBirth);
+      const { personalData, slackId, name } = user;
+      const dobStr = (personalData as JsonObject)?.dateOfBirth as string;
 
-      // const dateOfBirthStr = extractMonthDay(dateOfBirth as string);
-      const dateOfBirthStr = extractMonthDay('1999-06-12')
-      const todayDate = extractMonthDay('1999-06-12')
-      // console.log(dateOfBirthStr, todayDate);
-      if (email && dateOfBirthStr) {
-        if (dateOfBirthStr === todayDate) {
-          if (user?.slackId) {
-            const message = `<@${user?.slackId}> has a birthday today!`;
-            await slackBot.sendSlackMessageviaAPI({ text: message, channel: 'C07A8AU5UKB' }); //C066K6FKU1X
-          }
+      if (!dobStr) {
+        console.log("No date of birth found for user:", name);
+        continue;
+      }
+
+      const dobMonthDay = extractMonthDay(dobStr);
+
+      if (dobMonthDay === currentDateStr) {
+        if (slackId) {
+          const message = `Reminder: It's ${
+            slackId ? `<@${slackId}>` : name
+          }'s birthday today! 🎉 Let's make sure to send them some love and good wishes!`;
+          await slackBot.sendSlackMessageviaAPI({
+            text: message,
+            channel: SlackChannels.executive_channel,
+          });
+        } else {
+          console.log("No slackId found for user:", name);
         }
+      }
 
-        if (dateOfBirthStr === fourDaysLaterStr) {
-          if (user?.slackId) {
-            const message = `<@${user?.slackId}> has a birthday in 4 days. Don't forget the gift!`;
-            await slackBot.sendSlackMessageviaAPI({ text: message, channel: 'C07A8AU5UKB' }); //C06944N1NQ7
-          }
+      if (dobMonthDay === dateInFourDaysStr) {
+        if (slackId) {
+          const message = `Heads up everyone! ${
+            slackId ? `<@${slackId}>` : name
+          }'s birthday is coming up in 4 days! Let's not forget to prepare a gift 🎁 and make it a special day! 🎉`;
+          await slackBot.sendSlackMessageviaAPI({
+            text: message,
+            channel: SlackChannels.management_channel,
+          });
         }
       }
     }
 
     const jsonResponse = {
       status: "success",
-      message:
-        'reminder sent to the group'
+      message: "Reminders Sent!",
     };
 
     return NextResponse.json(jsonResponse);
   } catch (error) {
     console.error("Error in GET route:", error);
-    return new NextResponse(JSON.stringify({ status: "error", message: 'Internal Server Error' }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new NextResponse(
+      JSON.stringify({ status: "error", message: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 }
