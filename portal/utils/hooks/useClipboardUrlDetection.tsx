9c94ce43fb91103f498e20847fbcd4d044d09b80
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-07-19 21:56:36 +0530

    feat: new link creation flow
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- portal/utils/hooks/useClipboardUrlDetection.tsx
+++ portal/utils/hooks/useClipboardUrlDetection.tsx
@@ -5,18 +5,25 @@ const useClipboardURLDetection = () => {
   const [isTabFocused, setIsTabFocused] = useState(
     typeof window !== "undefined" && document.hasFocus()
   );
+  const [isPasteEvent, setIsPasteEvent] = useState(false);
+  const [validURLPasteEvent, setValidURLPasteEvent] = useState(false);
   const [copiedURL, setCopiedURL] = useState<string | null>(null);
 
   useEffect(() => {
-    const handleFocus = () => setIsTabFocused(true);
+    const handleFocus = () => {
+      setIsTabFocused(true);
+      setIsPasteEvent(false);
+    };
     const handleBlur = () => setIsTabFocused(false);
-
+    const handlePaste = () => setIsPasteEvent(true);
     window.addEventListener("focus", handleFocus);
     window.addEventListener("blur", handleBlur);
+    window.addEventListener("paste", handlePaste);
 
     return () => {
       window.removeEventListener("focus", handleFocus);
       window.removeEventListener("blur", handleBlur);
+      window.removeEventListener("paste", handlePaste);
     };
   }, []);
 
@@ -25,16 +32,18 @@ const useClipboardURLDetection = () => {
       if (!isTabFocused) return;
       const text = await navigator.clipboard.readText();
       if (isValidURL(text)) {
-        console.log(text);
         setCopiedURL(text);
       } else {
+        setIsPasteEvent(false);
         setCopiedURL(null);
       }
+      if (copiedURL && isPasteEvent) setValidURLPasteEvent(true);
+      setIsPasteEvent(false);
     };
     handleCopy();
-  }, [isTabFocused]);
+  }, [isTabFocused, isPasteEvent, validURLPasteEvent]);
 
-  return { copiedURL, setCopiedURL };
+  return { copiedURL, setCopiedURL, validURLPasteEvent, setValidURLPasteEvent };
 };
 
 export default useClipboardURLDetection;
