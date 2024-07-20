94a6afe359e51410f5966f21ef05085e3ddde2bb
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-07-21 03:14:44 +0530

    notification update fix

--- portal/utils/hooks/useNotifications.tsx
+++ portal/utils/hooks/useNotifications.tsx
@@ -1,16 +1,16 @@
-'use client';
-import { useEffect } from 'react';
-import useSWR, { SWRResponse } from 'swr';
-import { PortalSdk } from '../services/PortalSdk';
+"use client";
+import { useEffect } from "react";
+import useSWR, { SWRResponse } from "swr";
+import { PortalSdk } from "../services/PortalSdk";
 import {
   setNotifications,
   setNotificationsCount,
-} from '../redux/notification/notification.slice';
-import { useAppDispatch, useAppSelector } from '../redux/store';
-import { Notification } from '@prisma/client';
-import { JsonObject } from '@prisma/client/runtime/library';
-import { APP_BASE_URL } from '../constants/appInfo';
-import { INotification } from '@/components/screens/notifications/NotificationsList';
+} from "../redux/notification/notification.slice";
+import { useAppDispatch, useAppSelector } from "../redux/store";
+import { Notification } from "@prisma/client";
+import { JsonObject } from "@prisma/client/runtime/library";
+import { APP_BASE_URL } from "../constants/appInfo";
+import { INotification } from "@/components/screens/notifications/NotificationsList";
 
 const fetchNotifications = async (
   url: string,
@@ -21,43 +21,42 @@ const fetchNotifications = async (
   let notifications = response.data.notifications;
 
   if (!hasWallet) {
+    const walletNotification = {
+      userId,
+      title: "Wallet Address Required",
+      description: "Please add your wallet address to continue.",
+      matchId: `${userId}_onboard_walletAddress`,
+      notificationType: "SELF_GENERATED",
+      notificationData: {
+        actions: [
+          {
+            title: "Add Wallet Address",
+            trigger_type: "url",
+            trigger: `${APP_BASE_URL}/member/onboarding/wallet`,
+          },
+        ],
+        actionDone: false,
+      },
+    };
+
     const existingWalletNotification = notifications.find(
       (notification: Notification) =>
         notification.matchId === `${userId}_onboard_walletAddress`
     );
 
-    if (
-      existingWalletNotification &&
-      !existingWalletNotification?.notificationData?.actionDone
-    ) {
-      await PortalSdk.putData('/api/notifications/update', {
+    console.log("existingWalletNotification", existingWalletNotification);
+
+    if (existingWalletNotification) {
+      await PortalSdk.putData("/api/notifications/update", {
         ...existingWalletNotification,
+        ...walletNotification,
         updatedAt: new Date().toISOString(),
       });
 
       const updatedResponse = await PortalSdk.getData(url, null);
       notifications = updatedResponse.data.notifications;
-    } else if (!existingWalletNotification) {
-      const walletNotification = {
-        userId,
-        title: 'Wallet Address Required',
-        description: 'Please add your wallet address to continue.',
-        matchId: `${userId}_onboard_walletAddress`,
-        notificationType: 'SELF_GENERATED',
-        notificationData: {
-          actions: [
-            {
-              title: 'Add Wallet Address',
-              trigger_type: 'url',
-              trigger: `${APP_BASE_URL}/member/onboarding/wallet`,
-            },
-          ],
-          actionDone: false,
-        },
-      };
-
-      await PortalSdk.postData('/api/notifications/add', walletNotification);
-
+    } else {
+      await PortalSdk.postData("/api/notifications/add", walletNotification);
       const updatedResponse = await PortalSdk.getData(url, null);
       notifications = updatedResponse.data.notifications;
     }
@@ -89,7 +88,7 @@ export const useNotifications = () => {
         Boolean(payData?.walletAddress)
       ),
     {
-      refreshInterval: 3 * 60 * 1000,
+      refreshInterval: 1 * 60 * 1000,
     }
   );
 
