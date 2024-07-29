81f8546abd600bebbd7287ccf9b6627917e3ae40
Allen-Saji <allensaji04@gmail.com>
2024-07-29 13:16:11 +0530

    update slug route common-resources -> common_resources

---
+++ portal/app/quicklinks/common_resources/[departmentId]/[directoryId]/page.tsx
@@ -0,0 +1,34 @@
+import { CommonLinksByDirId } from "@/components/screens/Quicklinks/screens/CommonLinks/CommonLinksByDirId";
+import { APP_BASE_URL } from "@/utils/constants/appInfo";
+import { QuicklinksSdk } from "@/utils/services/QuicklinksSdk";
+import { Directory } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+async function slugToIdConversion(slug: string) {
+  try {
+    const slugString = slug.substring(0, slug.lastIndexOf("-"));
+    const response = await QuicklinksSdk.getData(
+      `${APP_BASE_URL}/api/quicklinks/directory?slug=${slugString}`
+    );
+    return response.data.directoryList.find((directory: Directory) => {
+      return (
+        directory.slug +
+          "-" +
+          new Date(directory.timestamp).getTime().toString().slice(-5) ===
+        slug
+      );
+    })?.id;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export default async function Home({
+  params,
+}: {
+  params: { directoryId: string };
+}) {
+  const directoryId = await slugToIdConversion(params.directoryId);
+  if (!directoryId) return notFound();
+  // untitled-78976
+  return <CommonLinksByDirId directoryId={directoryId} />;
+}
