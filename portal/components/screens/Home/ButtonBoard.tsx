c28a65222d902c04f6c11e99e27a7d633c396a9e
Manish Guhe <manishguhe301@gmail.com>
2024-08-09 19:26:53 +0530

    added functionality to auto create badge rewarded specific to user with status activated or rewarded based on the target date of time based badges created by admin.

--- portal/components/screens/Home/ButtonBoard.tsx
+++ portal/components/screens/Home/ButtonBoard.tsx
@@ -1,10 +1,132 @@
 import { APP_ROUTES } from "@/utils/constants/appInfo";
 import { useUser } from "@/utils/hooks/useUser";
+import { PortalSdk } from "@/utils/services/PortalSdk";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { BadgeRewarded, BadgeTemplate } from "@prisma/client";
+import { JsonObject } from "@prisma/client/runtime/library";
+import DrawerComponent from "@/components/elements/DrawerComponent";
+import BadgeCard from "./BadgeCard";
+import { Spinner } from "@/components/elements/Loaders";
 
 export const ButtonBoard = () => {
+  const [badges, setBadges] = useState<BadgeTemplate[]>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [badgeRewarded, setBadgeRewarded] = useState<BadgeRewarded[]>();
+  const [loading, setLoading] = useState<boolean>(false);
   const { user } = useUser();
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const res = await PortalSdk.getData(
+          "/api/badges?badgeType=TIME_BASED",
+          null
+        );
+        setBadges(res.data);
+      } catch (error) {
+        console.error("Error fetching badges:", error);
+      }
+    };
+
+    fetchBadges();
+  }, []);
+
+  useEffect(() => {
+    const fetchBadgesRewarded = async () => {
+      setLoading(true);
+
+      try {
+        const res = await PortalSdk.getData(
+          `/api/user/badge-rewarded?id=${user?.id}`,
+          null
+        );
+        const data = await res.data;
+        setBadgeRewarded(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching badges:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchBadgesRewarded();
+  }, [user?.id]);
+
+  const sendBadgeRequest = async (
+    status: string,
+    showsCounter: boolean,
+    badge: BadgeTemplate,
+    date?: string
+  ) => {
+    const badgePayload = {
+      userId: user?.id,
+      badgeTemplateId: badge.id,
+      name: badge.name,
+      sequence: "voyagers",
+      imageUrl: badge.imageurl,
+      status,
+      showsCounter,
+      ...(date && { date }),
+    };
+
+    try {
+      const res = await PortalSdk.postData(
+        "/api/user/badge-rewarded",
+        badgePayload
+      );
+      // console.log(`Badge ${status.toLowerCase()} successfully:`, res.data);
+    } catch (error) {
+      console.error(`Error ${status.toLowerCase()} badge:`, error);
+    }
+  };
+
+  const checkAndRewardBadge = async ({ badge }: { badge: BadgeTemplate }) => {
+    const { criteria } = badge;
+    const criteriaLogic = (criteria as JsonObject)?.criteriaLogic as string;
+    const joiningDate = dayjs((user?.workData as any)?.joining);
+    if (!criteria || !criteriaLogic || !joiningDate.isValid()) {
+      return;
+    }
+
+    const today = dayjs();
+    let targetDate;
+
+    if (criteriaLogic.includes("days")) {
+      const days = parseInt(criteriaLogic.split(" ")[0], 10);
+      targetDate = joiningDate.add(days, "day");
+    } else if (criteriaLogic.includes("months")) {
+      const months = parseInt(criteriaLogic.split(" ")[0], 10);
+      targetDate = joiningDate.add(months, "month");
+    } else if (criteriaLogic.includes("year")) {
+      const years = parseInt(criteriaLogic.split(" ")[0], 10);
+      targetDate = joiningDate.add(years, "year");
+    }
+    const formattedTargetDate = targetDate?.format("YYYY-MM-DD");
+
+    if (
+      targetDate &&
+      (today.isAfter(targetDate) || today.isSame(targetDate, "day"))
+    ) {
+      await sendBadgeRequest("REWARDED", false, badge, formattedTargetDate);
+    } else {
+      await sendBadgeRequest("ACTIVATED", true, badge, formattedTargetDate);
+    }
+  };
+
+  useEffect(() => {
+    if (badges) {
+      badges?.forEach((badge: BadgeTemplate) => {
+        checkAndRewardBadge({ badge });
+      });
+    }
+  }, [badges]);
 
   return (
     <div className="flex flex-row justify-between w-full py-2 px-3">
@@ -59,7 +181,10 @@ export const ButtonBoard = () => {
           alt={""}
           className="static w-full h-full opacity-[0.9] object-cover object-center rounded-lg"
         /> */}
-        <div className="absolute bottom-0 top-0 left-0 right-0 flex flex-col items-center justify-center gap-2 text-2xl">
+        <div
+          className="absolute bottom-0 top-0 left-0 right-0 flex flex-col items-center justify-center gap-2 text-2xl"
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <span className="icon_size material-symbols-outlined font-light">
             editor_choice
           </span>
@@ -88,6 +213,28 @@ export const ButtonBoard = () => {
           </span>
         </div>
       </Link>
+      <DrawerComponent isOpen={isOpen} handleClose={handleClose}>
+        <div className="p-4">
+          <h2 className="text-xl font-bold text-center mb-6">
+            Your Earned Badges
+          </h2>
+          {loading ? (
+            <div className="h-[100vh] flex justify-center items-center">
+              <Spinner className="h-10 w-10" />{" "}
+            </div>
+          ) : badgeRewarded ? (
+            <div className="flex flex-col gap-4">
+              {badgeRewarded.map((badge: BadgeRewarded) => (
+                <BadgeCard key={badge.id} badge={badge} />
+              ))}
+            </div>
+          ) : (
+            <div className="h-[100vh] flex justify-center items-center">
+              No badges found.
+            </div>
+          )}
+        </div>
+      </DrawerComponent>
     </div>
   );
 };
