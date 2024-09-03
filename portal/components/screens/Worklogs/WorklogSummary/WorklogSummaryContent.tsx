34bfdf2d85cc242932440af4cb4883a3d3a7affa
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2024-09-03 07:19:04 +0530

    left box setups

--- portal/components/screens/Worklogs/WorklogSummary/WorklogSummaryContent.tsx
+++ portal/components/screens/Worklogs/WorklogSummary/WorklogSummaryContent.tsx
@@ -7,6 +7,7 @@ import { PortalSdk } from "@/utils/services/PortalSdk";
 import { User, WorkLogs } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
 import dayjs from "dayjs";
+import { calculateMetrics } from "../WorklogBreakdown/BreakdownMetrics";
 
 export const WorklogSummaryContent = ({
   userData,
@@ -24,6 +25,7 @@ export const WorklogSummaryContent = ({
     : null;
 
   const [worklogSummary, setWorklogSummary] = useState<WorkLogs[]>([]);
+  const [userInfo, setUserInfo] = useState<User | null | undefined>(undefined);
   const { loading, setLoading } = useAsyncState();
   const pdfTargetRef = useRef(null);
   // console.log(userData?.id);
@@ -54,16 +56,42 @@ export const WorklogSummaryContent = ({
     // console.log(worklogSummary.map((wl) => wl.works));
   }, [fetchWorklogData, month, year]);
 
+  const isMonthly = !!month;
+  const isYearly = !!year && !month;
+  const metrics = calculateMetrics(worklogSummary, isMonthly, isYearly);
+
   return (
-    <div className="divide-x-2 flex flex-col md:flex-row overflow-hidden h-screen w-full">
-      <div className="w-[100%] md:w-[50%]  md:h-screen overflow-y-scroll">
-        <div ref={pdfTargetRef} className="pt-28 p-10">
-          <h1 className="text-2xl font-bold pb-8">
-            {userData?.name?.split(" ")[0]}&apos;s Worklog Summary for{" "}
-            {summaryTitle}
-          </h1>
+    <div className="divide-x-2 flex flex-col md:flex-row overflow-hiddenh-screen w-full h-screen bg-neutral-100">
+      <div className="w-[100%] md:w-[50%] overflow-y-scroll m-4 mt-4 bg-white rounded-2xl shadow-xl">
+        <div ref={pdfTargetRef} className="">
+          {userData &&
+            <div className="flex items-center justify-start p-8 py-4 shadow-md gap-4 bg-white sticky top-0 z-10">
+              <img
+                src={userData?.avatar ?? "/images/avatar.png"}
+                alt="avatar"
+                className="w-16 h-16 rounded-full"
+              />
+              <div>
+                <p className="text-xl font-bold">{userData?.name}</p>
+                <p className="text-sm font-regular">
+                  Worklog Summary for{" "}
+                  {summaryTitle}
+                </p>
+              </div>
+              <div className="ml-auto">
+                <h3 className="text-2xl font-bold">
+                  {summaryTitle}
+                </h3>
+                <p className="text-xs font-regular">
+                  {worklogSummary.length} Logs | {metrics.totalTasks} tasks
+                </p>
+              </div>
+            </div>
+          }
           {!loading ? (
-            <WorklogSummaryView worklogSummary={worklogSummary} />
+            <div className="">
+              <WorklogSummaryView worklogSummary={worklogSummary} workLogUser={userData} />
+            </div>
           ) : (
             <LoadingSkeleton />
           )}
