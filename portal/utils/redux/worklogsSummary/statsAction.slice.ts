dbe76145fdbccceb6096fb0972608d412edeb319
Manish Guhe <manishguhe301@gmail.com>
2024-09-12 15:36:12 +0530

    redux slice created  + all the metrics functiona checked to determine which is wrong but all are working correctly

---
+++ portal/utils/redux/worklogsSummary/statsAction.slice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const statsActionSlice = createSlice({
+  name: "statsActionSlice",
+  initialState: {
+  },
+  reducers: {
+  },
+});
+
+export const { } = statsActionSlice.actions;
+export default statsActionSlice.reducer;
