61f7089180d7fc1afa4a5e9c7807a43c5bf09cd6
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-09-15 22:56:27 +0530

    refactor:  department wise links to use new api calls and updated imports

--- portal/components/screens/Quicklinks/screens/ParentDirectory/SubDirectoryLinks.tsx
+++ portal/components/screens/Quicklinks/screens/ParentDirectory/SubDirectoryLinks.tsx
@@ -4,16 +4,17 @@ import { QuicklinksSdk } from "@/utils/services/QuicklinksSdk";
 import { useEffect } from "react";
 import LinkList from "../../LinkList/LinkList";
 import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
-import { setAllQuicklinks } from "@/utils/redux/quicklinks/quicklinks.slice";
 import useAsyncState from "@/utils/hooks/useAsyncState";
 import { LinkFiltersHeader } from "../../LinkList/LinkFiltersHeader";
 import { useQuickLinkDirs } from "../../hooks/useQuickLinksDirs";
+import { ViewButtonGroup } from "../../LinkList/ViewButtonGroup";
+import { useQuickLinkDirectory } from "../../hooks/useQuickLinkDirectory";
+import { setAllQuicklinks } from "@/utils/redux/quicklinks/slices/quicklinks.links.slice";
 
 export const SubDirectoryLinks = () => {
   const dispatch = useAppDispatch();
-  const { allQuicklinks, activeDirectoryId } = useAppSelector(
-    (state) => state.quicklinks
-  );
+  const { allQuicklinks } = useAppSelector((state) => state.quicklinksLinks);
+  const { activeDirectoryId } = useQuickLinkDirectory();
   const { thisDirectory, parentDirecotry } =
     useQuickLinkDirs(activeDirectoryId);
   const { loading, setLoading, error, setError } = useAsyncState();
@@ -37,10 +38,19 @@ export const SubDirectoryLinks = () => {
 
     getData();
   }, [activeDirectoryId, dispatch, setLoading]);
+
+  if (allQuicklinks.length === 0) return null;
+
   return (
-    <div className="flex flex-col w-full mt-8">
-      <LinkFiltersHeader title={thisDirectory?.title} />
-      <LinkList allQuicklinks={allQuicklinks} isLoading={loading} />
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className="py-[10px] font-bold text-xl">Links</h1>
+        <ViewButtonGroup />
+      </div>
+      <div className="flex flex-col w-full mt-3">
+        {/* <LinkFiltersHeader title={thisDirectory?.title} /> */}
+        <LinkList allQuicklinks={allQuicklinks} isLoading={loading} />
+      </div>
     </div>
   );
 };
