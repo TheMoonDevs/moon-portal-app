57841b832dba675615ee9ed4008e0292a8a1f364
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-09-15 22:56:27 +0530

    refactor:  department wise links to use new api calls and updated imports

--- portal/components/screens/Quicklinks/screens/Department/DepartmentQuicklinks.tsx
+++ portal/components/screens/Quicklinks/screens/Department/DepartmentQuicklinks.tsx
@@ -4,79 +4,11 @@ import store, { useAppSelector } from "@/utils/redux/store";
 import { ParentDirectoryLinks } from "../ParentDirectory/ParentDirectoryLinks";
 import { useQuickLinkDirs } from "../../hooks/useQuickLinksDirs";
 import { useRef } from "react";
-import { setActiveDirectoryId } from "@/utils/redux/quicklinks/quicklinks.slice";
 import QuicklinkHeaderWrapper from "../../global/QuicklinkHeaderWrapper";
-// import { QuicklinksSdk } from "@/utils/services/QuicklinksSdk";
-// import { useSearchParams } from "next/navigation";
-// import { useEffect } from "react";
-// import LinkList from "../../LinkList/LinkList";
-// import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
-// import {
-//   setAllQuicklinks,
-//   setTopUsedList,
-// } from "@/utils/redux/quicklinks/quicklinks.slice";
-// import TopUsedLink from "../Dashboard/TopUsedLink";
-// import { Link } from "@prisma/client";
-// import useAsyncState from "@/utils/hooks/useAsyncState";
-// import { LinkFiltersHeader } from "../../LinkList/LinkFiltersHeader";
-// import { useQuickLinkDirs } from "../../hooks/useQuickLinksDirs";
-
-// export const DepartmentLinks = ({
-//   departmentSlug,
-// }: {
-//   departmentSlug: string;
-// }) => {
-//   const dispatch = useAppDispatch();
-//   const params = useSearchParams();
-//   const rootParentDirId = params?.get("id");
-//   const { thisDirectory, parentDirecotry } = useQuickLinkDirs(rootParentDirId);
-
-//   const { allQuicklinks, topUsedList } = useAppSelector(
-//     (state) => state.quicklinks
-//   );
-//   const { loading, setLoading, error, setError } = useAsyncState();
-
-//   //Fetch link by department Id
-//   useEffect(() => {
-//     const getData = async () => {
-//       setLoading(true);
-//       // try {
-//       const reponse = await QuicklinksSdk.getData(
-//         `/api/quicklinks/link?rootParentDirId=${rootParentDirId}`
-//       );
-
-//       let quicklinks: Link[] = reponse.data.links;
-//       dispatch(setAllQuicklinks(quicklinks));
-
-//       const sortedQuicklinks = [...quicklinks]
-//         .sort((a, b) => b.clickCount - a.clickCount)
-//         .slice(0, 5);
-//       dispatch(setTopUsedList(sortedQuicklinks));
-
-//       setLoading(false);
-//       // } catch (error) {
-//       //   console.log(error);
-//       // }
-//     };
-//     getData();
-//   }, [rootParentDirId, dispatch, setLoading]);
-
-//   return (
-//     <div>
-//       <TopUsedLink title={`Top Used in ${thisDirectory?.title}`}>
-//         <LinkList
-//           allQuicklinks={topUsedList}
-//           withView="thumbnail"
-//           isLoading={loading}
-//         />
-//       </TopUsedLink>
-//       <div className="flex flex-col w-full">
-//         <LinkFiltersHeader title={`View All in ${thisDirectory?.title}`} />
-//         <LinkList allQuicklinks={allQuicklinks} isLoading={loading} />
-//       </div>
-//     </div>
-//   );
-// };
+import { usePathname } from "next/navigation";
+import ListOfDirectories from "../../DirectoryList";
+import { useQuickLinkDirectory } from "../../hooks/useQuickLinkDirectory";
+import { setActiveDirectoryId } from "@/utils/redux/quicklinks/slices/quicklinks.directory.slice";
 
 export const DepartmentLinks = ({
   rootParentDirId,
@@ -89,20 +21,46 @@ export const DepartmentLinks = ({
     store.dispatch(setActiveDirectoryId(rootParentDirId));
     initialize.current = true;
   }
-  const { activeDirectoryId } = useAppSelector((state) => state.quicklinks);
+
   // console.log(activeDirectoryId);
+  const { activeDirectoryId, directories } = useQuickLinkDirectory();
   const { thisDirectory } = useQuickLinkDirs(activeDirectoryId);
+  const pathname = usePathname();
+  const filteredDirectories = directories.filter(
+    (directory) => directory.parentDirId === activeDirectoryId
+  );
+  const { allQuicklinks } = useAppSelector((state) => state.quicklinksLinks);
+
   return (
     <div>
-      <QuicklinkHeaderWrapper>
-        <h1 className="text-3xl font-bold flex items-center gap-4">
-          <span className="material-symbols-outlined border border-neutral-200 rounded-full p-2">
-            groups
-          </span>{" "}
-          <span>{thisDirectory?.title}</span>
-        </h1>
-      </QuicklinkHeaderWrapper>
-      <ParentDirectoryLinks />
+      <QuicklinkHeaderWrapper
+        title={thisDirectory?.title || ""}
+        withBreadcrumb={{
+          rootType: "DEPARTMENT",
+        }}
+      />
+
+      {allQuicklinks.length === 0 && filteredDirectories.length === 0 ? (
+        <div className="w-full flex items-center justify-center h-full">
+          Start by adding a folder or link
+        </div>
+      ) : (
+        <div className="flex gap-10">
+          <div className="mt-4 flex justify-stretch gap-6 w-[70%]">
+            <div className="w-full">
+              <ParentDirectoryLinks />
+            </div>
+          </div>
+          <div className="my-8 w-[30%]">
+            <h1 className="text-xl font-bold">Folders</h1>
+            <ListOfDirectories
+              view="listView"
+              pathname={pathname}
+              directories={filteredDirectories}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
