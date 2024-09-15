5766d1dbe9422dc3f2d4b76a78fd8e35a739cf82
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-09-15 22:47:02 +0530

    refactor: update directory API calls and imports in Quicklinks pages

---
+++ portal/app/quicklinks/user/folders/top-used/page.tsx
@@ -0,0 +1,7 @@
+import UserTopUsedDirectories from "@/components/screens/Quicklinks/screens/User/UserTopUsed/UserTopUsedDirectories";
+
+const UserTopUsedDirectoriesPage = () => {
+  return <UserTopUsedDirectories withTitle={true} view="gridView" />;
+};
+
+export default UserTopUsedDirectoriesPage;
