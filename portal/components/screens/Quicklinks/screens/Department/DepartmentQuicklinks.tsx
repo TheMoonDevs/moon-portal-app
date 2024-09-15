b061f1934dbb259c4cf0c3cc2e35a570cb08d1e0
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-09-15 23:58:37 +0530

    refactor: Update QuicklinkSidebar component and add images for empty states
    - Update QuicklinkSidebar component to include a new feature.
    - Add images for empty states in LinkList and CommonQuicklinks components.

--- portal/components/screens/Quicklinks/screens/Department/DepartmentQuicklinks.tsx
+++ portal/components/screens/Quicklinks/screens/Department/DepartmentQuicklinks.tsx
@@ -9,6 +9,7 @@ import { usePathname } from "next/navigation";
 import ListOfDirectories from "../../DirectoryList";
 import { useQuickLinkDirectory } from "../../hooks/useQuickLinkDirectory";
 import { setActiveDirectoryId } from "@/utils/redux/quicklinks/slices/quicklinks.directory.slice";
+import Image from "next/image";
 
 export const DepartmentLinks = ({
   rootParentDirId,
@@ -41,8 +42,17 @@ export const DepartmentLinks = ({
       />
 
       {allQuicklinks.length === 0 && filteredDirectories.length === 0 ? (
-        <div className="w-full flex items-center justify-center h-full">
-          Start by adding a folder or link
+        <div className="flex flex-col gap-3 items-center justify-center h-[350px] w-full">
+          <Image
+            className="rounded-full object-cover"
+            src="/images/no-data.jpg"
+            alt="No data"
+            width={400}
+            height={400}
+          />
+          <p className="text-gray-400 text-lg">
+            Start by adding a folder or link
+          </p>
         </div>
       ) : (
         <div className="flex gap-10">
