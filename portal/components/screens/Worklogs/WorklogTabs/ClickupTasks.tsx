8a800895d54bebbf7c5a31705d977005bb43e96c
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-16 04:26:09 +0530

    chore: minor style changes

--- portal/components/screens/Worklogs/WorklogTabs/ClickupTasks.tsx
+++ portal/components/screens/Worklogs/WorklogTabs/ClickupTasks.tsx
@@ -3,13 +3,8 @@ import { filterTasks, filterTasksByPerson } from "@/utils/clickup/helper";
 import { tasks } from "@/utils/dummy/clickup/tasks";
 import { useUser } from "@/utils/hooks/useUser";
 import { Task } from "@/utils/@types/clickup/types";
-
-const getColour = (status: string) => {
-  if (status.toLocaleLowerCase() == "to do") return "bg-[#3C414A] text-white";
-  else if (status.toLocaleLowerCase() == "in review")
-    return "bg-[#F9BE34] text-black";
-  else return "bg-[#8077F1] text-white";
-};
+import { CircleIcon, Flag } from "lucide-react";
+import Link from "next/link";
 
 const ClickupTask = () => {
   const { user } = useUser();
@@ -30,21 +25,61 @@ const ClickupTask = () => {
 };
 
 const SingleTask = ({ status, tasks }: { status: string; tasks: Task[] }) => {
+  const getColour = (status: string) => {
+    if (status.toLocaleLowerCase() == "to do") return "bg-[#3C414A] text-white";
+    else if (status.toLocaleLowerCase() == "in review")
+      return "bg-[#F9BE34] text-black";
+    else return "bg-[#8077F1] text-white";
+  };
   const background = getColour(status);
 
   return (
     <div className="space-y-2">
-      <h1 className={"w-fit px-2 py-1 rounded-lg " + background}>
-        {status.toUpperCase()}
+      <h1 className={"w-fit px-2 py-1 rounded-lg flex gap-2 " + background}>
+        <CircleIcon />
+        <span>{status.toUpperCase()}</span>
       </h1>
       <div>
         {tasks.map((task) => (
-          <div key={task.taskId} className="flex justify-between">
+          <Link
+            href={task.url}
+            target="_blank"
+            key={task.taskId}
+            className="flex justify-between"
+          >
             <p>{task.name}</p>
-            <p>{task.priority} High</p>
-          </div>
+            <p>
+              <PriorityDisplay priority={task.priority || ""} />
+            </p>
+          </Link>
         ))}
       </div>
+    </div>
+  );
+};
+
+const PriorityDisplay = ({ priority }: { priority: string }) => {
+  if (!priority) return null;
+
+  const getPriorityColor = (priority: string) => {
+    switch (priority) {
+      case "urgent":
+        return "text-red-400";
+      case "high":
+        return "text-yellow-400";
+      case "normal":
+        return "text-blue-400";
+      case "low":
+        return "text-gray-400";
+      default:
+        return "text-gray-400";
+    }
+  };
+
+  return (
+    <div className="flex items-center space-x-2 text-sm">
+      <Flag className={`w-4 h-4 ${getPriorityColor(priority)}`} />
+      <span className="text-black">{priority}</span>
     </div>
   );
 };
