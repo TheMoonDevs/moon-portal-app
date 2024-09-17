8985377b269f3a4bedd50544e15448ef81afd31f
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-09-18 01:59:34 +0530

    feat: implement conditional fetching and 304 response for notifications

--- portal/app/api/notifications/update/route.ts
+++ portal/app/api/notifications/update/route.ts
@@ -10,7 +10,7 @@ export async function PUT(request: NextRequest) {
         id,
       },
       create: { id, ...data },
-      update: data,
+      update: {...data, updatedAt: new Date()},
     });
 
     const json_response = {
