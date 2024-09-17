3057fa1cabf2529cb0aebd944c6839680fa6285f
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-09-18 01:58:37 +0530

    refactor: replace useNotifications hook with useAppSelector for notifications state

--- portal/app/notifications/page.tsx
+++ portal/app/notifications/page.tsx
@@ -1,8 +1,6 @@
 import { Bottombar } from '@/components/global/Bottombar';
 import { PageAccess } from '@/components/global/PageAccess';
-import { useNotifications } from '@/utils/hooks/useNotifications';
 import NotificationsScreen from '@/components/screens/notifications/NotificationsScreen';
-import { Notification } from '@prisma/client';
 
 export default function NotificationsPage() {
 
--- portal/components/screens/notifications/NotificationsList.tsx
+++ portal/components/screens/notifications/NotificationsList.tsx
@@ -1,10 +1,11 @@
-'use client';
-import { Notification } from '@prisma/client';
-import React, { useState } from 'react';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import Link from 'next/link';
+"use client";
+import { Notification } from "@prisma/client";
+import React, { useState } from "react";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import Link from "next/link";
+import { useAppSelector } from "@/utils/redux/store";
 
-export interface INotification extends Omit<Notification, 'notificationData'> {
+export interface INotification extends Omit<Notification, "notificationData"> {
   notificationData: INotificationData;
 }
 
@@ -23,11 +24,11 @@ export const timeAgo = (timestamp: string) => {
   return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
 };
 
-const NotificationsList = ({
-  notifications,
-}: {
-  notifications: INotification[];
-}) => {
+const NotificationsList = () => {
+  const notifications = useAppSelector(
+    (state) => state.notifications.notifications
+  );
+  console.log(notifications);
   const [expandedId, setExpandedId] = useState<string | null>(null);
 
   const toggleDescription = (id: string) => {
@@ -39,14 +40,14 @@ const NotificationsList = ({
   };
 
   return (
-    <div className=''>
-      <div className='sticky top-0 bg-white z-10 p-4'>
-        <div className='flex justify-between items-center'>
-          <h4 className='text-lg font-bold'>Notifications</h4>
-          <button className='text-xs text-gray-600 flex gap-1 items-center p-2'>
+    <div className="">
+      <div className="sticky top-0 bg-white z-10 p-4">
+        <div className="flex justify-between items-center">
+          <h4 className="text-lg font-bold">Notifications</h4>
+          <button className="text-xs text-gray-600 flex gap-1 items-center p-2">
             <span
-              className='material-symbols-outlined'
-              style={{ fontSize: '16px' }}
+              className="material-symbols-outlined"
+              style={{ fontSize: "16px" }}
             >
               done_all
             </span>
@@ -55,59 +56,59 @@ const NotificationsList = ({
         </div>
       </div>
       {notifications.length === 0 ? (
-        <div className='flex flex-col items-center justify-center bg-gray-50 p-6 rounded-lg border border-gray-200'>
-          <span className='material-symbols-outlined text-gray-600 text-4xl mb-2'>
+        <div className="flex flex-col items-center justify-center bg-gray-50 p-6 rounded-lg border border-gray-200">
+          <span className="material-symbols-outlined text-gray-600 text-4xl mb-2">
             notifications
           </span>
-          <span className='text-gray-500 text-lg'>No notifications</span>
+          <span className="text-gray-500 text-lg">No notifications</span>
         </div>
       ) : (
-        <div className='space-y-2 p-4'>
+        <div className="space-y-2 p-4">
           {notifications.map((notification: INotification) => (
             <div
               key={notification.id}
-              className='flex flex-col p-3 bg-gray-100 rounded-lg shadow hover:bg-gray-200 transition cursor-pointer'
+              className="flex flex-col p-3 bg-gray-100 rounded-lg shadow hover:bg-gray-200 transition cursor-pointer"
               onClick={() => toggleDescription(notification.id)}
             >
-              <div className='flex items-start'>
-                <span className='material-symbols-outlined text-blue-500 mr-2'>
+              <div className="flex items-start">
+                <span className="material-symbols-outlined text-blue-500 mr-2">
                   circle_notifications
                 </span>
-                <div className='flex flex-col w-full'>
-                  <div className='flex justify-between'>
+                <div className="flex flex-col w-full">
+                  <div className="flex justify-between">
                     <div>
-                      <p className='text-sm font-semibold text-blue-700 first-letter:capitalize'>
+                      <p className="text-sm font-semibold text-blue-700 first-letter:capitalize">
                         {notification.title}
                       </p>
-                      <p className='text-xs text-gray-500 mt-1'>
+                      <p className="text-xs text-gray-500 mt-1">
                         {timeAgo(notification.updatedAt.toString())}
                       </p>
                     </div>
                     {notification?.notificationData?.actionDone && (
-                      <span className='material-symbols-outlined text-green-500'>
+                      <span className="material-symbols-outlined text-green-500">
                         done
                       </span>
                     )}
                   </div>
                   {expandedId === notification.id && (
-                    <div className='mt-2 flex flex-col space-y-2'>
-                      <p className='text-sm text-gray-500'>
+                    <div className="mt-2 flex flex-col space-y-2">
+                      <p className="text-sm text-gray-500">
                         {notification.description}
                       </p>
-                      <div className='flex flex-wrap gap-2'>
+                      <div className="flex flex-wrap gap-2">
                         {notification.notificationData?.actions?.map(
                           (action: IAction, index: number) => {
                             return notification.notificationData.actionDone ? (
                               <span
                                 key={`${index}-${notification.id}-action`}
-                                className='text-sm text-white bg-gray-400 py-1 px-3 rounded cursor-not-allowed'
+                                className="text-sm text-white bg-gray-400 py-1 px-3 rounded cursor-not-allowed"
                               >
                                 Done
                               </span>
                             ) : (
                               <Link
                                 key={`${index}-${notification.id}-action`}
-                                className='text-sm text-white bg-blue-600 py-1 px-3 rounded hover:bg-blue-700 transition'
+                                className="text-sm text-white bg-blue-600 py-1 px-3 rounded hover:bg-blue-700 transition"
                                 href={action?.trigger}
                                 onClick={(e) => e.stopPropagation()}
                               >
