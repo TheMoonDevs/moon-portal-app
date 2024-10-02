71c6345526d5276da454a2157f22f7ffdff27ab4
Manish Guhe <manishguhe301@gmail.com>
2024-10-02 13:54:05 +0530

    mark all as read integrated

--- portal/components/screens/notifications/NotificationsList.tsx
+++ portal/components/screens/notifications/NotificationsList.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useAppSelector } from "@/utils/redux/store";
 import { PortalSdk } from "@/utils/services/PortalSdk";
 import ToolTip from "@/components/elements/ToolTip";
+import { useUser } from "@/utils/hooks/useUser";
 
 export interface INotification extends Omit<Notification, "notificationData"> {
   notificationData: INotificationData;
@@ -30,8 +31,12 @@ const NotificationsList = () => {
   const notifications = useAppSelector(
     (state) => state.notifications.notifications
   );
-  console.log(notifications);
+  // console.log(notifications);
   const [expandedId, setExpandedId] = useState<string | null>(null);
+  const hasUnreadNotifications = notifications.some(
+    (notification: INotification) => !notification?.isRead
+  );
+  const { user } = useUser();
 
   const handleRead = async (notification: INotification) => {
     try {
@@ -56,12 +61,38 @@ const NotificationsList = () => {
     }
   };
 
+  const handleMarkAllAsRead = async () => {
+    const unreadNotifications = notifications.filter(
+      (notification: INotification) => !notification?.isRead
+    );
+    if (unreadNotifications.length === 0) return;
+
+    try {
+      await Promise.all(
+        unreadNotifications.map((notification: INotification) =>
+          PortalSdk.putData("/api/notifications/update", {
+            ...notification,
+            isRead: true,
+          })
+        )
+      );
+    } catch (error) {
+      console.error("Error marking all notifications as read", error);
+    }
+  };
+
   return (
     <div className="">
       <div className="sticky top-0 bg-white z-10 p-4">
         <div className="flex justify-between items-center">
           <h4 className="text-lg font-bold">Notifications</h4>
-          <button className="text-xs text-gray-600 flex gap-1 items-center p-2">
+          <button
+            className={`text-xs text-gray-600 flex gap-1 items-center p-2 ${
+              !hasUnreadNotifications ? "cursor-not-allowed opacity-50" : ""
+            }`}
+            onClick={handleMarkAllAsRead}
+            disabled={!hasUnreadNotifications}
+          >
             <span
               className="material-symbols-outlined"
               style={{ fontSize: "16px" }}
@@ -85,18 +116,24 @@ const NotificationsList = () => {
             <div
               key={notification.id}
               className={`flex flex-col p-3 ${
-                !notification.isRead && "bg-gray-100"
+                (!notification.isRead ||
+                  !notification?.notificationData?.actionDone) &&
+                "bg-gray-100"
               } rounded-lg shadow hover:bg-gray-200 transition cursor-pointer`}
               onClick={() => toggleDescription(notification)}
             >
               <div className="flex items-start">
                 <span
                   className={`material-symbols-outlined ${
-                    !notification.isRead && "text-blue-500"
+                    !notification.isRead ||
+                    !notification?.notificationData?.actionDone
+                      ? "text-blue-500"
+                      : ""
                   } mr-2`}
                 >
                   circle_notifications
                 </span>
+
                 <div className="flex flex-col w-full">
                   <div className="flex justify-between">
                     <div>
@@ -116,26 +153,31 @@ const NotificationsList = () => {
                         done
                       </span>
                     )}
-                    <ToolTip
-                      title={
-                        notification.isRead ? "Mark as unread" : "Mark as read"
-                      }
-                    >
-                      <span
-                        className={`material-symbols-outlined ${
-                          notification.isRead ? "" : "text-blue-500"
-                        }`}
-                        style={{ fontSize: "20px", cursor: "pointer" }}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          handleRead(notification);
-                        }}
+                    {notification.matchId !==
+                      `${user?.id}_onboard_walletAddress` && (
+                      <ToolTip
+                        title={
+                          notification.isRead
+                            ? "Mark as unread"
+                            : "Mark as read"
+                        }
                       >
-                        {notification.isRead
-                          ? "mark_chat_read"
-                          : "mark_chat_unread"}
-                      </span>
-                    </ToolTip>
+                        <span
+                          className={`material-symbols-outlined ${
+                            notification.isRead ? "" : "text-blue-500"
+                          }`}
+                          style={{ fontSize: "20px", cursor: "pointer" }}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleRead(notification);
+                          }}
+                        >
+                          {notification.isRead
+                            ? "mark_chat_read"
+                            : "mark_chat_unread"}
+                        </span>
+                      </ToolTip>
+                    )}
                   </div>
                   {expandedId === notification.id && (
                     <div className="mt-2 flex flex-col space-y-2">
