7781cda7df775bd08abb4e0a89b4a746beb192a9
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-10-04 10:09:21 +0530

    update: initial states objects and utils

--- portal/components/screens/Houses/state.ts
+++ portal/components/screens/Houses/state.ts
@@ -1,10 +1,16 @@
-import { HOUSEID, Mission, MissionTask } from '@prisma/client';
-import dayjs, { Dayjs } from 'dayjs';
+import { HOUSEID, Mission, MissionTask } from "@prisma/client";
+import dayjs, { Dayjs } from "dayjs";
 
 export const initialMissionState: Partial<Mission> = {
-  id: '',
+  id: "",
   house: HOUSEID.MANAGEMENT,
-  month: '',
+  month: "",
+  status: {
+    label: "To Do",
+    value: "IN_TODO",
+    color: "#6b7280",
+  },
+  priority: null,
   completed: false,
   housePoints: 0,
   indiePoints: 0,
@@ -13,16 +19,22 @@ export const initialMissionState: Partial<Mission> = {
   completedAt: null,
   expirable: true,
   expiresAt: null,
-  title: '',
-  description: '*', 
+  title: "",
+  description: "*",
 };
 
 export const initialTaskState: Partial<MissionTask> = {
-  id: '',
-  missionId: '',
+  missionId: "",
   userId: null,
   title: null,
   description: null,
+  status: {
+    label: "To Do",
+    value: "IN_TODO",
+    color: "#6b7280",
+  },
+  priority: null,
+  assignees: [],
   indiePoints: 0,
   createdAt: new Date(),
   updatedAt: new Date(),
@@ -30,9 +42,8 @@ export const initialTaskState: Partial<MissionTask> = {
   completed: false,
   expirable: true,
   expiresAt: null,
-  avatar: '',
-  name: '',
-  email: '',
+  avatar: "",
+  name: "",
+  email: "",
   userInfoId: null,
 };
-
