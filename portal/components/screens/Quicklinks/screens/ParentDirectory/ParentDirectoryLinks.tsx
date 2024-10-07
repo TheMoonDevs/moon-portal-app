6b9281ca3e7b14f95f43b566a3076dfcc816504f
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-10-07 12:39:35 +0530

    refactor & cleanup: fetching in parent component to fix the issue

--- portal/components/screens/Quicklinks/screens/ParentDirectory/ParentDirectoryLinks.tsx
+++ portal/components/screens/Quicklinks/screens/ParentDirectory/ParentDirectoryLinks.tsx
@@ -1,59 +1,14 @@
 "use client";
-import { QuicklinksSdk } from "@/utils/services/QuicklinksSdk";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import LinkList from "../../LinkList/LinkList";
-import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
 
-import TopUsedLink from "../Dashboard/TopUsedLink";
-import { Link } from "@prisma/client";
-import useAsyncState from "@/utils/hooks/useAsyncState";
-import { LinkFiltersHeader } from "../../LinkList/LinkFiltersHeader";
-import { useQuickLinkDirs } from "../../hooks/useQuickLinksDirs";
-import { Box, styled, Tab, Tabs } from "@mui/material";
+import LinkList from "../../LinkList/LinkList";
+import { useAppSelector } from "@/utils/redux/store";
 import { ViewButtonGroup } from "../../LinkList/ViewButtonGroup";
 import QuicklinksTabs from "../../elements/Tabs";
-import { useQuickLinkDirectory } from "../../hooks/useQuickLinkDirectory";
-import {
-  setAllQuicklinks,
-  setTopUsedLinksList,
-} from "@/utils/redux/quicklinks/slices/quicklinks.links.slice";
 
-export const ParentDirectoryLinks = () => {
-  const dispatch = useAppDispatch();
-  //   const params = useSearchParams();
-  //   const rootParentDirId = params?.get("id");
-
+export const ParentDirectoryLinks = ({ loading }: { loading: boolean }) => {
   const { allQuicklinks, topUsedLinksList } = useAppSelector(
     (state) => state.quicklinksLinks
   );
-  const { activeDirectoryId } = useQuickLinkDirectory();
-  const { loading, setLoading } = useAsyncState();
-
-  //Fetch link by department Id
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      // try {
-      const reponse = await QuicklinksSdk.getData(
-        `/api/quicklinks/link?rootParentDirId=${activeDirectoryId}`
-      );
-
-      let quicklinks: Link[] = reponse.data.links;
-      dispatch(setAllQuicklinks(quicklinks));
-
-      const sortedQuicklinks = [...quicklinks]
-        .sort((a, b) => b.clickCount - a.clickCount)
-        .slice(0, 5);
-      dispatch(setTopUsedLinksList(sortedQuicklinks));
-
-      setLoading(false);
-      // } catch (error) {
-      //   console.log(error);
-      // }
-    };
-    getData();
-  }, [activeDirectoryId, dispatch, setLoading]);
 
   return (
     <div className="mt-2">
