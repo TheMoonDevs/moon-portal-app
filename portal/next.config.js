fbe1adb3fcd89cfead710aaf3db2841041666e39
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-10-16 00:08:34 +0530

    refactor: update next.config.js and package.json

--- portal/next.config.js
+++ portal/next.config.js
@@ -35,19 +35,30 @@ const nextConfig = {
       },
     ],
   },
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
   experimental: {
+    serverSourceMaps: false,
     //looseMode: true,
     esmExternals: "loose", // <-- add this
     serverComponentsExternalPackages: ["mongoose"], // <-- and this
   },
-  webpack: (config, { isServer }) => {
-    // Experiments configuration
-    config.experiments = {
-      topLevelAwait: true,
-      layers: true,
-    };
-
-    return config;
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    // Important: return the modified config
+    return config
   },
 };
 
