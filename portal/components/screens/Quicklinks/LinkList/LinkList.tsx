ea79860e2886d4d68859350666d46a7229f8117f
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-10-25 13:37:24 +0530

    style: changes the sidebar, search bar and profile dropdown

--- portal/components/screens/Quicklinks/LinkList/LinkList.tsx
+++ portal/components/screens/Quicklinks/LinkList/LinkList.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 
-import { Button, CircularProgress } from "@mui/material";
-import { Link as Quicklink } from "@prisma/client";
-
-import { ViewButtonGroup } from "./ViewButtonGroup";
-import { LinkItem } from "./LinkItem";
-import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
-import Image from "next/image";
-import { setIsCreateLinkModalOpen } from "@/utils/redux/quicklinks/slices/quicklinks.ui.slice";
+import { Button, CircularProgress } from '@mui/material';
+import { Link as Quicklink } from '@prisma/client';
+import { SearchOff } from '@mui/icons-material';
+import { ViewButtonGroup } from './ViewButtonGroup';
+import { LinkItem } from './LinkItem';
+import { useAppDispatch, useAppSelector } from '@/utils/redux/store';
+import { setIsCreateLinkModalOpen } from '@/utils/redux/quicklinks/slices/quicklinks.ui.slice';
+import Image from 'next/image';
 
 export enum VIEW {
-  list = "list",
-  group = "widget",
-  thumbnail = "thumbnail",
-  line = "line",
+  list = 'list',
+  group = 'widget',
+  thumbnail = 'thumbnail',
+  line = 'line',
 }
 
-export type withView = "all" | "widget" | "list" | "thumbnail" | "line";
+export type withView = 'all' | 'widget' | 'list' | 'thumbnail' | 'line';
 
 export default function LinkList({
   allQuicklinks,
-  withView = "all",
+  withView = 'all',
   isLoading,
   inSearchBar,
 }: {
@@ -33,45 +33,52 @@ export default function LinkList({
   const dispatch = useAppDispatch();
   return (
     <>
-      <div className={`w-full ${inSearchBar ? "overflow-hidden" : ""}`}>
+      <div className={`w-full ${inSearchBar ? 'overflow-hidden' : ''}`}>
         {allQuicklinks?.length === 0 && isLoading && (
-          <div className="w-full flex justify-center h-52 items-center ">
+          <div className="flex w-full items-center justify-center">
             <CircularProgress color="inherit" />
           </div>
         )}
 
         {allQuicklinks?.length === 0 && !isLoading ? (
-          <div className="w-full flex flex-col justify-center h-[500px] items-center ">
-            <Image
-              className="rounded-full object-cover"
-              src="/images/nothing-2.png"
-              alt="No data"
-              width={300}
-              height={300}
-            />
+          <div className="flex h-[250px] w-full flex-col items-center justify-center">
+            <div className="flex items-center justify-center">
+              {inSearchBar ? (
+                <SearchOff sx={{ fontSize: 60 }} className="text-red-400" />
+              ) : (
+                <Image
+                  className="rounded-full object-cover"
+                  src="/images/nothing-2.png"
+                  alt="No data"
+                  width={300}
+                  height={300}
+                />
+              )}
+            </div>
             <div>
-              <h1 className="text-gray-400 text-lg">No Quicklink was found!</h1>
+              <h1 className="text-lg text-gray-400">No Quicklink was found!</h1>
             </div>
             <div className="flex items-center gap-5">
-              <h1 className="text-gray-400 text-base">
-                Press{" "}
-                <span className="border-b-2 border-blue-500 border-dashed text-blue-500 text-sm font-semibold">
+              <h1 className="text-base text-gray-400">
+                Press{' '}
+                <span className="border-b-2 border-dashed border-blue-500 text-sm font-semibold text-blue-500">
                   Ctrl + V
-                </span>{" "}
+                </span>{' '}
                 to add one
               </h1>
             </div>
           </div>
         ) : (
           <div
-            className={`pb-6 ${
-              (currentView === VIEW.list && withView === "all") ||
-              (withView === "list"
-                ? "flex flex-col"
-                : currentView === "thumbnail"
-                ? "flex flex-row flex-wrap gap-5"
-                : "grid grid-cols-3 gap-2")
-            } gap-10  w-full`}
+            className={`w-full gap-10 pb-6 ${
+              currentView === 'list' && withView === 'all'
+                ? 'flex flex-col'
+                : withView === 'list'
+                  ? 'flex flex-col'
+                  : currentView === 'thumbnail'
+                    ? 'flex flex-row flex-wrap gap-5'
+                    : ''
+            }`}
           >
             {allQuicklinks?.map((link) => (
               <LinkItem
