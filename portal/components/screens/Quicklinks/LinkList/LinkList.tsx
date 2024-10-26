42f11935861e4e1376c2f3bb8ea055bdc2d7fd9f
Kshitij <kshitij.suraj@gmail.com>
2024-10-26 19:47:32 +0530

    fix: height issue in image if no data was found

--- portal/components/screens/Quicklinks/LinkList/LinkList.tsx
+++ portal/components/screens/Quicklinks/LinkList/LinkList.tsx
@@ -8,6 +8,7 @@ import { LinkItem } from './LinkItem';
 import { useAppDispatch, useAppSelector } from '@/utils/redux/store';
 import { setIsCreateLinkModalOpen } from '@/utils/redux/quicklinks/slices/quicklinks.ui.slice';
 import Image from 'next/image';
+import { cn } from '@/app/lib/utils';
 
 export enum VIEW {
   list = 'list',
@@ -41,11 +42,9 @@ export default function LinkList({
         )}
 
         {allQuicklinks?.length === 0 && !isLoading ? (
-          <div className="flex h-[250px] w-full flex-col items-center justify-center">
-            <div className="flex items-center justify-center">
-              {inSearchBar ? (
-                <SearchOff sx={{ fontSize: 60 }} className="text-red-400" />
-              ) : (
+          !inSearchBar ? (
+            <div className="flex h-[500px] w-full flex-col items-center justify-center">
+              <div className="flex items-center justify-center">
                 <Image
                   className="rounded-full object-cover"
                   src="/images/nothing-2.png"
@@ -53,32 +52,37 @@ export default function LinkList({
                   width={300}
                   height={300}
                 />
-              )}
+              </div>
+              <div>
+                <h1 className="text-lg text-gray-400">
+                  No Quicklink was found!
+                </h1>
+              </div>
+              <div className="flex items-center gap-5">
+                <h1 className="text-base text-gray-400">
+                  Press{' '}
+                  <span className="border-b-2 border-dashed border-blue-500 text-sm font-semibold text-blue-500">
+                    Ctrl + V
+                  </span>{' '}
+                  to add one
+                </h1>
+              </div>
             </div>
-            <div>
+          ) : (
+            <div className="flex h-[250px] w-full flex-col items-center justify-center">
+              <SearchOff sx={{ fontSize: 60 }} className="text-red-400" />
               <h1 className="text-lg text-gray-400">No Quicklink was found!</h1>
             </div>
-            <div className="flex items-center gap-5">
-              <h1 className="text-base text-gray-400">
-                Press{' '}
-                <span className="border-b-2 border-dashed border-blue-500 text-sm font-semibold text-blue-500">
-                  Ctrl + V
-                </span>{' '}
-                to add one
-              </h1>
-            </div>
-          </div>
+          )
         ) : (
           <div
-            className={`w-full gap-10 pb-6 ${
-              currentView === 'list' && withView === 'all'
-                ? 'flex flex-col'
-                : withView === 'list'
-                  ? 'flex flex-col'
-                  : currentView === 'thumbnail'
-                    ? 'flex flex-row flex-wrap gap-5'
-                    : ''
-            }`}
+            className={cn(
+              'py-4',
+              currentView === VIEW.thumbnail &&
+                'flex flex-wrap items-center gap-4',
+              currentView === VIEW.group &&
+                'grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4',
+            )}
           >
             {allQuicklinks?.map((link) => (
               <LinkItem
