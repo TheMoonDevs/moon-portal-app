ed245fe5838431e13f71f59e2b116ee90d755633
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2024-10-26 08:25:13 +0530

    fix: added wallet connect id

--- payzone/utils/configure/WalletWagmiProvider.tsx
+++ payzone/utils/configure/WalletWagmiProvider.tsx
@@ -13,6 +13,7 @@ import {
   rainbowWallet,
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
+import { APP_INFO } from "../constants/appInfo";
 
 coinbaseWallet.preference = "all";
 
@@ -32,8 +33,8 @@ const connectors = connectorsForWallets(
     },
   ],
   {
-    appName: "Your App Name",
-    projectId: "<YOUR WALLETCONNECT PROJECT ID>",
+    appName: APP_INFO.name,
+    projectId: "2f785c1b9ea689743190f2e9871ce3df",
   }
 );
 
