db1c7a1ea44aac880f0da4db8b5beb50d1e0d21d
Manish Guhe <manishguhe301@gmail.com>
2024-10-26 17:54:09 +0530

    fix: pointers -> schema, Ui fixed, integrated emoji reply feature, and some minor changes, build fixed

--- portal/components/screens/Worklogs/WorklogSummary/ReplyBox.tsx
+++ portal/components/screens/Worklogs/WorklogSummary/ReplyBox.tsx
@@ -24,28 +24,27 @@ export const ReplyBox: React.FC<ReplyBoxProps> = ({
         isReplying ? 'max-h-60' : 'max-h-0'
       } ${isChatCard && 'overflow-hidden'}`}
     >
-      <div className='flex items-center bg-neutral-100 p-3 rounded-lg border border-neutral-300 gap-2'>
+      <div className="relative"> {/* Add relative positioning for the container */}
         <textarea
-          className='flex-grow w-full p-3 rounded-lg bg-neutral-50 text-neutral-700 border-none focus:outline-none resize-none h-16'
+          className='flex-grow w-full p-3 rounded-lg bg-neutral-50 text-neutral-700 border-none focus:outline-none resize-none h-24 pr-14' // Add padding-right to accommodate the button
           value={inputValue}
           onChange={handleInputChange}
-          placeholder='Type a message...'
+          placeholder='Enter a comment'
           disabled={isSending}
         />
-        <IconButton
+        <button
           onClick={handleReplySend}
           disabled={!inputValue.trim() || isSending}
-          title='Send Message'
-          className={`ml-2 transition-colors text-[#414bea] ${
-            !inputValue.trim() ? 'text-neutral-400' : 'text-blue-500'
+          className={`absolute bottom-5 right-4 py-1 px-4 rounded-md ${
+            !inputValue.trim() ? 'text-neutral-400 bg-gray-100' : 'bg-[#6b4bf2] text-white'
           }`}
         >
           {isSending ? (
-            <CircularProgress size={16} />
+            <CircularProgress size={16} color='inherit'/>
           ) : (
-            <span className='material-symbols-outlined'>send</span>
+            'Send'
           )}
-        </IconButton>
+        </button>
       </div>
     </div>
   );
