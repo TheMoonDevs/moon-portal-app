db33908b0fd0cedd4f59324cc449f63ce4efc770
Manish Guhe <manishguhe301@gmail.com>
2024-10-26 22:57:12 +0530

    fix: analytics ui

--- portal/components/screens/Worklogs/WorklogSummary/StatiStics.tsx
+++ portal/components/screens/Worklogs/WorklogSummary/StatiStics.tsx
@@ -12,22 +12,22 @@ const StatiStics = ({ worklogSummary }: { worklogSummary: WorkLogs[] }) => {
   const weekData = getWeekData(groupedByWeek);
 
   return (
-    <div className='pt-5 w-full flex justify-center'>
-      <div className='overflow-x-auto'>
-        <table className='w-full table-auto rounded-lg border border-gray-200'>
-          <thead className='bg-gray-200'>
-            <tr className='text-left'>
-              <th className='p-4 text-sm font-semibold text-gray-600'></th>
+    <div className="flex w-full justify-center pt-5">
+      <div className="overflow-x-auto rounded-lg">
+        <table className="w-full table-auto border border-gray-200">
+          <thead className="bg-gray-200">
+            <tr className="text-left">
+              <th className="p-4 text-sm font-semibold text-gray-600"></th>
               {weekData.map((week, idx) => {
                 const [weekNumber, weekDates] = week.weekLabel.split(' (');
                 return (
                   <th
                     key={idx}
-                    className='p-4 text-sm font-semibold text-gray-700'
+                    className="p-4 text-sm font-semibold text-gray-700"
                   >
-                    <div className='flex flex-col items-center'>
+                    <div className="flex flex-col items-center">
                       <span>{weekNumber}</span>
-                      <span className='text-xs text-gray-500'>
+                      <span className="text-xs text-gray-500">
                         ({weekDates}
                       </span>
                     </div>
@@ -37,8 +37,8 @@ const StatiStics = ({ worklogSummary }: { worklogSummary: WorkLogs[] }) => {
             </tr>
           </thead>
           <tbody>
-            <tr className='bg-white'>
-              <td className='p-4 text-sm font-semibold text-gray-600'>
+            <tr className="bg-white">
+              <td className="p-4 text-sm font-semibold text-gray-600">
                 Task Completion
               </td>
               {weekData.map((week, idx) => {
@@ -46,34 +46,52 @@ const StatiStics = ({ worklogSummary }: { worklogSummary: WorkLogs[] }) => {
                   idx > 0 ? weekData[idx - 1].totalTasks : 0;
                 const percentageChange = calculatePercentageChange(
                   week.totalTasks,
-                  prevTotalTasks
+                  prevTotalTasks,
                 );
                 const trendColor =
                   percentageChange > 0
                     ? 'text-green-500'
                     : percentageChange < 0
-                    ? 'text-red-500'
-                    : 'text-gray-500';
+                      ? 'text-red-500'
+                      : 'text-gray-500';
+
+                const completionPercentage =
+                  (week.completedTasks / week.totalTasks) * 100;
 
                 return (
                   <td
                     key={idx}
-                    className='p-4 text-sm font-medium text-gray-600 text-center'
+                    className="p-4 text-center text-sm font-medium text-gray-600"
                   >
-                    <div className='flex flex-col items-center'>
+                    <div className="flex flex-col items-center">
                       <div>
-                        {week.completedTasks} / {week.totalTasks}
+                        <span>
+                          {week.completedTasks} / {week.totalTasks}
+                        </span>
+                        {idx === 0 && (
+                          <div className="mt-1 flex flex-col items-center justify-center">
+                            <span
+                              className="material-symbols-outlined text-xs text-blue-500"
+                              style={{ fontSize: '20px' }}
+                            >
+                              start
+                            </span>
+                            <span className="text-xs text-gray-500">
+                              ({completionPercentage.toFixed(1)}%)
+                            </span>{' '}
+                          </div>
+                        )}
                       </div>
                       {idx > 0 && (
                         <div
-                          className={`text-xs mt-1 ${trendColor} flex flex-col items-center justify-center`}
+                          className={`mt-1 text-xs ${trendColor} flex flex-col items-center justify-center`}
                         >
-                          <span className='material-symbols-outlined'>
+                          <span className="material-symbols-outlined">
                             {percentageChange > 0
                               ? 'arrow_drop_up'
                               : percentageChange < 0
-                              ? 'arrow_drop_down'
-                              : 'horizontal_rule'}
+                                ? 'arrow_drop_down'
+                                : 'horizontal_rule'}
                           </span>
                           {percentageChange.toFixed(1)}%
                         </div>
@@ -83,40 +101,40 @@ const StatiStics = ({ worklogSummary }: { worklogSummary: WorkLogs[] }) => {
                 );
               })}
             </tr>
-            <tr className='bg-gray-50'>
-              <td className='p-4 text-sm font-semibold text-gray-600'>
+            <tr className="bg-gray-50">
+              <td className="p-4 text-sm font-semibold text-gray-600">
                 Missed Logs
               </td>
               {weekData.map((week, idx) => (
                 <td
                   key={idx}
-                  className='p-4 text-sm font-medium text-gray-600 text-center'
+                  className="p-4 text-center text-sm font-medium text-gray-600"
                 >
                   {week.missedLogs}
                 </td>
               ))}
             </tr>
-            <tr className='bg-white'>
-              <td className='p-4 text-sm font-semibold text-gray-600'>
+            <tr className="bg-white text-[10px]">
+              <td className="px-4 py-2 font-semibold text-gray-600">
                 Most Productive <br /> Day
               </td>
               {weekData.map((week, idx) => (
                 <td
                   key={idx}
-                  className='p-4 text-sm font-medium text-gray-600 text-center'
+                  className="px-4 py-2 text-center font-medium text-gray-600"
                 >
                   {week.mostProductiveDay}
                 </td>
               ))}
             </tr>
-            <tr className='bg-gray-50'>
-              <td className='p-4 text-sm font-semibold text-gray-600'>
+            <tr className="bg-gray-50 text-[10px]">
+              <td className="px-4 py-2 font-semibold text-gray-600">
                 Least Productive <br /> Day
               </td>
               {weekData.map((week, idx) => (
                 <td
                   key={idx}
-                  className='p-4 text-sm font-medium text-gray-600 text-center'
+                  className="px-4 py-2 text-center font-medium text-gray-600"
                 >
                   {week.leastProductiveDay}
                 </td>
