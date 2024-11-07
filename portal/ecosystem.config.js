c3075cdb68ae0b875e02793fb9eb855ac7e6abdf
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-11-07 06:33:09 +0530

    fix: configure PM2 to use fork mode and set instance count for cron runner

--- portal/ecosystem.config.js
+++ portal/ecosystem.config.js
@@ -4,9 +4,10 @@ module.exports = {
             name: "cron-runner",
             script: "./cron/run-crons.js",
             cron_restart: "0,30 * * * *", // Runs at 0 and 30 minutes of every hour
+            instances: 1,
+            exec_mode: 'fork',
             watch: false,
-            autorestart: false,
-            time: true
+            autorestart: false
         }
     ]
 };
