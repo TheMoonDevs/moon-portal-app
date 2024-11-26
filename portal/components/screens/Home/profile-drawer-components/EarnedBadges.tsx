415cb6b2b8dcb8cd9b9dd177400a03e928264f42
Manish <manishguhe301.com>
2024-11-26 17:27:09 +0530

    fix: responsiveness

--- portal/components/screens/Home/profile-drawer-components/EarnedBadges.tsx
+++ portal/components/screens/Home/profile-drawer-components/EarnedBadges.tsx
@@ -37,31 +37,33 @@ const EarnedBadges = () => {
   return (
     <div className="pb-3">
       <h6 className="pb-2 font-bold">Achievements</h6>
-      <div className="">
+      <div className="flex justify-center">
         {loading ? (
           <div className="flex items-center justify-center">
             <BadgeLoader />
           </div>
         ) : (badgeRewarded?.length ?? 0 > 0) ? (
-          <div className="custom-scrollbar flex flex-row items-center gap-4 overflow-x-scroll">
-            {badgeRewarded?.map((badge: BadgeRewarded) => (
-              <div
-                className="flex w-24 flex-col items-center justify-center gap-1"
-                key={badge.id}
-              >
-                <div className="hover:before:animate-shine focus:before:animate-shine relative h-full w-24 overflow-hidden rounded-full before:absolute before:left-[-75%] before:top-0 before:z-[2] before:h-full before:w-1/4 before:skew-x-[-25deg] before:transform before:bg-[linear-gradient(to_right,rgba(255,255,255,0.4)_0%,rgba(255,255,255,0.4)_100%)] before:content-['']">
-                  <img
-                    key={badge.id}
-                    src={badge.imageUrl || ''}
-                    alt={badge.name}
-                    className="h-24 w-24 rounded-full border-4 border-gray-300 object-cover shadow-lg"
-                  />
+          <div className="flex w-full justify-center">
+            <div className="mx-auto flex w-full flex-row flex-wrap items-center justify-center gap-2 max-sm:justify-between">
+              {badgeRewarded?.map((badge: BadgeRewarded) => (
+                <div
+                  className="flex w-24 flex-col items-center justify-center gap-1 max-sm:w-20"
+                  key={badge.id}
+                >
+                  <div className="hover:before:animate-shine focus:before:animate-shine relative h-full w-24 overflow-hidden rounded-full before:absolute before:left-[-75%] before:top-0 before:z-[2] before:h-full before:w-1/4 before:skew-x-[-25deg] before:transform before:bg-[linear-gradient(to_right,rgba(255,255,255,0.4)_0%,rgba(255,255,255,0.4)_100%)] before:content-[''] max-sm:w-20">
+                    <img
+                      key={badge.id}
+                      src={badge.imageUrl || ''}
+                      alt={badge.name}
+                      className="h-24 w-24 rounded-full border-4 border-gray-300 object-cover shadow-lg max-sm:h-20 max-sm:w-20"
+                    />
+                  </div>
+                  <p className="px-1 text-center text-[11px] font-semibold capitalize max-sm:text-[9px]">
+                    {badge.name}
+                  </p>
                 </div>
-                <p className="px-1 text-center text-xs font-semibold capitalize">
-                  {badge.name}
-                </p>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         ) : (
           <div className="flex items-center justify-center">
