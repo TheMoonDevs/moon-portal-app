8977320df13ada003276c5374697d2ec22aa9a0e
Manish <manishguhe301@gmail.com>
2025-02-04 14:11:17 +0530

    feat: fetching users and worklogs of only today and yesterday

--- portal/app/api/cron-jobs/core-team/worklog-summary-reminder/route.ts
+++ portal/app/api/cron-jobs/core-team/worklog-summary-reminder/route.ts
@@ -1,12 +1,47 @@
 import { prisma } from "@/prisma/prisma";
+import { User } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
+import { formatISO, startOfDay, subDays } from "date-fns";
 
 
 export async function GET(request: NextRequest) {
+  const today = formatISO(startOfDay(new Date()));
+  const yesterday = formatISO(startOfDay(subDays(new Date(), 1)));
+
+  const users = await prisma.user.findMany({
+    where: {
+      userType: "MEMBER",
+      role: "CORETEAM",
+      status: "ACTIVE",
+      // id: "665bfefcc58926c7f6935c0c", //remove and add your user id for testing
+    },
+  });
+
+  const usersWithWorkLogs = await Promise.all(
+    users.map(async (user: User) => {
+      const workLogs = await prisma.workLogs.findMany({
+        where: {
+          userId: user.id,
+          date: {
+            gte: yesterday.split('T')[0],
+            lte: today.split('T')[0],
+          },
+        },
+      });
+
+      return {
+        userName: user.name,
+        workLogs,
+        slackId: user.slackId,
+      };
+    })
+  );
+
   try {
     const jsonResponse = {
       status: "success",
       message: "Reminders Sent!",
+      usersWithWorkLogs,
     };
 
     return NextResponse.json(jsonResponse);
