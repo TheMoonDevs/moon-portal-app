6fe9c68c92bc2b541f7affcfed0d39d530956a99
Kshitij <kshitij.suraj@gmail.com>
2025-02-04 18:52:57 +0530

    fix: private log is hidden in mobile and tablet

--- portal/components/screens/Worklogs/WorklogViewPageWrapper.tsx
+++ portal/components/screens/Worklogs/WorklogViewPageWrapper.tsx
@@ -1,27 +1,28 @@
-"use client";
+'use client';
 
-import { useSearchParams } from "next/navigation";
-import { WorklogView } from "./WorklogView";
-import { useMediaQuery } from "@mui/material";
-import media from "@/styles/media";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import store from "@/utils/redux/store";
-import { PortalSdk } from "@/utils/services/PortalSdk";
-import { WorkLogsHelper } from "./WorklogsHelper";
-import { WorkLogs } from "@prisma/client";
+import { useSearchParams } from 'next/navigation';
+import { WorklogView } from './WorklogView';
+import { useMediaQuery } from '@mui/material';
+import media from '@/styles/media';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import store from '@/utils/redux/store';
+import { PortalSdk } from '@/utils/services/PortalSdk';
+import { WorkLogsHelper } from './WorklogsHelper';
+import { WorkLogs } from '@prisma/client';
+import { PrivateWorklogView } from './PrivateWorklogView';
 
 export const WorklogViewPageWrapper = ({ id }: { id: string }) => {
   const queryParams = useSearchParams();
-  const _date = queryParams?.get("date");
-  const _logType = queryParams?.get("logType");
+  const _date = queryParams?.get('date');
+  const _logType = queryParams?.get('logType');
   const isTabletOrMore = useMediaQuery(media.moreTablet);
 
   const [logsList, setLogsList] = useState<WorkLogs[]>([]);
 
   useEffect(() => {
     if (!isTabletOrMore) return;
-    if (_logType != "dayLog") return;
+    if (_logType != 'dayLog') return;
     const _user = store.getState().auth.user;
     const _total_days_in_month = dayjs(_date).daysInMonth();
     const _logList = Array.from({
@@ -30,7 +31,7 @@ export const WorklogViewPageWrapper = ({ id }: { id: string }) => {
       const _date2 = dayjs()
         .month(dayjs(_date).month())
         .date(i + 1)
-        .format("YYYY-MM-DD");
+        .format('YYYY-MM-DD');
       // const _worklog = yearLogData?.data?.workLogs.find(
       //   (wl: WorkLogs) => wl.date === _date2
       // );
@@ -40,27 +41,27 @@ export const WorklogViewPageWrapper = ({ id }: { id: string }) => {
     setLogsList(_logList);
   }, [isTabletOrMore, _logType, _date]);
 
-  if (isTabletOrMore && _logType === "dayLog" && _date && _date?.length > 0) {
+  if (isTabletOrMore && _logType === 'dayLog' && _date && _date?.length > 0) {
     const centerdate = dayjs(_date);
-    const leftdate = centerdate.subtract(1, "day");
-    const rightdate = centerdate.add(1, "day");
+    const leftdate = centerdate.subtract(1, 'day');
+    const rightdate = centerdate.add(1, 'day');
 
     return (
-      <div className="bg-white flex flex-row flex-wrap items-center justify-start gap-4 h-screen ">
-        <div className="my-4 h-[90vh] bg-white overflow-y-scroll">
-          <div className="flex flex-col max-w-[400px] ">
+      <div className="flex h-screen flex-row flex-wrap items-center justify-start gap-4 bg-white">
+        <div className="my-4 h-[90vh] overflow-y-scroll bg-white">
+          <div className="flex max-w-[400px] flex-col">
             {logsList.map((log, _i) => (
               <div
                 key={log.id + _i}
-                className={`p-4 border-b text-black ${
-                  log.date === dayjs().format("YYYY-MM-DD")
-                    ? "text-blue-500 font-bold"
-                    : log.date === dayjs(centerdate).format("YYYY-MM-DD")
-                    ? "font-bold"
-                    : ""
+                className={`border-b p-4 text-black ${
+                  log.date === dayjs().format('YYYY-MM-DD')
+                    ? 'font-bold text-blue-500'
+                    : log.date === dayjs(centerdate).format('YYYY-MM-DD')
+                      ? 'font-bold'
+                      : ''
                 }`}
               >
-                <p>{dayjs(log.date).format("MMM DD")}</p>
+                <p>{dayjs(log.date).format('MMM DD')}</p>
               </div>
             ))}
           </div>
@@ -72,11 +73,15 @@ export const WorklogViewPageWrapper = ({ id }: { id: string }) => {
             logType={_logType}
           />
         </div> */}
-        <div className="my-4 h-[90vh] bg-white">
+        <div className="my-4 h-[90vh] overflow-y-scroll bg-white">
           <WorklogView
             id={id}
-            date={centerdate.format("YYYY-MM-DD")}
+            date={centerdate.format('YYYY-MM-DD')}
             logType={_logType}
+          />
+          <PrivateWorklogView
+            date={centerdate.format('YYYY-MM-DD')}
+            logType={'privateWorklogs'}
           />
         </div>
         {/* <div className="my-4 h-[90vh] bg-white">
@@ -90,5 +95,13 @@ export const WorklogViewPageWrapper = ({ id }: { id: string }) => {
     );
   }
 
-  return <WorklogView id={id} date={_date} logType={_logType} />;
+  return (
+    <div className="my-4 h-[90vh] overflow-y-scroll bg-white">
+      <WorklogView id={id} date={_date} logType={_logType} />;
+      <PrivateWorklogView
+        date={dayjs(_date)?.format('YYYY-MM-DD')}
+        logType={'privateWorklogs'}
+      />
+    </div>
+  );
 };
