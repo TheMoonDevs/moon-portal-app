9cae49971e0a37228a3de9f38b21239fef2fcb2b
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-02-06 22:20:38 +0530

    build: set runtime to experimental-edge in middleware

--- home/middleware.ts
+++ home/middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const runtime = 'experimental-edge';
+
 export async function middleware(req: NextRequest) {
   const slug = req.nextUrl.pathname.split("/").pop();
   try {
