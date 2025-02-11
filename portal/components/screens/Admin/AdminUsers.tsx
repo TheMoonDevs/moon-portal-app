67b69c6351a83c33b4e8c939ce62d0f08ad4707a
Manish <manishguhe301@gmail.com>
2025-02-11 22:46:07 +0530

    fix: minor Ux fixes admin users component

--- portal/components/screens/Admin/AdminUsers.tsx
+++ portal/components/screens/Admin/AdminUsers.tsx
@@ -10,54 +10,61 @@ import { PortalSdk } from "@/utils/services/PortalSdk";
 import { User } from "@prisma/client";
 import { Spinner } from "@/components/elements/Loaders";
 
-export const AdminUsers = ({users, loading} : {
+export const AdminUsers = ({
+  users,
+  loading,
+}: {
   users: User[];
   loading: boolean;
 }) => {
   const router = useRouter();
   return (
-    <MobileBox>
-      <p className="text-neutral-400 tracking-[0.5em] uppercase text-xs text-center mb-4">
+    <MobileBox customClass="!w-[50%]">
+      <p className="mb-4 text-center text-xs uppercase tracking-[0.5em] text-neutral-400">
         CLIENTS / MEMBER
       </p>
 
       {loading ? (
-        <div className="flex items-center justify-center h-full">
+        <div className="flex h-full items-center justify-center">
           <Spinner />
         </div>
       ) : (
-        <div className="flex flex-col grow gap-4 my-2 justify-start max-h-full overflow-y-auto scrollbar">
+        <div className="scrollbar my-2 flex max-h-full w-[90%] grow flex-col justify-start gap-4 overflow-y-auto">
           {users.map((user) => (
-            <Link
+            <div
               key={user.id}
-              className="flex flex-row gap-4 items-center justify-center border border-neutral-700 hover:bg-neutral-800 px-4 py-2 rounded-lg cursor-pointer"
-              href={`${APP_ROUTES.userEditor}?id=${user.id}`}
-              onClick={() => {
-                console.log("clicked", user.id);
-                //router.push(`${APP_ROUTES.userEditor}?id=${user.id}`);
-              }}
+              className="flex flex-row items-center justify-between rounded-lg border border-neutral-700 px-4 py-2 hover:bg-neutral-800"
             >
-              <div className=" rounded-full p-1 ">
-                <img
-                  src={user?.avatar || undefined}
-                  alt={user?.name || ""}
-                  className="w-12 h-12 object-cover object-center rounded-full "
-                />
-              </div>
-              <div>
-                <p className="text-neutral-300">{user.name}</p>
-                <p className="text-sm text-neutral-600">
-                  {user.userType} | {user.username}-{user.password}
-                </p>
-              </div>
+              <Link
+                className="flex cursor-pointer flex-row items-center justify-center gap-4 rounded-lg px-4 py-2 hover:bg-neutral-800"
+                href={`${APP_ROUTES.userEditor}?id=${user.id}`}
+                onClick={() => {
+                  console.log("clicked", user.id);
+                  //router.push(`${APP_ROUTES.userEditor}?id=${user.id}`);
+                }}
+              >
+                <div className="rounded-full p-1">
+                  <img
+                    src={user?.avatar || undefined}
+                    alt={user?.name || ""}
+                    className="h-12 w-12 rounded-full object-cover object-center"
+                  />
+                </div>
+                <div>
+                  <p className="text-neutral-300">{user.name}</p>
+                  <p className="text-sm text-neutral-600">
+                    {user.userType} | {user.username}-{user.password}
+                  </p>
+                </div>
+              </Link>
               <span className="material-icons text-neutral-400">
                 chevron_right
               </span>
-            </Link>
+            </div>
           ))}
         </div>
       )}
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4">
         <Link href={APP_ROUTES.userEditor}>
           <GreyButton rightIcon={"add"}>Add New User</GreyButton>
         </Link>
