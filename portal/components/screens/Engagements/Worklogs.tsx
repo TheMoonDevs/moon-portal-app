42403acdce01e463ccc65bd45b9acfa6dd737606
Manish <manishguhe301@gmail.com>
2025-02-16 18:06:19 +0530

    feat: integrated stats for content

--- portal/components/screens/Engagements/Worklogs.tsx
+++ portal/components/screens/Engagements/Worklogs.tsx
@@ -1,6 +1,7 @@
 import { MdxAppEditor } from '@/utils/configure/MdxAppEditor';
 import { User, WorkLogs } from '@prisma/client';
 import dayjs from 'dayjs';
+import { getStatsOfContent } from '../Worklogs/WorklogEditor';
 
 interface WorkLogsByDate {
   [date: string]: WorkLogs[];
@@ -38,37 +39,55 @@ const EngagementWorklogs = ({
           </h1>
           {workLogsForDate.map((workLog) => (
             <div key={workLog.id} className="my-2 rounded-lg bg-white px-6">
-              {workLog.works?.map((work) => (
-                <div
-                  key={`${workLog.createdAt}-index-${workLog.id}`}
-                  className="flex flex-col items-start gap-0"
-                >
-                  {team
-                    .filter((user) => user.id === workLog.userId)
-                    .map((user) => (
-                      <div
-                        key={user.id}
-                        className="flex flex-row items-center gap-2"
-                      >
-                        <img
-                          src={user.avatar || '/images/avatar.png'}
-                          alt={user.name || ''}
-                          className="mt-1 h-8 w-8 rounded-full object-cover"
-                        />
-                        <span className="text-sm">{user.name}</span>
-                      </div>
-                    ))}
-                  <div className="ml-4 flex-grow">
-                    <MdxAppEditor
-                      readOnly
-                      markdown={(work as { content: string }).content || ''}
-                      contentEditableClassName="summary_mdx flex flex-col gap-4 z-1 mb-[-20px] ml-3"
-                      editorKey={'engagement-mdx'}
-                      className="z-1"
-                    />
+              {workLog.works?.map((work) => {
+                const user = team.find((u) => u.id === workLog.userId);
+                const markdownData = workLog?.works[0];
+                const stats =
+                  markdownData &&
+                  typeof markdownData === 'object' &&
+                  'content' in markdownData
+                    ? getStatsOfContent(markdownData.content as string)
+                        .split(' / ')
+                        .map(Number)
+                    : [0, 0];
+                const [completed, total] = stats;
+                return (
+                  <div
+                    key={`${workLog.createdAt}-index-${workLog.id}`}
+                    className="flex flex-col items-start gap-0"
+                  >
+                    {team
+                      .filter((user) => user.id === workLog.userId)
+                      .map((user) => (
+                        <div
+                          key={user.id}
+                          className="flex flex-row items-center gap-2"
+                        >
+                          <img
+                            src={user.avatar || '/images/avatar.png'}
+                            alt={user.name || ''}
+                            className="mt-1 h-[30px] w-[30px] rounded-full object-cover"
+                          />
+                          <span className="text-[0.8em] uppercase tracking-widest text-neutral-500">
+                            {user?.name} -{' '}
+                            <span className="font-semibold">
+                              {completed} / {total}
+                            </span>
+                          </span>
+                        </div>
+                      ))}
+                    <div className="ml-4 flex-grow">
+                      <MdxAppEditor
+                        readOnly
+                        markdown={(work as { content: string }).content || ''}
+                        contentEditableClassName="summary_mdx flex flex-col gap-4 z-1 mb-[-20px] ml-3"
+                        editorKey={'engagement-mdx'}
+                        className="z-1"
+                      />
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
           ))}
         </div>
