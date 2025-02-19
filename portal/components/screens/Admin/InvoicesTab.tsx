260dcf5ddc167b1b3aa67dd080e8b571666f10f2
Kshitij <kshitij.suraj@gmail.com>
2025-02-20 02:12:35 +0530

    feat: form ui, create invoice and file upload feature

--- portal/components/screens/Admin/InvoicesTab.tsx
+++ portal/components/screens/Admin/InvoicesTab.tsx
@@ -9,7 +9,12 @@ import ToolTip from '@/components/elements/ToolTip';
 import { Autocomplete, Chip, IconButton, TextField } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import { useUser } from '@/utils/hooks/useUser';
+import { FileWithPath } from '@mantine/dropzone';
+import { TMD_PORTAL_API_KEY } from '@/utils/constants/appInfo';
+import Link from 'next/link';
+import { toast } from 'sonner';
 type InvoiceFormState = {
   clientId: string;
   startDate: string | null | Dayjs;
@@ -21,6 +26,7 @@ type InvoiceFormState = {
   amountTotal: number;
   amountToPay: number;
   amountDiscount: number;
+  isInvoicePaid: boolean;
   payType: PayType;
   invoicePdf: string; // Store file URL or path
   workInfo: any;
@@ -54,10 +60,64 @@ const InvoicesTab = ({
     amountTotal: 0,
     amountToPay: 0,
     amountDiscount: 0,
+    isInvoicePaid: false,
     payType: PayType.BANK,
     invoicePdf: '', // Store file URL or path
     workInfo: {},
   });
+
+  const [files, setFiles] = useState<File | FileList | FileWithPath>();
+  const [isFileUploading, setIsFileUploading] = useState<boolean>(false);
+  const { user } = useUser();
+
+  const handleFileDrop = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (files) setFiles(undefined);
+    const droppedFiles = event.target.files?.item(0);
+    console.log(droppedFiles);
+    if (droppedFiles) setFiles(droppedFiles);
+  };
+
+  const handleUpload = async () => {
+    setIsFileUploading(true);
+    if (files) {
+      const formData = new FormData();
+      formData.append('file', files as File, (files as FileWithPath).path);
+      if (user) {
+        const userId = user.id;
+        formData.append('userId', userId);
+      }
+      formData.append('folderName', 'invoices');
+      try {
+        const response = await fetch('/api/upload/file-upload', {
+          method: 'POST',
+          body: formData,
+          headers: {
+            tmd_portal_api_key: TMD_PORTAL_API_KEY,
+          },
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          const fileUrl = responseData.fileInfo[0].fileUrl;
+
+          setFormData((prevFormData) => ({
+            ...prevFormData,
+            invoicePdf: fileUrl, // Store file URL or path
+          }));
+          console.log('File uploaded successfully!');
+          setIsFileUploading(false);
+        } else {
+          // Handle error
+          setIsFileUploading(false);
+          console.error('Failed to upload file:', response.statusText);
+        }
+      } catch (error) {
+        setIsFileUploading(false);
+        console.error('Error uploading file:', error);
+        // Handle error
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchInvoicesByClientId = async () => {
@@ -78,8 +138,63 @@ const InvoicesTab = ({
       fetchInvoicesByClientId();
     }
   }, [selectedClientId]);
+
+  const filePreview = () => {
+    const pdf = files as File;
+    const fileUrl = URL.createObjectURL(pdf);
+    return (
+      <Link
+        href={fileUrl}
+        target="_blank"
+        className="mt-2 flex items-center gap-2 text-sm text-blue-500"
+      >
+        <span className="icon_size material-icons">picture_as_pdf</span>
+        <p className="underline">{pdf.name}</p>
+        <span className="icon_size material-symbols-outlined">
+          {' '}
+          open_in_new
+        </span>
+      </Link>
+    );
+  };
+
+  const resetForm = () => {
+    setFormData({
+      clientId: selectedClientId || '',
+      startDate: null,
+      endDate: null,
+      dueDate: null,
+      title: '',
+      description: '',
+      devIds: [], // Store user IDs as an array instead of a relation
+      amountTotal: 0,
+      amountToPay: 0,
+      amountDiscount: 0,
+      isInvoicePaid: false,
+      payType: PayType.BANK,
+      invoicePdf: '', // Store file URL or path
+      workInfo: {},
+    });
+  };
+
   const handleUpdate = async (e: FormEvent) => {};
-  const handleFormSubmit = async (e: FormEvent) => {};
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoadingState({ ...loadingState, adding: true });
+
+      // upload the pdf file to the server
+      await handleUpload();
+      console.log(formData.invoicePdf);
+
+      const res = await PortalSdk.postData('/api/client-invoice', formData);
+      toast.success('Invoice added successfully');
+      setLoadingState({ ...loadingState, adding: false });
+    } catch (error) {
+      console.error(error);
+      setLoadingState({ ...loadingState, adding: false });
+    }
+  };
   const handleDelete = async (id: string) => {};
   const handleEditInvoice = (invoice: Invoice) => {};
 
@@ -130,7 +245,24 @@ const InvoicesTab = ({
                 setFormData({ ...formData, title: e.target.value })
               }
               className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
-              placeholder="Enter engagement title..."
+              placeholder="Enter invoice title..."
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="description"
+              className="mb-1 block text-sm font-medium text-neutral-300"
+            >
+              Invoice Description
+            </label>
+            <textarea
+              id="description"
+              value={formData.description}
+              onChange={(e) =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+              className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
+              placeholder="Enter invoice description..."
             />
           </div>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -187,6 +319,46 @@ const InvoicesTab = ({
                   setFormData({
                     ...formData,
                     endDate: newValue,
+                  });
+                }}
+                sx={{
+                  border: '1px solid #737373',
+                  borderRadius: '4px',
+                  width: '100%',
+                  backgroundColor: '#262626',
+                  '& .MuiPaper-root': {
+                    '& .MuiPickersLayout-root': {
+                      '& MuiDateCalendar-root': {
+                        backgroundColor: '#1f1f1f !important',
+                      },
+                    },
+                  },
+                  '& .MuiDateCalendar-root': {
+                    backgroundColor: '#1f1f1f !important',
+                  },
+                  '& .MuiInputBase-input': {
+                    color: 'white !important',
+                  },
+                  '& .MuiButtonBase-root': {
+                    color: 'white !important',
+                  },
+                }}
+                format="DD-MM-YYYY"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="dueDate"
+                className="mb-1 block text-sm font-medium text-neutral-300"
+              >
+                Select due date
+              </label>
+              <DatePicker
+                value={formData.dueDate}
+                onChange={(newValue) => {
+                  setFormData({
+                    ...formData,
+                    dueDate: newValue,
                   });
                 }}
                 sx={{
@@ -275,21 +447,21 @@ const InvoicesTab = ({
               />
             </div>
           </LocalizationProvider>
-          {/* <div className="mb-5">
+          <div className="mb-5">
             <label
-              htmlFor="isActive"
+              htmlFor="isPaid"
               className="mb-1 block text-sm font-medium text-neutral-300"
             >
-              Is Engagement Active
+              Is Invoice Paid?
             </label>
             <select
               id="isActive"
               name="isActive"
-              value={formData.isActive.toString()}
+              value={formData.isInvoicePaid.toString()}
               onChange={(e) =>
                 setFormData({
                   ...formData,
-                  isActive: e.target.value === 'true',
+                  isInvoicePaid: e.target.value === 'true',
                 })
               }
               className="block w-full rounded-md border border-neutral-300 bg-neutral-800 p-2 text-neutral-300 focus:border-neutral-500 focus:outline-none focus:ring focus:ring-neutral-500"
@@ -297,68 +469,118 @@ const InvoicesTab = ({
               <option value="true">Yes</option>
               <option value="false">No</option>
             </select>
-          </div> */}
-          {/* <div className="mb-5">
+          </div>
+          <div className="mb-5">
             <label
-              htmlFor="engagementType"
+              htmlFor="payType"
               className="mb-1 block text-sm font-medium text-neutral-300"
             >
-              Engagement Type
+              Payment Type
             </label>
             <select
-              id="engagementType"
-              name="engagementType"
-              value={formData.engagementType || ''}
+              id="payType"
+              name="payType"
+              value={formData.payType || ''}
               onChange={(e) =>
                 setFormData({
                   ...formData,
-                  engagementType: e.target.value as ENGAGEMENTTYPE,
+                  payType: e.target.value as PayType,
                 })
               }
               className="block w-full rounded-md border border-neutral-300 bg-neutral-800 p-2 text-neutral-300 focus:border-neutral-500 focus:outline-none focus:ring focus:ring-neutral-500"
             >
-              <option value={''}>Select Engagement Type</option>
-              <option value={ENGAGEMENTTYPE.HOURLY}>Hourly</option>
-              <option value={ENGAGEMENTTYPE.PART_TIME}>Part Time</option>
-              <option value={ENGAGEMENTTYPE.FULL_TIME}>Full Time</option>
-              <option value={ENGAGEMENTTYPE.FIXED}>Fixed</option>
+              <option value={''}>Select Payment Type</option>
+              <option value={PayType.BANK}>BANK</option>
+              <option value={PayType.CRYPTO}>CRYPTO</option>
             </select>
-          </div> */}
-          {/* {formData.engagementType &&
-            (formData.engagementType === 'FIXED' ? (
-              <div className="mb-5">
-                <label
-                  htmlFor="fixed"
-                  className="mb-1 block text-sm font-medium text-neutral-300"
-                >
-                  Enter Progress Percentage
-                </label>
-                <input
-                  type="text"
-                  value={formData.progressPercentage || ''}
-                  onChange={(e) =>
-                    setFormData({
-                      ...formData,
-                      progressPercentage: Number(e.target.value),
-                    })
-                  }
-                  className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
-                />
-              </div>
-            ) : (
-              <div className="mb-5">
-                <label className="mb-1 block text-sm font-medium text-neutral-300">
-                  Enter Number of Hours
-                </label>
-                <input
-                  type="text"
-                  readOnly={formData.engagementType !== 'HOURLY'}
-                  value={formData.numberOfHours || ''}
-                  onChange={handleNoOfHoursChange}
-                  className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
-                />
-              </div>
-            ))} */}
+          </div>
+
+          <div className="mb-5">
+            <label
+              htmlFor="amountTotal"
+              className="mb-1 block text-sm font-medium text-neutral-300"
+            >
+              Total Amount (in USD)
+            </label>
+            <input
+              type="text"
+              id="amountTotal"
+              value={formData.amountTotal || 0}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  amountTotal: Number(e.target.value),
+                })
+              }
+              className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
+              placeholder="Enter total amount..."
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="amountDiscount"
+              className="mb-1 block text-sm font-medium text-neutral-300"
+            >
+              Discount Amount (in USD)
+            </label>
+            <input
+              type="text"
+              id="amountDiscount"
+              value={formData.amountDiscount || 0}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  amountDiscount: Number(e.target.value),
+                })
+              }
+              className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
+              placeholder="Enter discount amount..."
+            />
+          </div>
+
+          <div className="mb-5">
+            <label
+              htmlFor="amountToPay"
+              className="mb-1 block text-sm font-medium text-neutral-300"
+            >
+              Amount to Pay (in USD)
+            </label>
+            <input
+              type="text"
+              id="amountToPay"
+              value={
+                Number(formData.amountTotal) && Number(formData.amountDiscount)
+                  ? Number(formData.amountTotal - formData.amountDiscount) || 0
+                  : Number(formData.amountToPay) || 0
+              }
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  amountToPay: Number(e.target.value),
+                })
+              }
+              className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
+              placeholder="Enter amount to pay..."
+            />
+          </div>
+
+          <div className="mb-5">
+            <label
+              htmlFor="fileUpload"
+              className="mb-1 block text-sm font-medium text-neutral-300"
+            >
+              Upload Invoice PDF
+            </label>
+            <input
+              type="file"
+              multiple={false}
+              id="fileUpload"
+              onChange={handleFileDrop}
+              className="w-full rounded border border-neutral-500 bg-neutral-800 p-2 text-neutral-200"
+            />
+            {files && filePreview()}
+          </div>
+
           <div className="mt-auto">
             <button
               type="submit"
@@ -407,13 +629,15 @@ const InvoicesTab = ({
     <div className="relative flex !h-screen w-full items-center justify-center gap-6">
       <MobileBox customClass="!w-full mt-6 custom-scrollbar ">
         <p className="mb-4 text-center text-xs uppercase tracking-[0.5em] text-neutral-400">
-          filteredUsers
+          CLIENTS
         </p>
         <div className="my-2 flex max-h-full w-[90%] grow flex-col justify-start gap-4">
           {filteredUsers.map((user) => (
             <div
               onClick={() => {
                 setSelectedClientId(user.id);
+                resetForm();
+                setLoadingState(INITIAL_LOADING_STATE);
                 setFormData((prev) => ({
                   ...prev,
                   clientId: user.id,
@@ -494,7 +718,25 @@ const InvoicesTab = ({
                     <div
                       key={invoice.id}
                       className="flex items-center rounded border border-neutral-700 px-2 py-3"
-                    ></div>
+                    >
+                      <div className="flex w-full flex-row items-center justify-between">
+                        <div className="flex flex-row items-center gap-4">
+                          <div className="flex flex-col">
+                            <p className="text-sm font-bold text-neutral-300">
+                              {invoice.title}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex flex-row items-center gap-4">
+                          <p className="text-sm font-bold text-neutral-300">
+                            {dayjs(invoice.createdAt).format('DD MMM YYYY')}
+                          </p>
+                          <p className="text-sm font-bold text-neutral-300">
+                            {dayjs(invoice.updatedAt).format('DD MMM YYYY')}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
                   );
                 })}
               </div>
@@ -502,9 +744,7 @@ const InvoicesTab = ({
             {!loadingState.addNew && !loadingState.updating && (
               <button
                 className="absolute bottom-0 flex w-full items-center justify-center gap-2 rounded-lg bg-neutral-800 px-5 py-2 text-white shadow-md hover:bg-neutral-700"
-                onClick={() =>
-                  setLoadingState({ ...loadingState, addNew: true })
-                }
+                onClick={handleFormSubmit}
               >
                 <span className="material-symbols-outlined">group_add</span>
                 Add New Invoice
