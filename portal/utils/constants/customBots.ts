a41cc671f851b6bf162124e5493d213c170f6cdc
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-02-22 03:32:42 +0530

    feat: add GitHub SDK for repository and pull request management

--- portal/utils/constants/customBots.ts
+++ portal/utils/constants/customBots.ts
@@ -1,4 +1,6 @@
-export const CustomBotKickstarter = {
-    createRepo: 'https://r72jktvfamkooemrvnle4quyoq0slfam.lambda-url.ap-southeast-2.on.aws/create-repo',
-    createBot: 'https://r72jktvfamkooemrvnle4quyoq0slfam.lambda-url.ap-southeast-2.on.aws/create-app',
-}
+// export const CustomBotKickstarter = {
+//     createRepo: 'https://r72jktvfamkooemrvnle4quyoq0slfam.lambda-url.ap-southeast-2.on.aws/create-repo',
+//     createBot: 'https://r72jktvfamkooemrvnle4quyoq0slfam.lambda-url.ap-southeast-2.on.aws/create-app',
+// }
+export const TEMPLATE_REPO_OWNER = 'TheMoonDevs';
+export const TEMPLATE_REPO = 'CustomBots-Template';
