d268abbc6df4858ec129bee87ae822b71b6520f1
Kshitij <kshitij.suraj@gmail.com>
2025-02-26 14:01:09 +0530

    feat: added stack component, stack config file and infinte scroll stack component

---
+++ home/utils/configs/stackConfig.ts
@@ -0,0 +1,173 @@
+interface StackConfig {
+  id: string;
+  name: string;
+  img_url: string;
+  type: string;
+}
+
+export const stacks: StackConfig[] = [
+  {
+    name: 'Android',
+    type: 'framework',
+    id: 'android',
+    img_url: '/stacks/android.svg',
+  },
+  { name: 'AWS', type: 'cloud', id: 'aws', img_url: '/stacks/aws.svg' },
+  {
+    name: 'Bash',
+    type: 'language',
+    id: 'bash',
+    img_url: '/stacks/bash.svg',
+  },
+  {
+    name: 'CSS',
+    type: 'language',
+    id: 'css',
+    img_url: '/stacks/css.svg',
+  },
+  {
+    name: 'Dart',
+    type: 'language',
+    id: 'dart',
+    img_url: '/stacks/dart.svg',
+  },
+  {
+    name: 'Digital Ocean',
+    type: 'cloud',
+    id: 'digital-ocean',
+    img_url: '/stacks/digital-ocean.svg',
+  },
+  {
+    name: 'Flutter',
+    type: 'framework',
+    id: 'flutter',
+    img_url: '/stacks/flutter.svg',
+  },
+  { name: 'Go', type: 'language', id: 'go', img_url: '/stacks/go.svg' },
+  {
+    name: 'Google Cloud',
+    type: 'cloud',
+    id: 'google-cloud',
+    img_url: '/stacks/google-cloud.svg',
+  },
+  {
+    name: 'HTML',
+    type: 'language',
+    id: 'html',
+    img_url: '/stacks/html.svg',
+  },
+  {
+    name: 'Java',
+    type: 'language',
+    id: 'java',
+    img_url: '/stacks/java.svg',
+  },
+  {
+    name: 'JavaScript',
+    type: 'language',
+    id: 'javascript',
+    img_url: '/stacks/javascript.svg',
+  },
+  {
+    name: 'Kafka',
+    type: 'framework',
+    id: 'kafka',
+    img_url: '/stacks/kafka.svg',
+  },
+  {
+    name: 'Kotlin',
+    type: 'language',
+    id: 'kotlin',
+    img_url: '/stacks/kotlin.svg',
+  },
+  {
+    name: 'MongoDB',
+    type: 'database',
+    id: 'mongo-db',
+    img_url: '/stacks/mongo-db.svg',
+  },
+  {
+    name: 'MUI',
+    type: 'framework',
+    id: 'mui',
+    img_url: '/stacks/mui.svg',
+  },
+  {
+    name: 'MySQL',
+    type: 'database',
+    id: 'mysql',
+    img_url: '/stacks/mysql.svg',
+  },
+  {
+    name: 'Next.js',
+    type: 'framework',
+    id: 'nextjs',
+    img_url: '/stacks/nextjs.svg',
+  },
+  {
+    name: 'Node.js',
+    type: 'runtime',
+    id: 'nodejs',
+    img_url: '/stacks/nodejs.svg',
+  },
+  {
+    name: 'Objective-C',
+    type: 'language',
+    id: 'objective-c',
+    img_url: '/stacks/objective-c.svg',
+  },
+  {
+    name: 'PostgreSQL',
+    type: 'database',
+    id: 'postgres',
+    img_url: '/stacks/postgres.svg',
+  },
+  {
+    name: 'Powershell',
+    type: 'language',
+    id: 'powershell',
+    img_url: '/stacks/powershell.svg',
+  },
+  {
+    name: 'Python',
+    type: 'language',
+    id: 'python',
+    img_url: '/stacks/python.svg',
+  },
+  {
+    name: 'React',
+    type: 'framework',
+    id: 'react',
+    img_url: '/stacks/react.svg',
+  },
+  {
+    name: 'S3 Bucket',
+    type: 'cloud',
+    id: 's3-bucket',
+    img_url: '/stacks/s3-bucket.svg',
+  },
+  {
+    name: 'SQL',
+    type: 'language',
+    id: 'sql-language',
+    img_url: '/stacks/sql-language.svg',
+  },
+  {
+    name: 'Swift',
+    type: 'language',
+    id: 'swift',
+    img_url: '/stacks/swift.svg',
+  },
+  {
+    name: 'Tailwind',
+    type: 'framework',
+    id: 'tailwind',
+    img_url: '/stacks/tailwind.svg',
+  },
+  {
+    name: 'TypeScript',
+    type: 'language',
+    id: 'typescript',
+    img_url: '/stacks/typescript.svg',
+  },
+];
