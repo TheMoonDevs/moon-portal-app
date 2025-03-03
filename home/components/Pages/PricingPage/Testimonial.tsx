1ad702c9c9a444cf9503a713b607903bdce0fcfa
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2025-03-04 01:42:43 +0530

    fix: padding in testimonials section

--- home/components/Pages/PricingPage/Testimonial.tsx
+++ home/components/Pages/PricingPage/Testimonial.tsx
@@ -60,7 +60,7 @@ interface Testimonial {
 
 export function Testimonial() {
   return (
-    <div className="mx-full-bleed relative z-40 flex h-screen flex-col items-center justify-center bg-black p-6 px-8">
+    <div className="mx-full-bleed relative z-40 flex h-screen flex-col items-center justify-center bg-black p-6 px-8 xl:px-[12rem]">
       <Header />
       <CarouselContainer />
     </div>
