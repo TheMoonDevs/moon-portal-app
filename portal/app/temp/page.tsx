bca2e0c0e53ae4bdb24f2b0adeefa574ece4da2e
Kshitij <kshitij.suraj@gmail.com>
2025-03-13 03:15:29 +0530

    feat: novel editor component

---
+++ portal/app/temp/page.tsx
@@ -0,0 +1,11 @@
+import TailwindAdvancedEditor from '@/components/NovelEditor';
+
+const NovelEditorPage = () => {
+  return (
+    <div>
+      <TailwindAdvancedEditor />
+    </div>
+  );
+};
+
+export default NovelEditorPage;
