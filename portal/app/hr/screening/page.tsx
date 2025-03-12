a3526dd32432aee0f139e6f0b1f8da656656a6c6
Kshitij <kshitij.suraj@gmail.com>
2025-03-13 03:26:32 +0530

    fix: build error

--- portal/app/hr/screening/page.tsx
+++ portal/app/hr/screening/page.tsx
@@ -1,6 +1,5 @@
-import { LoginPage } from "@/components/screens/Login/LoginPage";
-import { ScreeningPage } from "@/components/screens/Screening/ScreeningPage";
-import React from "react";
+import { ScreeningPage } from '@/components/screens/Screening/ScreeningPage';
+import React from 'react';
 
 const ThisScreeningPage: React.FC = () => {
   return <ScreeningPage />;
