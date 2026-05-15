efe971895e55850a82471621a40a565edf5b7f4d
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:58:37 +0530

    refactor: standardize import statements and improve component layout

--- portal/components/screens/User/ClickupWorklogsPage.tsx
+++ portal/components/screens/User/ClickupWorklogsPage.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import { APP_ROUTES } from "@/utils/constants/appInfo";
-import { useUser } from "@/utils/hooks/useUser";
-import Link from "next/link";
+import Link from 'next/link';
+
+import { APP_ROUTES } from '@/utils/constants/appInfo';
+import { useUser } from '@/utils/hooks/useUser';
 
 export const ClickupWorklogsPage = () => {
   const { user } = useUser(false);
@@ -10,14 +11,14 @@ export const ClickupWorklogsPage = () => {
   if (!user?.workData) return null;
   return (
     <div className="flex flex-col">
-      <div className="absolute left-0 right-0 top-0 bg-white flex flex-row gap-3 py-3 items-center justify-between border-b border-neutral-400">
+      <div className="absolute inset-x-0 top-0 flex flex-row items-center justify-between gap-3 border-b border-neutral-400 bg-white py-3">
         <Link href={APP_ROUTES.home}>
           <div className="cursor-pointer rounded-lg px-2 text-neutral-900 hover:text-neutral-700">
             <span className="icon_size material-icons">arrow_back</span>
           </div>
         </Link>
         <h1 className="uppercase tracking-widest">Your Work Logs</h1>
-        <div className="text-xl rounded-lg px-2 text-neutral-900 hover:text-neutral-700">
+        <div className="rounded-lg px-2 text-xl text-neutral-900 hover:text-neutral-700">
           <span className="icon_size material-icons">add_circle_outline</span>
         </div>
       </div>
@@ -25,18 +26,18 @@ export const ClickupWorklogsPage = () => {
         <iframe
           src={(user.workData as any)?.worklogPubLink}
           style={{
-            width: "100%",
-            height: "100vh",
-            border: "none",
+            width: '100%',
+            height: '100vh',
+            border: 'none',
           }}
         ></iframe>
       )}
       <Link
-        href={(user.workData as any)?.worklogLink || ""}
+        href={(user.workData as any)?.worklogLink || ''}
         rel="noopener noreferrer"
         target="_blank"
       >
-        <div className="bg-black w-[2em] h-[2em] flex flex-row items-center justify-center rounded-[50%] text-2xl p-2 absolute bottom-[3em] right-[1em]">
+        <div className="absolute bottom-[3em] right-[1em] flex size-[2em] flex-row items-center justify-center rounded-[50%] bg-black p-2 text-2xl">
           <span className="icon_size material-icons text-white">edit</span>
         </div>
       </Link>
