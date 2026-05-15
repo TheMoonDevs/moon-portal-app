138f7dc65251e04a27f525f8bcee4c0409732c70
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:34:41 +0530

    fix: update import path for Mission and MissionTask in slice files

--- portal/utils/redux/missions/mission.ui.slice.ts
+++ portal/utils/redux/missions/mission.ui.slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Mission, MissionTask } from "@prisma/client";
+import { Mission, MissionTask } from "@db/client";
 
 interface IMissionUiState {
   isEditModalOpen: boolean;
