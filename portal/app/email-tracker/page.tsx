44f6528c67b215afcd6013c695e543b59d5aef82
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:26:26 +0530

    refactor: standardize import quotes and clean up code formatting

--- portal/app/email-tracker/page.tsx
+++ portal/app/email-tracker/page.tsx
@@ -1,14 +1,12 @@
-import { Bottombar } from "@/components/global/Bottombar";
-import { PageAccess } from "@/components/global/PageAccess";
-import EmailTrackerCard from "@/components/screens/EmailTracker/EmailTrackerCard";
-import { APP_ROUTES } from "@/utils/constants/appInfo";
-import Link from "next/link";
+import { Bottombar } from '@/components/global/Bottombar';
+import { PageAccess } from '@/components/global/PageAccess';
+import EmailTrackerCard from '@/components/screens/EmailTracker/EmailTrackerCard';
 
 const EmailTracker = () => {
   return (
     <PageAccess isAuthRequired={true} isAdminRequired={false}>
-      <div className="mx-auto px-1 md:px-4 pb-5">
-        <div className="flex items-center lg:justify-center md:min-h-screen max-w-screen overflow-x-auto">
+      <div className="mx-auto px-1 pb-5 md:px-4">
+        <div className="max-w-screen flex items-center overflow-x-auto md:min-h-screen lg:justify-center">
           <EmailTrackerCard />
         </div>
       </div>
