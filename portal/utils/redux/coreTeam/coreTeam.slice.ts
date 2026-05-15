54b4a13690772d7bddb3a4c1c12685009bc3014f
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:34:23 +0530

    fix: update import path for User in coreTeam slice

--- portal/utils/redux/coreTeam/coreTeam.slice.ts
+++ portal/utils/redux/coreTeam/coreTeam.slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User } from '@prisma/client';
+import { User } from '@db/client';
 
 interface CoreTeamState {
   members: User[];
