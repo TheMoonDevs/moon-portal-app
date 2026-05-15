915503e7913e4c2e048a03741df97b3fa285acc5
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:29:03 +0530

    refactor: remove unnecessary blank line in NotificationsPage component

--- portal/app/notifications/page.tsx
+++ portal/app/notifications/page.tsx
@@ -3,7 +3,6 @@ import { PageAccess } from '@/components/global/PageAccess';
 import NotificationsScreen from '@/components/screens/notifications/NotificationsScreen';
 
 export default function NotificationsPage() {
-
   return (
     <PageAccess isAuthRequired={true}>
       <NotificationsScreen />
