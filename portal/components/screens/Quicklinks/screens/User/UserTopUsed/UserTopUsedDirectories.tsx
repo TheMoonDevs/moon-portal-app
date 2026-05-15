42a14b2f5c9c6ace34d9cc8081caf70dea3c7f0b
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:56:59 +0530

    refactor: standardize import statements and improve code formatting

--- portal/components/screens/Quicklinks/screens/User/UserTopUsed/UserTopUsedDirectories.tsx
+++ portal/components/screens/Quicklinks/screens/User/UserTopUsed/UserTopUsedDirectories.tsx
@@ -1,35 +1,37 @@
-"use client";
+'use client';
 
-import { useUser } from "@/utils/hooks/useUser";
-import { useAppDispatch, useAppSelector } from "@/utils/redux/store";
-import { QuicklinksSdk } from "@/utils/services/QuicklinksSdk";
-import { DirectoryList, USERDIRECTORYTYPE } from "@prisma/client";
-import { useEffect } from "react";
-import ListOfDirectories from "../../../DirectoryList";
-import { setTopUsedDirectoryList } from "@/utils/redux/quicklinks/slices/quicklinks.directory.slice";
-import { useQuickLinkDirectory } from "../../../hooks/useQuickLinkDirectory";
+import type { DirectoryList } from '@db/client';
+import { USERDIRECTORYTYPE } from '@db/client';
+import { useEffect } from 'react';
+
+import { useUser } from '@/utils/hooks/useUser';
+import { setTopUsedDirectoryList } from '@/utils/redux/quicklinks/slices/quicklinks.directory.slice';
+import { useAppDispatch, useAppSelector } from '@/utils/redux/store';
+import { QuicklinksSdk } from '@/utils/services/QuicklinksSdk';
+
+import ListOfDirectories from '../../../DirectoryList';
 
 const UserTopUsedDirectories = ({
   withTitle = true,
-  view = "gridView",
+  view = 'gridView',
   searchQuery,
 }: {
   withTitle?: boolean;
-  view?: "listView" | "gridView";
+  view?: 'listView' | 'gridView';
   searchQuery?: string;
 }) => {
   const { user } = useUser();
   const { topUsedDirectoryList } = useAppSelector(
-    (state) => state.quicklinksDirectory
+    (state) => state.quicklinksDirectory,
   );
   const dispatch = useAppDispatch();
 
   const filterDirectory = (
-    searchQuery: string | undefined
+    searchQuery: string | undefined,
   ): DirectoryList[] => {
     if (!searchQuery) return topUsedDirectoryList;
     return topUsedDirectoryList.filter((dir) =>
-      dir.title.toLowerCase().includes(searchQuery)
+      dir.title.toLowerCase().includes(searchQuery),
     );
   };
 
@@ -38,18 +40,18 @@ const UserTopUsedDirectories = ({
     const getTopUsedDirectories = async () => {
       try {
         const userTopUsedDirectories = await QuicklinksSdk.getData(
-          `/api/quicklinks/directory-list/user-directory?userId=${user.id}&directoryType=${USERDIRECTORYTYPE.OTHER}`
+          `/api/quicklinks/directory-list/user-directory?userId=${user.id}&directoryType=${USERDIRECTORYTYPE.OTHER}`,
         );
         const sortedByClickCount = userTopUsedDirectories.sort(
           (a: any, b: any) => {
             return b.clickCount - a.clickCount;
-          }
+          },
         );
 
         dispatch(
           setTopUsedDirectoryList(
-            sortedByClickCount.map((dir: any) => dir.directoryData)
-          )
+            sortedByClickCount.map((dir: any) => dir.directoryData),
+          ),
         );
       } catch (error) {
         console.log(error);
@@ -63,10 +65,10 @@ const UserTopUsedDirectories = ({
     <div>
       {withTitle && (
         <div>
-          <h1 className="text-3xl font-bold flex items-center gap-4 pb-5 max-sm:text-2xl max-sm:gap-3 ">
-            <span className="material-symbols-outlined border border-neutral-200 rounded-full p-2">
+          <h1 className="flex items-center gap-4 pb-5 text-3xl font-bold max-sm:gap-3 max-sm:text-2xl">
+            <span className="material-symbols-outlined rounded-full border border-neutral-200 p-2">
               history
-            </span>{" "}
+            </span>{' '}
             <span>Top Used Folders</span>
           </h1>
         </div>
