630a0998e47d970fd6a6ac6e29f79b6d31fbdc9f
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-05-15 16:40:08 +0530

    refactor: standardize quotes and improve formatting in ErrorPage component

--- portal/components/global/ErrorPage.tsx
+++ portal/components/global/ErrorPage.tsx
@@ -1,73 +1,74 @@
-"use client"
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
+'use client';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 
 const ErrorPage = () => {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-    const bgRef = useRef<HTMLSpanElement>(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const bgRef = useRef<HTMLSpanElement>(null);
 
-    const handleMouseMove = (event: MouseEvent) => {
-        setMousePosition({ x: event?.clientX, y: event?.clientY });
+  const handleMouseMove = (event: MouseEvent) => {
+    setMousePosition({ x: event?.clientX, y: event?.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    setMousePosition({ x: 0, y: 0 });
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseleave', handleMouseLeave);
     };
+  }, []);
 
-    const handleMouseLeave = () => {
-        setMousePosition({ x: 0, y: 0 });
-    };
+  useEffect(() => {
+    if (bgRef.current) {
+      const rect = bgRef.current.getBoundingClientRect();
+      let xTilt = -(mousePosition.y - (rect.top + rect.height / 2)) / 200;
+      let yTilt = (mousePosition.x - (rect.left + rect.width / 2)) / 200;
+      if (mousePosition.x === 0 && mousePosition.y === 0) {
+        xTilt = 0;
+        yTilt = 0;
+      }
+      bgRef.current.style.transform = `perspective(1000px) rotateX(${xTilt}deg) rotateY(${yTilt}deg)`;
+      bgRef.current.style.transition = 'all 0.2s ease-out';
+    }
+  }, [mousePosition]);
 
-    useEffect(() => {
-        document.addEventListener("mousemove", handleMouseMove);
-        document.addEventListener("mouseleave", handleMouseLeave);
-
-        return () => {
-            document.removeEventListener("mousemove", handleMouseMove);
-            document.removeEventListener("mouseleave", handleMouseLeave);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (bgRef.current) {
-            const rect = bgRef.current.getBoundingClientRect();
-            let xTilt = -(mousePosition.y - (rect.top + rect.height / 2)) / 200;
-            let yTilt = (mousePosition.x - (rect.left + rect.width / 2)) / 200;
-            if (mousePosition.x === 0 && mousePosition.y === 0) {
-                xTilt = 0;
-                yTilt = 0;
-            }
-            bgRef.current.style.transform = `perspective(1000px) rotateX(${xTilt}deg) rotateY(${yTilt}deg)`;
-            bgRef.current.style.transition = "all 0.2s ease-out";
-        }
-    }, [mousePosition]);
-
-    return (
-        <ErrorPageStyled>
-            <div className="background_404">
-                <span ref={bgRef}>404</span>
-                <div className="blur_blob"></div>
-            </div>
-            <div className="top_heading">
-                <span>404</span>
-                <span>Error Page</span>
-            </div>
-            <div className="center_heading">
-                <h1 className="title">404</h1>
-                <p className="subtitle">Sorry, we couldn&apos;t find this page</p>
-                <Link className="go_back" href="/">
-                    <span className="material-symbols-outlined"> arrow_back </span>
-                    <span className="go_back_text">Back To Home</span>
-                </Link>
-            </div>
-            <div className="bottom_heading">
-                <span>This page you are looking for doesn&apos;t</span>
-                <span>exist or some other error occurred.</span>
-            </div>
-        </ErrorPageStyled>
-    );
+  return (
+    <ErrorPageStyled>
+      <div className="background_404">
+        <span ref={bgRef}>404</span>
+        <div className="blur_blob"></div>
+      </div>
+      <div className="top_heading">
+        <span>404</span>
+        <span>Error Page</span>
+      </div>
+      <div className="center_heading">
+        <h1 className="title">404</h1>
+        <p className="subtitle">Sorry, we couldn&apos;t find this page</p>
+        <Link className="go_back" href="/">
+          <span className="material-symbols-outlined"> arrow_back </span>
+          <span className="go_back_text">Back To Home</span>
+        </Link>
+      </div>
+      <div className="bottom_heading">
+        <span>This page you are looking for doesn&apos;t</span>
+        <span>exist or some other error occurred.</span>
+      </div>
+    </ErrorPageStyled>
+  );
 };
 
 export default ErrorPage;
 
-import styled from "@emotion/styled";
-import media from "@/styles/media";
+import styled from '@emotion/styled';
+
+import media from '@/styles/media';
 
 export const ErrorPageStyled = styled.div`
   height: 100dvh;
@@ -225,7 +226,7 @@ export const ErrorPageStyled = styled.div`
         position: relative;
         &:after {
           transition: max-width 0.2s ease-in;
-          content: "";
+          content: '';
           position: absolute;
           bottom: 0;
           left: 0;
