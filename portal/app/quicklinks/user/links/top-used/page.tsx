c2dc0bf3b20a133cb6815d7a18f58b620a6c27ab
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-08-07 19:20:14 +0530

    chore: remove Quicklinks feature

--- portal/app/quicklinks/user/links/top-used/page.tsx
+++
@@ -1,7 +0,0 @@
-import UserTopUsedLinks from '@/components/screens/Quicklinks/screens/User/UserTopUsed/UserTopUsedLinks';
-
-const UserTopUsedLinksPage = () => {
-  return <UserTopUsedLinks withTitle />;
-};
-
-export default UserTopUsedLinksPage;
--- portal/app/quicklinks/user/user-list/page.tsx
+++
@@ -1,7 +0,0 @@
-import UserSavedList from '@/components/screens/Quicklinks/screens/User/UserSavedList';
-
-const UserListPage = () => {
-  return <UserSavedList />;
-};
-
-export default UserListPage;
